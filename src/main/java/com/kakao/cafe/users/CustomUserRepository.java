c5b0adde2408403bec0da3ddaacd519078c89c54
WOC <tom558@naver.com>
2022-01-10 19:37:38 +0900

    feat : implement custom repository update

--- src/main/java/com/kakao/cafe/users/CustomUserRepository.java
+++ src/main/java/com/kakao/cafe/users/CustomUserRepository.java
@@ -3,18 +3,22 @@ package com.kakao.cafe.users;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.*;
 
 @Primary
 @Repository
 public class CustomUserRepository implements UserRepository {
 
+    private Map<String, User> userMap;
+
     public CustomUserRepository() {
+        userMap = new HashMap<>();
     }
 
     @Override
     public void update(User user) {
-        System.out.println("TODO");
+        if (userMap.containsKey(user.getUserId())) throw new IllegalArgumentException();
+        userMap.put(user.getUserId(), user);
     }
 
     @Override
