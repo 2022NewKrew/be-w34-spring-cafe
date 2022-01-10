d93dbcc490864db81484f77005f79561928fce97
SeongMin Park <parkseongmin98@gmail.com>
2022-01-10 22:11:06 +0900

    카카오 카페 1단계 (#24)
    * [setting] resources 디렉토리 추가
    
    * [setting] 환경설정
    
    * [feat] html 파일 변경
    
    * [setting] suffix 설정
    
    * [feat] UserController 추가
    
    * [feat] User, UserService 추가

---
+++ src/main/java/com/kakao/cafe/domain/User.java
@@ -0,0 +1,47 @@
+package com.kakao.cafe.domain;
+
+public class User {
+    String userId;
+    String password;
+    String name;
+    String email;
+
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
