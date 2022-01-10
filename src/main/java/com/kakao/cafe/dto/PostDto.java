afc78c0764ac62ee328321c1f723dc689b1c02e2
kusakina0608 <platina.kusakina@gmail.com>
2022-01-11 01:56:22 +0900

    PostDto, UserDto 추가

---
+++ src/main/java/com/kakao/cafe/dto/PostDto.java
@@ -0,0 +1,21 @@
+package com.kakao.cafe.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PostDto {
+    private Long postId;
+    private String title;
+    private String content;
+    private String writerEmail;
+    private Long viewCount;
+    private LocalDateTime regDate, modDate;
+}
---
+++ src/main/java/com/kakao/cafe/dto/UserDto.java
@@ -0,0 +1,19 @@
+package com.kakao.cafe.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserDto {
+    private String email;
+    private String username;
+    private String password;
+    private LocalDateTime regDate, modDate;
+}
