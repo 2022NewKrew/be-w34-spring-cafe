4bfa182097d6f107fb2bff8f23f8dd0baf2f77d5
edward.baek <edward.baek@kakaocorp.com>
2022-01-10 19:35:34 +0900

    feat : userAccount를 위한 repository 추가
    - 추후에 확장성을 위해서 인터페이스 정의
    - Memory에 저장하는 repository 생성

---
+++ src/test/java/com/kakao/cafe/infra/user/UserAccountMemoryRepositoryImplTest.java
@@ -0,0 +1,71 @@
+package com.kakao.cafe.infra.user;
+
+import com.kakao.cafe.domain.user.UserAccount;
+import com.kakao.cafe.domain.user.UserAccountRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserAccountMemoryRepositoryImplTest {
+
+    @Autowired
+    UserAccountRepository accountRepository;
+
+    UserAccount testUser1 = UserAccount.builder()
+            .email("edward@kakao.com")
+            .password("1234")
+            .username("edward")
+            .createdAt(LocalDate.now())
+            .build();
+
+    UserAccount testUser2 = UserAccount.builder()
+            .email("peach@kakao.com")
+            .password("4321")
+            .username("peach")
+            .createdAt(LocalDate.now())
+            .build();
+
+    @Test
+    @DisplayName("저장 테스트")
+    void save() {
+
+        UserAccount result = accountRepository.save(testUser1).orElseThrow(IllegalArgumentException::new);
+
+        Assertions.assertThat(result.getUserAccountId()).isNotNull();
+        Assertions.assertThat(result.getUsername()).isEqualTo(testUser1.getUsername());
+        Assertions.assertThat(result.getEmail()).isEqualTo(testUser1.getEmail());
+        Assertions.assertThat(result.getPassword()).isEqualTo(testUser1.getPassword());
+    }
+
+    @Test
+    @DisplayName("찾기 테스트")
+    void find() {
+
+        UserAccount saved = accountRepository.save(testUser1).orElseThrow(IllegalArgumentException::new);
+
+        UserAccount found = accountRepository.find(saved.getUserAccountId()).orElseThrow(IllegalArgumentException::new);
+
+        Assertions.assertThat(found.getUserAccountId()).isEqualTo(saved.getUserAccountId());
+    }
+
+    @Test
+    @DisplayName("전부 찾아오기")
+    void findAll() {
+        accountRepository.save(testUser1);
+        accountRepository.save(testUser2);
+
+        List<UserAccount> found = accountRepository.findAll();
+
+        Assertions.assertThat(found.size()).isEqualTo(2);
+    }
+
+}
