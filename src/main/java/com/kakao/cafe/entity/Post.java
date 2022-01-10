f9e29db09dd5ef61f01263ba666a790f5e568bb1
kusakina0608 <platina.kusakina@gmail.com>
2022-01-10 18:59:32 +0900

    Post Entity 추가

---
+++ src/main/java/com/kakao/cafe/entity/Post.java
@@ -0,0 +1,9 @@
+package com.kakao.cafe.entity;
+
+public class Post extends BaseEntity {
+    private Long postId;
+    private String title;
+    private String content;
+    private String writerEmail;
+    private Long viewCount;
+}
