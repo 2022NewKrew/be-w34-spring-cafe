cb62ef8bbaea10f56264a3ba0b1f5dc945ba4f7e
Dongchan KIM <fromecha@gmail.com>
2022-01-10 14:31:55 +0900

    [setting] mustache template 설정 테스트

--- src/main/java/com/kakao/cafe/user/controller/UserController.java
+++ src/main/java/com/kakao/cafe/user/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.kakao.cafe.user.controller;
 
+import com.kakao.cafe.user.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class UserController {
+    private List<User> users = new ArrayList<>();
+
+    @RequestMapping("/abcdef")
+    public String user() {
+        return "form";
+    }
+
+//    @PostMapping("/")
+
 }
