ed608a3cc550f7a7e00b5d891f4cef15f2750bc7
jayden-bin <jayden.bin@kakaocorp.com>
2022-01-10 15:55:56 +0900

    ModelAndView 수정
    - UserController에서 ModelAndView -> Model로 수정

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -3,13 +3,12 @@ package com.kakao.cafe.controller;
 import com.kakao.cafe.service.UserService;
 import com.kakao.cafe.vo.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -19,35 +18,32 @@ public class UserController {
     UserService userService;
 
     @GetMapping
-    ModelAndView users(HttpServletRequest request) {
+    String users(Model model) {
         List<User> userList = userService.getUserList();
-        ModelAndView mv = new ModelAndView();
-        mv.addObject("users", userList);
-        mv.setViewName("user/list");
+        model.addAttribute("users", userList);
 
-        return mv;
+        return "user/list";
     }
 
     @GetMapping("/form")
-    String form(HttpServletRequest request) {
+    String form() {
 
         return "user/form";
     }
 
     @GetMapping("/create")
-    String create(HttpServletRequest request, User user) {
+    String create(User user) {
         userService.insertUser(user);
         return "redirect:/users";
     }
 
     @GetMapping("/{userId}")
-    ModelAndView profile(@PathVariable String userId) {
+    String profile(@PathVariable String userId, Model model) {
         User user = userService.getUserByUserId(userId);
-        ModelAndView mv = new ModelAndView();
-        mv.addObject("name", user.getName());
-        mv.addObject("email", user.getEmail());
-        mv.setViewName("user/profile");
-        return mv;
+        model.addAttribute("name", user.getName());
+        model.addAttribute("email", user.getEmail());
+
+        return "user/profile";
     }
 
 }
