005fa482e4eeb5ff19bbe557ec1900dec80efdbb
binary.yun <binary.yun@kakaocorp.com>
2022-01-11 01:25:36 +0900

    fix: 유저 리스트를 synchronizedList로 변경

--- src/main/java/com/kakao/cafe/domain/user/UserInMemoryRepository.java
+++ src/main/java/com/kakao/cafe/domain/user/UserInMemoryRepository.java
@@ -3,11 +3,13 @@ package com.kakao.cafe.domain.user;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class UserInMemoryRepository implements UserRepository {
-    private static final ArrayList<User> userList = new ArrayList<>();
+    private static final List<User> userList = Collections.synchronizedList(new ArrayList<>());
 
     @Override
     public Optional<User> findByUserId(String userId) {
