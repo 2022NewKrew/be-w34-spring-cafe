d5eda884519456774b4ed951a7d42cd5a31e6e48
ben.ny <ben.ny@kakaocorp.com>
2022-01-10 22:08:54 +0900

    Add : User 클래스 추가

---
+++ src/main/java/com/kakao/cafe/web/User.java
@@ -0,0 +1,33 @@
+package com.kakao.cafe.web;
+
+public class User {
+    private static long offset = 0;
+    private final long id;
+    private final String userId;
+    private final String password;
+    private final String name;
+    private final String email;
+
+    public User(String userId, String password, String name, String email) {
+        offset += 1;
+        this.id = offset;
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+}
