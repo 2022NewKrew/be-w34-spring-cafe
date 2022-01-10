ba5d8b13a6a10d962d3ecde3b91a8b17eb6e5dc4
chiveloper <wodlr2007@naver.com>
2022-01-10 22:11:08 +0900

    [feature] 회원가입, 리스트 페이지 연동하기 (#25)
    - 회원가입, 리스트 페이지를 클릭했을 때 원하는 페이지로 연동하기
    - templates에 form, list html을 넣어 연동하기
    
    Co-authored-by: pride.chicken <pride.chicken@kakaocorp.com>

---
+++ src/main/java/com/kakao/cafe/model/User.java
@@ -0,0 +1,47 @@
+package com.kakao.cafe.model;
+
+public class User {
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public User(String userId, String password, String name, String email){
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
