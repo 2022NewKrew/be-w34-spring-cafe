3987cfbb0ccee9e0ecc62feab6f660c0c5a95326
= <=>
2022-01-10 17:46:39 +0900

    프로필 api 주소 변경

--- src/main/java/com/kakao/cafe/UserController.java
+++ src/main/java/com/kakao/cafe/UserController.java
@@ -37,7 +37,7 @@ public class UserController {
         return "/user/list";
     }
 
-    @GetMapping("/user/profile/{userId}")
+    @GetMapping("/user/{userId}")
     public String getUserProfile(@PathVariable String userId, Model model) {
         User foundUser = null;
         for (User user : userList) {
