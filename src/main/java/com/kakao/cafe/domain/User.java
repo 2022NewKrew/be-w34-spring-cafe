455233c6e6290d9a6d678010f172dc70e81efa89
Kimgeunwook <wook961206@naver.com>
2022-01-10 21:27:28 +0900

    Refactor: User클래스 코드 정리

--- src/main/java/com/kakao/cafe/domain/User.java
+++ src/main/java/com/kakao/cafe/domain/User.java
@@ -8,13 +8,4 @@ public class User {
     private String password;
     private String name;
     private String email;
-
-
-    public User(String userId, String password, String name, String email) {
-        this.userId = userId;
-        this.password = password;
-        this.name = name;
-        this.email = email;
-    }
-
 }
