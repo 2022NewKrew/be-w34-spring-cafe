5438f02dca838933e3cb7f9e82e23656d9ac373d
kimdaeseon <kimdaeseon@khu.ac.kr>
2022-01-10 22:21:47 +0900

    리뷰 반영완료
    구현되지 않은 기능 추가
    1. 회원 목록에서 번호 출력
    2. 유저 리스트를 클릭했을 때 유저 프로필로 이동
    RequestMapping("/users") 추가 및 변수명 수정 targetUser -> user

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -6,31 +6,33 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("users")
 public class UserController {
     private final List<User> users = new ArrayList<>();
 
-    @GetMapping("/users")
+    @GetMapping()
     public String showUsers(Model model) {
         model.addAttribute("users", users);
+        model.addAttribute("users.index", 1);
         return "list";
     }
 
-    @PostMapping("/users")
+    @PostMapping()
     public String register(User user) {
         users.add(user);
         return "redirect:users";
     }
 
-    @GetMapping("/users/{userId}")
+    @GetMapping("/{userId}")
     public String showUserProfile(@PathVariable String userId, Model model) {
-        User targetUser = findUser(userId);
-        model.addAttribute("user", targetUser);
+        User user = findUser(userId);
+        model.addAttribute("user", user);
         return "profile";
     }
 
