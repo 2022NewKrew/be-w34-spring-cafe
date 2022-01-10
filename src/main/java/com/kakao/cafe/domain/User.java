9fc12a1c926b98a50e4e9cee382848f608348701
jun.dev <jun.dev@kakaocorp.com>
2022-01-10 13:59:11 +0900

    feat: User클래스 추가

---
+++ src/main/java/com/kakao/cafe/domain/User.java
@@ -0,0 +1,24 @@
+package com.kakao.cafe.domain;
+
+public class User {
+    private String userID;
+    private String userPW;
+    private String userName;
+    private String userEmail;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getUserPW() {
+        return userPW;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+}
