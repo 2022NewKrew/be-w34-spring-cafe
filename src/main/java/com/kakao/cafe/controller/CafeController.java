fa884db679472398cb3c7a1e6d492dcbc81a4510
kusakina0608 <platina.kusakina@gmail.com>
2022-01-10 18:58:23 +0900

    CafeController 추가

---
+++ src/main/java/com/kakao/cafe/controller/CafeController.java
@@ -0,0 +1,19 @@
+package com.kakao.cafe.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
+@RequiredArgsConstructor
+public class CafeController {
+//    private final BoardService boardService;
+//    private final MemberService memberService;
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/board/list";
+    }
+}
