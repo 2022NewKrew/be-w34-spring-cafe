1762215ecf6dbe06168c4d56ce15f06833514286
danny.an <danny.an@kakaocorp.com>
2022-01-10 16:20:03 +0900

    UserController 생성 & 회원가입 기능 구현

---
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.kakao.cafe.controller;
+
+import com.kakao.cafe.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    private List<User> users = new ArrayList<>();
+
+    @GetMapping("")
+    public String users() {
+        return "/user/list";
+    }
+
+    @GetMapping("/signup")
+    public String signUp() {
+        return "/user/form";
+    }
+
+    @PostMapping("/signup")
+    public String createUser(User user) {
+        users.add(user);
+        return "redirect:/users";
+    }
+}
