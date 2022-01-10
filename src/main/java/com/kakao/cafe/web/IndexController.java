2f0a5a39704d77ede5d31496395f26dc7dd93bee
chance.kk <chance.kk@kakaocorp.com>
2022-01-10 16:05:16 +0900

    Created IndexController, layout/header and footer

---
+++ src/main/java/com/kakao/cafe/web/IndexController.java
@@ -0,0 +1,26 @@
+package com.kakao.cafe.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class IndexController {
+
+    Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+/*    @GetMapping("/user/form")
+    public String getUserForm() {
+        return
+    }*/
+
+    @PostMapping("/user/form")
+    public String postUser() {
+        return "redirect:index";
+    }
+}
