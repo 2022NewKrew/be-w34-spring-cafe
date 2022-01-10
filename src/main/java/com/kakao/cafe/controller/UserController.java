df32bef63a48ed7d3c53edc245080fdc30ef2c5c
weather.white <weather.white@kakaocorp.com>
2022-01-10 15:40:01 +0900

    [step-1-1]feat: 회원목록 조회
    - 템플릿엔진 세팅, 경로 변경 (list.html)
    - logger 추가
    - 회원목록조회 구현

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -2,6 +2,8 @@ package com.kakao.cafe.controller;
 
 import com.kakao.cafe.domain.User;
 import com.kakao.cafe.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +15,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("users")
 public class UserController {
 
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserService userService = new UserService();
 
     @PostMapping("")
     public String signUp(@ModelAttribute User user){
+        logger.info("POST:/users 회원가입 {}", user.getUserId());
         userService.signUp(user);
         return "redirect:/users";
     }
+
+    @GetMapping("")
+    public String viewUsersList(Model model){
+        logger.info("GET:/users 유저목록조회");
+        model.addAttribute("users", userService.findAllUsers());
+        return "user/list";
+    }
+
+
 
 }
