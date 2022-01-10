71a646f713999bbb42e05927517f42c1ebd8e87a
hungryjayy <juwonson2068@naver.com>
2022-01-10 16:54:20 +0900

    feat: Add get /users

---
+++ src/main/java/com/kakao/cafe/common/MvcConfiguration.java
@@ -0,0 +1,15 @@
+package com.kakao.cafe.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/templates/", "classpath:/static/");
+    }
+}
--- src/main/java/com/kakao/cafe/user/UserController.java
+++ src/main/java/com/kakao/cafe/user/UserController.java
@@ -1,6 +1,10 @@
 package com.kakao.cafe.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,6 +15,8 @@ import java.util.List;
 public class UserController {
     private final List<User> users = new ArrayList<>();
 
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @PostMapping("/users")
     public String save(
             @RequestParam String userId,
@@ -18,11 +24,18 @@ public class UserController {
             @RequestParam String name,
             @RequestParam String email
     ) {
-        System.out.println("회원가입");
+        logger.info("회원 가입");
         User newUser = new User(userId, password, name, email);
 
         users.add(newUser);
 
-        return "redirect: /users";
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String findAll(Model model) {
+        model.addAttribute("users", users);
+
+        return "resources/templates/user";
     }
 }
