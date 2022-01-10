e83510857ea7c36032f5b934927efa4c206067f6
kusakina0608 <platina.kusakina@gmail.com>
2022-01-11 02:03:30 +0900

    PostController와 화면에서 글 목록을 처리하도록 변경

--- src/main/java/com/kakao/cafe/controller/PostController.java
+++ src/main/java/com/kakao/cafe/controller/PostController.java
@@ -1,10 +1,12 @@
 package com.kakao.cafe.controller;
 
-import com.kakao.cafe.service.BoardService;
+import com.kakao.cafe.dto.PageRequestDto;
+import com.kakao.cafe.service.PostService;
 import com.kakao.cafe.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,12 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/board")
 @Log4j2
 @RequiredArgsConstructor
-public class BoardController {
-    private final BoardService boardService;
+public class PostController {
+    private final PostService postService;
     private final UserService userService;
 
     @GetMapping("/list")
-    public String getBoardList() {
+    public String getBoardList(PageRequestDto pageRequestDto, Model model) {
+        model.addAttribute("posts", postService.getList(pageRequestDto));
         return "/index";
     }
 }
