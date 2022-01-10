29ef28d0a0de3763036f9aa9a4722a22002f1ead
HungryJayy <49271821+hungryjayy@users.noreply.github.com>
2022-01-10 22:10:50 +0900

    카카오 카페 Day1 (#12)
    * Initial commit
    
    * feat: Add post /users
    
    * feat: Add get /users
    
    * feat: Add get /users/{userId}
    
    * feat: Add view mapping config
    
    * fix: Modify path
    
    * style: EOF

---
+++ src/main/java/com/kakao/cafe/common/MvcConfiguration.java
@@ -0,0 +1,26 @@
+package com.kakao.cafe.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/templates/", "classpath:/static/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        registry.addViewController("/users/form").setViewName("user/form");
+        registry.addViewController("/users/login").setViewName("user/login");
+        registry.addViewController("/questions/form").setViewName("qna/form");
+    }
+}
---
+++ src/main/java/com/kakao/cafe/user/UserController.java
@@ -0,0 +1,53 @@
+package com.kakao.cafe.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Controller
+public class UserController {
+    private final List<User> users = new ArrayList<>();
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @PostMapping("/users")
+    public String save(
+            @RequestParam String userId,
+            @RequestParam String password,
+            @RequestParam String name,
+            @RequestParam String email
+    ) {
+        logger.info("회원 가입");
+        User newUser = new User(userId, password, name, email);
+        users.add(newUser);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String findAll(Model model) {
+        logger.info("회원 목록");
+        model.addAttribute("users", users);
+        return "user/list";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String findById(@PathVariable String userId, Model model) {
+        logger.info("회원 프로필");
+        User target = users.stream()
+                .filter(user -> user.isSameUser(userId))
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
+        model.addAttribute("profile", target);
+        return "user/profile";
+    }
+
+}
