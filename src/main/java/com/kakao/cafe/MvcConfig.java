ba16f3487617e8f480a53202c0cd46b108fb3835
lsh0902 <hehesi2007@naver.com>
2022-01-10 15:15:08 +0900

    fixed

--- src/main/java/com/kakao/cafe/MvcConfig.java
+++ src/main/java/com/kakao/cafe/MvcConfig.java
@@ -9,6 +9,8 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
 
-//        registry.addViewController("/user/form").setViewName("user/form");
+        registry.addViewController("/user/form").setViewName("user/form");
+        registry.addViewController("/users/login").setViewName("user/login");
+        registry.addViewController("/questions/form").setViewName("qna/form");
     }
 }
