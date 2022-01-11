61cd242f02482fe7fc5f8764fe928f88e8925064
jun.dev <jun.dev@kakaocorp.com>
2022-01-11 10:18:44 +0900

    feat: Article repository, service 추가

---
+++ src/main/java/com/kakao/cafe/repository/ArticleRepository.java
@@ -0,0 +1,18 @@
+package com.kakao.cafe.repository;
+
+import com.kakao.cafe.domain.Article;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleRepository {
+    static private List<Article> articles = new ArrayList<>();
+
+    public void addArticle(Article article){
+        articles.add(article);
+    }
+
+    public List<Article> getAllAlticles(){
+        return articles;
+    }
+}
