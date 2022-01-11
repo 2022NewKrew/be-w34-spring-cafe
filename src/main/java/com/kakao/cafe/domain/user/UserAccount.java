1092cd06327edc782e8e58ae5cbdd18a62d13687
edward.baek <edward.baek@kakaocorp.com>
2022-01-11 23:05:14 +0900

    feat : getUsername() 추가

--- src/main/java/com/kakao/cafe/domain/user/UserAccount.java
+++ src/main/java/com/kakao/cafe/domain/user/UserAccount.java
@@ -39,7 +39,15 @@ public class UserAccount {
         return this.email.equals(email);
     }
 
+    public boolean checkUserAccountId(Long id) {
+        return Objects.equals(this.userAccountId, id);
+    }
+
     public Long getUserAccountId() {
         return userAccountId;
+    }
+
+    public String getUsername() {
+        return username;
     }
 }
