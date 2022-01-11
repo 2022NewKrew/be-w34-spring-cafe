f03869e6801930f57ec26d5d79fdad26e1d85e8e
sukstar76 <sukstar76@gmail.com>
2022-01-11 17:15:47 +0900

    ArticleRepository
    게시글들을 저장하기위한 메모리 저장소

---
+++ src/main/java/com/kakao/cafe/articles/MemoryArticleRepository.java
@@ -0,0 +1,40 @@
+package com.kakao.cafe.articles;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class MemoryArticleRepository implements ArticleRepository {
+    private final List<Article> articles;
+
+    public MemoryArticleRepository() {
+        articles = new ArrayList<>();
+    }
+
+    @Override
+    public Article save(Article article) {
+        articles.add(article);
+
+        return article;
+    }
+
+    @Override
+    public Optional<Article> findById(Long id) {
+        for (Article article : articles) {
+            if (article.getId().equals(id)) {
+                return Optional.of(article);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Article> findAll() {
+        return articles;
+    }
+
+    public int getArticlesSize() {
+        return articles.size();
+    }
+}
