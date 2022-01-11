9265018adbacfd093926e82af6e450284cdfd424
bryan.ik <bryan.ik@kakaocorp.com>
2022-01-11 17:40:35 +0900

    FEAT : 회원가입 시 User 의 생성 날짜/시간을 함께 저장하도록 수정

--- src/main/java/com/kakao/cafe/model/User.java
+++ src/main/java/com/kakao/cafe/model/User.java
@@ -1,5 +1,6 @@
 package com.kakao.cafe.model;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.atomic.AtomicLong;
 import lombok.ToString;
 
@@ -15,20 +16,24 @@ public class User {
     private final String name;
     private final String email;
 
+    private final LocalDateTime createdAt;
+
     static {
         idGenerator = new AtomicLong(1L);
     }
 
     public static User of(String userId, String password, String name, String email) {
-        return new User(userId, password, name, email);
+        return new User(userId, password, name, email, LocalDateTime.now());
     }
 
-    private User(String userId, String password, String name, String email) {
+    private User(String userId, String password, String name, String email,
+        LocalDateTime createdAt) {
         this.id = idGenerator.getAndIncrement();
         this.userId = userId;
         this.password = password;
         this.name = name;
         this.email = email;
+        this.createdAt = createdAt;
     }
 
     public Long getId() {
