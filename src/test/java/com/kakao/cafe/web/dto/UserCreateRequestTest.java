ea2844a58892c7157abfcb032d086448ec49ad98
clo.d <clo.d@kakaocorp.com>
2022-01-12 06:35:28 +0900

    1단계 구현 - UserCreateRequest::toEntity 테스트 작성

---
+++ src/test/java/com/kakao/cafe/web/dto/UserCreateRequestTest.java
@@ -0,0 +1,28 @@
+package com.kakao.cafe.web.dto;
+
+import com.kakao.cafe.domain.user.User;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class UserCreateRequestTest {
+
+    @Test
+    public void toEntity_테스트() {
+        //given
+        final String userId = "clo.d";
+        final String password = "1234";
+        final String name = "dongwoon";
+        final String email = "clo.d@kakaocorp.com";
+        final UserCreateRequest dto = new UserCreateRequest(userId, password, name, email);
+
+        //when
+        final User user = dto.toEntity();
+
+        //then
+        assertThat(user.getUserId()).isEqualTo(userId);
+        assertThat(user.getPassword()).isEqualTo(password);
+        assertThat(user.getName()).isEqualTo(name);
+        assertThat(user.getEmail()).isEqualTo(email);
+    }
+}
