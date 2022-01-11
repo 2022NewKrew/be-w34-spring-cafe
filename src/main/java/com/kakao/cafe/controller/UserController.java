74c38585837729743f2a4adac88ccf9196ba5b2d
Hongjeongmin <wjdals0471@naver.com>
2022-01-11 14:15:49 +0900

    UI 수정 회원폼 업데이트폼 수정, qna리스트폼 수정

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.kakao.cafe.controller;
 
 import com.kakao.cafe.user.User;
+import com.kakao.cafe.user.UserDto;
 import com.kakao.cafe.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -36,8 +38,18 @@ public class UserController {
     }
 
     @GetMapping("/create")
-    public String viewQuestionForm() {
+    public String viewUserCreateForm() {
         return "user/form";
+    }
+
+    @GetMapping("/{id}/update")
+    public String viewUserUpdateForm(@PathVariable("id") Long id, Model model) {
+
+        User user = userService.findOne(id);
+        UserDto userDto = new UserDto(user.getId(), user.getUserId(), user.getName(), user.getEmail());
+
+        model.addAttribute("user", userDto);
+        return "user/update_form";
     }
 
 }
---
+++ src/main/java/com/kakao/cafe/user/UserDto.java
@@ -0,0 +1,47 @@
+package com.kakao.cafe.user;
+
+public class UserDto {
+    private Long id;
+    private String userId;
+    private String name;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserDto(Long id, String userId, String name, String email) {
+        this.id = id;
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
