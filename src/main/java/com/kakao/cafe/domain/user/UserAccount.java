966028bd72e838a956b6c71c1cdee8bca20ec4ee
edward.baek <edward.baek@kakaocorp.com>
2022-01-11 14:57:00 +0900

    feat : 아이디를 내부에서 atomic 클래스로 생성하도록 변경

--- src/main/java/com/kakao/cafe/domain/user/UserAccount.java
+++ src/main/java/com/kakao/cafe/domain/user/UserAccount.java
@@ -1,10 +1,17 @@
 package com.kakao.cafe.domain.user;
 
-import org.springframework.cglib.core.Local;
+import lombok.Builder;
+import lombok.ToString;
 
 import java.time.LocalDate;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
 
+@ToString(exclude = {"password"})
+@Builder
 public class UserAccount {
+
+    private static final AtomicLong idGenerator;
 
     private final Long userAccountId;
 
@@ -16,89 +23,23 @@ public class UserAccount {
 
     private final LocalDate createdAt;
 
+    static {
+        idGenerator = new AtomicLong();
+    }
+
     public UserAccount(Long userAccountId, String username, String password, String email, LocalDate createdAt) {
-        this.userAccountId = userAccountId;
+        this.userAccountId = Objects.requireNonNullElseGet(userAccountId, idGenerator::getAndIncrement);
         this.username = username;
         this.password = password;
         this.email = email;
         this.createdAt = createdAt;
     }
 
+    public boolean checkEmail(String email) {
+        return this.email.equals(email);
+    }
+
     public Long getUserAccountId() {
         return userAccountId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public LocalDate getCreatedAt() {
-        return createdAt;
-    }
-
-    @Override
-    public String toString() {
-        return "UserAccount{" +
-                "userAccountId=" + userAccountId +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
-                ", createdAt=" + createdAt +
-                '}';
-    }
-
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    public static class Builder {
-
-        private Long builderUserAccountId;
-        private String builderUsername;
-        private String builderPassword;
-        private String builderEmail;
-        private LocalDate builderCreatedAt;
-
-        public UserAccount build() {
-            return new UserAccount(
-                    builderUserAccountId,
-                    builderUsername,
-                    builderPassword,
-                    builderEmail,
-                    builderCreatedAt);
-        }
-
-        public Builder userAccountId(Long id) {
-            this.builderUserAccountId = id;
-            return this;
-        }
-
-        public Builder username(String username) {
-            this.builderUsername = username;
-            return this;
-        }
-
-        public Builder password(String password) {
-            this.builderPassword = password;
-            return this;
-        }
-
-        public Builder email(String email) {
-            this.builderEmail = email;
-            return this;
-        }
-
-        public Builder createdAt(LocalDate createdAt) {
-            this.builderCreatedAt = createdAt;
-            return this;
-        }
     }
 }
