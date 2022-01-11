6ad547326e00ca6205b9fd607f73b0a06bfcd815
pride.chicken <pride.chicken@kakaocorp.com>
2022-01-11 18:29:16 +0900

    [feature] 유저 리스트, 유저 프로필 구현
    - 유저들의 리스트를 받아 띄우는 메소드 구현
    - userId에 맞는 프로필을 띄우는 메소드 구현

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -7,7 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -17,7 +20,7 @@ public class UserController {
     @PostMapping("/user")
     public String signUp(User user) {
         UserList.addUserList(user);
-        logger.info("user create : {}", user);
+        logger.info("user create : {}", user.getUserId());
         return "redirect:/user/list";
     }
 
@@ -25,4 +28,23 @@ public class UserController {
     public String openSignUp() {
         return "/user/form";
     }
+
+    @GetMapping("/user/list")
+    public String getUserList(Model model) {
+        List<User> users = UserList.getUsers();
+        logger.info("user list : {}", users);
+        model.addAttribute("users", users);
+
+        return "/user/list";
+    }
+
+    @GetMapping("/user/{userId}")
+    public String getUserProfile(@PathVariable String userId, Model model) {
+        User user = UserList.getUserByUserId(userId);
+
+        logger.info("get user profile : {}", user.getUserId());
+        model.addAttribute("user", UserList.getUserByUserId(userId));
+        return "/user/profile";
+    }
+
 }
