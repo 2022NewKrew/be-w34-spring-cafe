ec13c7cf1088bd9b0af961d1c5fbb82868db3014
jb.jang <jb.jang@kakaocorp.com>
2022-01-11 10:28:50 +0900

    feat: 회원 기능 추가

---
+++ src/main/java/com/kakao/cafe/service/UserService.java
@@ -0,0 +1,47 @@
+package com.kakao.cafe.service;
+
+import com.kakao.cafe.domain.user.User;
+import com.kakao.cafe.web.dto.UserCreateRequestDto;
+import com.kakao.cafe.web.dto.UserListResponseDto;
+import com.kakao.cafe.web.dto.UserProfileResponseDto;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private static final List<User> userList = new ArrayList<>();
+    private static long idNumber = 0L;
+
+    public void create(UserCreateRequestDto requestDto) {
+        User user = new User(++idNumber, requestDto.getUserId(), requestDto.getPassword(), requestDto.getName(), requestDto.getEmail());
+        userList.add(user);
+    }
+
+    public List<UserListResponseDto> findAll() {
+        List<UserListResponseDto> responseDtoList = new ArrayList<>();
+        for (User user : userList) {
+            UserListResponseDto responseDto = new UserListResponseDto();
+            responseDto.setId(user.getId());
+            responseDto.setUserId(user.getUserId());
+            responseDto.setName(user.getName());
+            responseDto.setEmail(user.getEmail());
+            responseDtoList.add(responseDto);
+        }
+        return responseDtoList;
+    }
+
+    public UserProfileResponseDto findByUserId(String userId) {
+        UserProfileResponseDto responseDto = new UserProfileResponseDto();
+        Optional<User> foundUser = userList.stream().filter(user -> user.getUserId().equals(userId)).findFirst();
+        if (foundUser.isPresent()) {
+            responseDto.setUserId(foundUser.get().getUserId());
+            responseDto.setEmail(foundUser.get().getEmail());
+            return responseDto;
+        }
+        return null;
+    }
+}
