7fefa7472c9438b51cdfa54f8b2556e4a867219a
edward.baek <edward.baek@kakaocorp.com>
2022-01-11 21:59:56 +0900

    feat : QuestionPost 엔티티 작성

---
+++ src/main/java/com/kakao/cafe/domain/post/QuestionPost.java
@@ -0,0 +1,60 @@
+package com.kakao.cafe.domain.post;
+
+import com.kakao.cafe.application.dto.result.QuestionPostDetailResult;
+import lombok.Builder;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+
+@ToString
+@Builder
+public class QuestionPost {
+
+    private static AtomicLong idGenerator;
+
+    private final Long questionPostId;
+    private final String title;
+    private final String content;
+    private final LocalDateTime createdAt;
+    private int viewCount;
+    private final Long userAccountId;
+
+    static {
+        idGenerator = new AtomicLong();
+    }
+
+    public QuestionPost(Long questionPostId, String title, String content, LocalDateTime createdAt, int viewCount, Long userAccountId) {
+        this.questionPostId = Objects.requireNonNullElseGet(questionPostId, idGenerator::getAndIncrement);
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.viewCount = viewCount;
+        this.userAccountId = userAccountId;
+    }
+
+    public Long getQuestionPostId() {
+        return this.questionPostId;
+    }
+
+    public Long getUserAccountId() {
+        return this.userAccountId;
+    }
+
+    public void viewCountIncrease() {
+        this.viewCount++;
+    }
+
+    public QuestionPostDetailResult toResult(String author) {
+        return new QuestionPostDetailResult(
+                questionPostId,
+                title,
+                content,
+                createdAt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                viewCount,
+                author
+        );
+    }
+}
