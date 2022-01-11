7cd7e8d43f5342735b5d9d04142cdf055f78f86c
Jungyu Choi <devwithpug@kakao.com>
2022-01-11 13:35:30 +0900

    step-2: 게시글 작성, 게시글 목록 구현

---
+++ src/main/java/com/kakao/cafe/controller/MainController.java
@@ -0,0 +1,29 @@
+package com.kakao.cafe.controller;
+
+import com.kakao.cafe.domain.article.ArticleDto;
+import com.kakao.cafe.service.ArticleService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    private final ArticleService articleService;
+
+    public MainController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        List<ArticleDto> articles = articleService.getArticles();
+        model.addAttribute("articles", articles);
+
+        return "index";
+    }
+}
