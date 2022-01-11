468e64eacdaf1f9676378392f05b42f9ecf0e09d
Jungyu Choi <devwithpug@kakao.com>
2022-01-11 16:59:52 +0900

    refactor: ArticleDto 생성자의 접근제어자 public -> protected 변경

--- src/main/java/com/kakao/cafe/domain/article/ArticleDto.java
+++ src/main/java/com/kakao/cafe/domain/article/ArticleDto.java
@@ -6,7 +6,7 @@ public class ArticleDto {
     private final String title;
     private final String description;
 
-    public ArticleDto(Long id, String title, String description) {
+    protected ArticleDto(Long id, String title, String description) {
         this.id = id;
         this.title = title;
         this.description = description;
