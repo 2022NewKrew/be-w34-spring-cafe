4f793e0a2e2f37f1146f9bad03bf33b2538efb01
edward.baek <edward.baek@kakaocorp.com>
2022-01-11 21:58:09 +0900

    chore : restassured 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.rest-assured:rest-assured:4.4.0")
+	testImplementation("io.rest-assured:spring-mock-mvc:4.4.0")
 	testCompileOnly("org.projectlombok:lombok:1.18.22")
 	testAnnotationProcessor("org.projectlombok:lombok:1.18.22")
 }
