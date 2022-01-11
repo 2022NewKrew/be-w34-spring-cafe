c62607ded388c91025af6dc19fc4ad0df93b2a90
chiveloper <wodlr2007@naver.com>
2022-01-11 22:10:41 +0900

    [Pride] 스프링 카페 2단계 구현 (#113)
    * [feature] templates로 이동 및 html 중복 처리
    
    - form, list, profile html 파일들을 templates으로 이동함
    - 중복되는 html 코드가 있어 이 부분을 footer, header, navigationBar로 중복 방지
    
    * [feature] User, UserList를 domain으로 이동 & lombok 이용
    
    - model보다는 domain이 맞을 것 같아 domain으로 이동함
    - getter 이용을 위한 lombok 사용
    
    * [refactor] 요구사항에 맞는 url 변경, 메소드 이름 변경
    
    - 요구사항에 맞도록 url을 변경
    
    * [feature] 유저 리스트, 유저 프로필 구현
    
    - 유저들의 리스트를 받아 띄우는 메소드 구현
    - userId에 맞는 프로필을 띄우는 메소드 구현
    
    Co-authored-by: pride.chicken <pride.chicken@kakaocorp.com>

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -1,33 +1,50 @@
 package com.kakao.cafe.controller;
 
-import com.kakao.cafe.model.User;
-import com.kakao.cafe.model.UserList;
+import com.kakao.cafe.domain.User;
+import com.kakao.cafe.domain.UserList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @PostMapping("/user/create")
+    @PostMapping("/user")
     public String signUp(User user) {
         UserList.addUserList(user);
         logger.info("user create : {}", user.getUserId());
-        return "redirect:/user";
-    }
-
-    @GetMapping("/user")
-    public String getUserList(){
-        return "/user/list";
+        return "redirect:/user/list";
     }
 
     @GetMapping("/user/form")
-    public String signUp(Model model){
+    public String openSignUp() {
         return "/user/form";
     }
+
+    @GetMapping("/user/list")
+    public String getUserList(Model model) {
+        List<User> users = UserList.getUsers();
+        logger.info("user list : {}", users);
+        model.addAttribute("users", users);
+
+        return "/user/list";
+    }
+
+    @GetMapping("/user/{userId}")
+    public String getUserProfile(@PathVariable String userId, Model model) {
+        User user = UserList.getUserByUserId(userId);
+
+        logger.info("get user profile : {}", user.getUserId());
+        model.addAttribute("user", UserList.getUserByUserId(userId));
+        return "/user/profile";
+    }
+
 }
