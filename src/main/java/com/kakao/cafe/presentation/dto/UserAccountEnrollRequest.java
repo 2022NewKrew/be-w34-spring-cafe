396ad4de2d5099578b376cdcef7dab14a0014b67
edward.baek <edward.baek@kakaocorp.com>
2022-01-11 14:55:27 +0900

    feat : dto에 @Data 어노테이션 추가

---
+++ src/main/java/com/kakao/cafe/presentation/dto/UserAccountEnrollRequest.java
@@ -0,0 +1,26 @@
+package com.kakao.cafe.presentation.dto;
+
+import com.kakao.cafe.application.dto.UserAccountEnrollCommand;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class UserAccountEnrollRequest {
+
+    @Email
+    private final String email;
+    @NotNull
+    private final String username;
+    @NotNull
+    private final String password;
+
+    public UserAccountEnrollCommand toCommand() {
+        return new UserAccountEnrollCommand(
+                password,
+                username,
+                email
+        );
+    }
+}
