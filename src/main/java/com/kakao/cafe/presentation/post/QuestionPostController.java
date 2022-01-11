6e7ad4aa28015147260258e15b253f5e66209981
edward.baek <edward.baek@kakaocorp.com>
2022-01-11 22:00:52 +0900

    feat : QuestionPostController & ApiController 작성 및 테스트

---
+++ src/main/java/com/kakao/cafe/presentation/post/QuestionPostController.java
@@ -0,0 +1,31 @@
+package com.kakao.cafe.presentation.post;
+
+import com.kakao.cafe.application.dto.result.QuestionPostDetailResult;
+import com.kakao.cafe.application.post.QuestionPostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/posts")
+@RequiredArgsConstructor
+public class QuestionPostController {
+
+    private final QuestionPostService questionPostService;
+
+    @GetMapping("/form")
+    public String writePost() {
+        return "qnaform";
+    }
+
+    @GetMapping("/{id}/detail")
+    public String postDetail(@PathVariable(name = "id") Long id, Model model) {
+        QuestionPostDetailResult postDetail = questionPostService.getPostDetail(id);
+        questionPostService.clickPost(id);
+        model.addAttribute("post", postDetail);
+        return "qnadetail";
+    }
+}
