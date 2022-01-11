f7e0cf334cfe955479b2990ab975cd78d63feee8
SaeGon-Heo <her84562@gmail.com>
2022-01-11 18:13:03 +0900

    import 정리

--- src/main/java/com/kakao/cafe/controller/ArticleController.java
+++ src/main/java/com/kakao/cafe/controller/ArticleController.java
@@ -2,10 +2,8 @@ package com.kakao.cafe.controller;
 
 import com.kakao.cafe.domain.ArticleDto;
 import com.kakao.cafe.service.ArticleService;
-import com.kakao.cafe.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -4,7 +4,6 @@ import com.kakao.cafe.domain.UserDto;
 import com.kakao.cafe.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
