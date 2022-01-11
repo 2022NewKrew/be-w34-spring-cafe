953307d2b9f1341a7223fa51b0e4c92974d06349
edward.baek <edward.baek@kakaocorp.com>
2022-01-11 14:58:23 +0900

    refactor : 패키지 위치 이동, user -> users로 변경, getter사용 줄이기

--- src/main/java/com/kakao/cafe/presentation/user/UserAccountApiController.java
+++ src/main/java/com/kakao/cafe/presentation/user/UserAccountApiController.java
@@ -1,9 +1,10 @@
-package com.kakao.cafe.api;
+package com.kakao.cafe.presentation.user;
 
-import com.kakao.cafe.api.dto.UserAccountEnrollRequest;
-import com.kakao.cafe.api.dto.UserAccountEnrollResponse;
+import com.kakao.cafe.presentation.dto.UserAccountEnrollRequest;
+import com.kakao.cafe.presentation.dto.UserAccountEnrollResponse;
 import com.kakao.cafe.application.user.UserAccountService;
 import com.kakao.cafe.domain.user.UserAccount;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,14 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserAccountApiController {
 
     private final UserAccountService userAccountService;
-
-    public UserAccountApiController(UserAccountService userAccountService) {
-        this.userAccountService = userAccountService;
-    }
 
     @PostMapping("")
     public ResponseEntity<UserAccountEnrollResponse> enroll(@RequestBody UserAccountEnrollRequest request) {
