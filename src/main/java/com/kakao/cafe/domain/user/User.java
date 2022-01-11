ebe29b467c786a646ddda81739b0f82c423cd955
jb.jang <jb.jang@kakaocorp.com>
2022-01-11 10:25:05 +0900

    feat: User 클래스 추가

---
+++ src/main/java/com/kakao/cafe/domain/user/User.java
@@ -0,0 +1,38 @@
+package com.kakao.cafe.domain.user;
+
+public class User {
+
+    private final Long id;
+    private final String userId;
+    private final String password;
+    private final String name;
+    private final String email;
+
+    public User(Long id, String userId, String password, String name, String email) {
+        this.id = id;
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
