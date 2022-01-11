fd0a3304f21d72c1a2f1d0988f20618977699aa3
bryan.ik <bryan.ik@kakaocorp.com>
2022-01-11 17:54:38 +0900

    FEAT : UserRepository 에 Map 추가
    - Key 를 Name, Value 를 UserId 로 갖는 Map 을 사용해 findUserByName() 호출 시 순차 탐색을 하지 않도록 수정

--- src/main/java/com/kakao/cafe/repository/UserRepositoryImpl.java
+++ src/main/java/com/kakao/cafe/repository/UserRepositoryImpl.java
@@ -12,14 +12,17 @@ import org.springframework.stereotype.Repository;
 public class UserRepositoryImpl implements UserRepository {
 
     private final Map<String, User> users;
+    private final Map<String, String> nameToUserId;
 
     public UserRepositoryImpl() {
         this.users = new HashMap<>();
+        this.nameToUserId = new HashMap<>();
     }
 
     @Override
     public void add(User user) {
         users.put(user.getUserId(), user);
+        nameToUserId.put(user.getName(), user.getUserId());
     }
 
     @Override
@@ -35,8 +38,10 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public Optional<User> findUserByName(String name) {
-        return users.values().stream()
-            .filter(user -> user.getName().equals(name))
-            .findFirst();
+        Optional<String> userId = Optional.ofNullable(nameToUserId.get(name));
+        if (userId.isEmpty()) {
+            return Optional.empty();
+        }
+        return findUserByUserId(userId.get());
     }
 }
