b05e3a195bf89df983ad29aed05953a57c79fa6f
myles.nah <myles.nah@kakaocorp.com>
2022-01-11 13:47:31 +0900

    refactor: final 변수로 변경

--- src/main/java/com/kakao/cafe/advice/LoggingAspect.java
+++ src/main/java/com/kakao/cafe/advice/LoggingAspect.java
@@ -7,8 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-
 @Component
 @Aspect
 public class LoggingAspect {
