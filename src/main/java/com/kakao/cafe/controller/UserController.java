b5c4e8690c810fd5471964abf20abd7dd3c0a52a
Seonghun337 <77823678+Seonghun337@users.noreply.github.com>
2022-01-11 22:10:02 +0900

    feat : implement step 1-1 (#78)
    POST /users
    sign up
    
    GET /users
    get user list
    
    GET /users/{id}
    get user profile
    
    Co-authored-by: ethan <ethan.337@kakaocorp.com>

---
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.kakao.cafe.controller;
+
+import com.kakao.cafe.domain.User;
+import com.kakao.cafe.dto.UserDto;
+import com.kakao.cafe.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    private final UserService userService = new UserService();
+
+    @PostMapping("/users")
+    public String addUser(@ModelAttribute UserDto userDto) {
+        logger.info("POST /user {}", userDto);
+        userService.addUser(userDto);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String findAll(Model model) {
+        logger.info("GET /users");
+        model.addAttribute("users", userService.findAll());
+        model.addAttribute("countOfUser", userService.getCountOfUser());
+        return "user/list";
+    }
+
+    @GetMapping("/users/{id}")
+    public String findById(@PathVariable int id , Model model) {
+        logger.info("GET /users/{}", id);
+        model.addAttribute("user",userService.findById(id));
+        return "user/profile";
+    }
+
+
+}
