7366fc326bc7c15d2d31ecb0b39de6d956180adf
kenny.park <kenny.park@kakaocorp.com>
2022-01-11 16:36:16 +0900

    Question클래스에 createdDateTime속성 추가 및 View에 반영

--- src/main/java/com/kakao/cafe/domain/question/Question.java
+++ src/main/java/com/kakao/cafe/domain/question/Question.java
@@ -3,17 +3,21 @@ package com.kakao.cafe.domain.question;
 import com.kakao.cafe.dto.QuestionSaveDto;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class Question {
     private int id;
     private String title;
     private String writer;
     private String contents;
+    private LocalDateTime createdDateTime;
 
     public Question(int id, QuestionSaveDto questionSaveDto) {
         this.id = id;
         this.title = questionSaveDto.getTitle();
         this.writer = questionSaveDto.getWriter();
         this.contents = questionSaveDto.getContents();
+        this.createdDateTime =  LocalDateTime.now();
     }
 }
