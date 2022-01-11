006bb6da7575c4b4d35e2fad227e3b6ac3da7127
reynold-yoon <reynold.yoon@kakaocorp.com>
2022-01-11 17:59:32 +0900

    프론트 수정 & 회원 프로필 조회 기능 추가 & HTML 중복 제거

--- src/main/java/com/kakao/cafe/web/controller/UserController.java
+++ src/main/java/com/kakao/cafe/web/controller/UserController.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -21,12 +22,13 @@ public class UserController {
 
     @GetMapping("/user/form")
     public String form() {
+        logger.info("GET /user/form: response user form page");
         return "user/form";
     }
 
     @PostMapping("/users")
     public String createUser(User user) {
-        logger.info("POST /users: {}", user);
+        logger.info("POST /users: request {}", user);
         // user 생성
         user.setId(++sequence);
         users.add(user);
@@ -35,10 +37,19 @@ public class UserController {
 
     @GetMapping("/users")
     public String showUsers(Model model) {
-        logger.info("GET /users: {}", users);
+        logger.info("GET /users: response user list page with {}", users);
         // users 조회
         model.addAttribute("users", users);
         return "user/list";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String showUser(Model model, @PathVariable String userId) {
+        User user = users.stream().filter(obj -> userId.equals(obj.getUserId())).findAny().orElse(null);
+        logger.info("GET /users/{}: response user profile page with {}", userId, user);
+        // user 조회
+        model.addAttribute("user", user);
+        return "user/profile";
     }
 
 }
