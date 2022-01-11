0ab881a1a13eed17c59b0aed24f873a80c726233
roger.lee <2wjdwo97@gmail.com>
2022-01-11 11:38:30 +0900

    Create Post domain
    Create Post class
    Create List of Post class in RogerDB class

---
+++ src/main/java/com/kakao/cafe/domain/Post.java
@@ -0,0 +1,70 @@
+package com.kakao.cafe.domain;
+
+import java.time.LocalDate;
+
+public class Post {
+    // 다음 생성하는 row 의 id 값을 저장하고 있는 변수
+    private static int postSeq = 1;
+    private int id;
+    private String title;
+    private String content;
+    private LocalDate createdAt;
+    private User writer;
+    private int viewCount;
+
+    public Post() {
+    }
+
+    public Post(int id, String title, String content, LocalDate createdAt, User writer, int viewCount) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.writer = writer;
+        this.viewCount = viewCount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public User getWriter() {
+        return writer;
+    }
+
+    public void setWriter(User writer) {
+        this.writer = writer;
+    }
+
+    public int getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(int viewCount) {
+        this.viewCount = viewCount;
+    }
+}
