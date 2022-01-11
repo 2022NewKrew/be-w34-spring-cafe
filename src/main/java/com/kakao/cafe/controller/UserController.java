e73ddba5fbbb3582dfe8c714f1f10794e121a5c7
tony.ahn <tony.ahn@kakaocorp.com>
2022-01-11 22:41:59 +0900

    중복 html 처리

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -34,4 +35,5 @@ public class UserController {
         model.addAttribute("user", user);
         return "/user/profile";
     }
+
 }
