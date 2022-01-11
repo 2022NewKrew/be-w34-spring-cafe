456747a84febc7ffdbd0e65b1a3ce34886866fda
edward.baek <edward.baek@kakaocorp.com>
2022-01-11 14:54:13 +0900

    chore : groovy -> kotlin으로 변경, 의존성 추가
    - validation 의존성
    - lombok 의존성

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+	id("org.springframework.boot") version "2.6.2"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("java")
+}
+
+group = "com.kakao"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	compileOnly("org.projectlombok:lombok:1.18.22")
+	annotationProcessor("org.projectlombok:lombok:1.18.22")
+	implementation("org.springframework.boot:spring-boot-starter-mustache")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testCompileOnly("org.projectlombok:lombok:1.18.22")
+	testAnnotationProcessor("org.projectlombok:lombok:1.18.22")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
