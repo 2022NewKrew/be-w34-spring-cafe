66fcbc4325a67bb27ea5ef4359eb2d610288027e
pi.raspberry <pi.raspberry@kakaocorp.com>
2022-01-11 17:53:11 +0900

    Implement user listing
    - 회원목록 기능을 구현함.
    - list.html을 수정하여 회원 목록을 출력하도록 수정함.

--- src/main/java/com/kakao/cafe/UserController.java
+++ src/main/java/com/kakao/cafe/UserController.java
@@ -5,11 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Controller
 public class UserController {
@@ -20,6 +18,21 @@ public class UserController {
     public String postUserCreate(String userId, String password, String name, String email) {
         logger.info("[postUserCreate] userId = {}, password = {}, name = {}, email = {}", userId, password, name, email);
         users.add(new User(userId, password, name, email));
-        return "hello";
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String getUsers(Model model) {
+        logger.info("[getUsers]");
+        List<Map<String, String>> userList = new ArrayList<>();
+        for(int i = 0; i < users.size(); i++) {
+            userList.add(Map.of("index", Integer.toString(i+1),
+                    "userId", users.get(i).getUserId(),
+                    "name", users.get(i).getName(),
+                    "email", users.get(i).getEmail())
+                    );
+        }
+        model.addAttribute("users", userList);
+        return "user/list";
     }
 }
