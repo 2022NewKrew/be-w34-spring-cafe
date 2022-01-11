de858320dc50b5d0483948fa26f74b595d736d12
jh Lim <jhwlim25@gmail.com>
2022-01-11 14:31:47 +0900

    feat : 회원가입 유효성 검사 추가 및 에러 발생시 리다이렉트 처리

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -1,18 +1,42 @@
 package com.kakao.cafe.controller;
 
+import com.kakao.cafe.model.user.UserSignupRequest;
+import com.kakao.cafe.service.user.UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Slf4j
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
 
     @PostMapping
-    public String signup() {
+    public String signup(@Valid UserSignupRequest user, Errors errors, RedirectAttributes rttr) {
+        try {
+            validateParams(errors);
+            userService.signupUser(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+            rttr.addFlashAttribute("msg", e.getMessage());
+            return "redirect:/signup";
+        }
+        rttr.addFlashAttribute("msg", "회원가입에 성공하였습니다.");
         return "redirect:/";
     }
 
+    private void validateParams(Errors errors) {
+        if (errors.hasErrors()) {
+            errors.getAllErrors().forEach(error -> log.warn("{}", error.getDefaultMessage()));
+            throw new IllegalArgumentException("형식에 맞지 않는 입력 값입니다.");
+        }
+    }
 }
--- src/main/java/com/kakao/cafe/model/user/UserSignupRequest.java
+++ src/main/java/com/kakao/cafe/model/user/UserSignupRequest.java
@@ -2,12 +2,19 @@ package com.kakao.cafe.model.user;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 @ToString
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserSignupRequest {
+    @NotEmpty(message = "이메일은 필수 입력 값 입니다.")
+    @Email(message = "이메일이 형식이 맞지 않습니다.")
     private String email;
+    @NotEmpty(message = "닉네임은 필수 입력 값 입니다.")
     private String nickname;
+    @NotEmpty(message = "비밀번호는 필수 입력 값 입니다.")
     private String password;
 
     @Builder
