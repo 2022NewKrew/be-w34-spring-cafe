40fd02ff166df392812cae8ce218cade9297add6
jun.dev <jun.dev@kakaocorp.com>
2022-01-11 10:52:05 +0900

    feat: article클래스에 date변수 추가

--- src/main/java/com/kakao/cafe/domain/Article.java
+++ src/main/java/com/kakao/cafe/domain/Article.java
@@ -1,14 +1,19 @@
 package com.kakao.cafe.domain;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Article {
     private String name; //User의 name과 동일
     private String title;
     private String contents;
+    private Date date;
 
     public Article(String writer, String title, String contents) {
         this.name = writer;
         this.title = title;
         this.contents = contents;
+        this.date = new Date();
     }
 
     public String getName() {
@@ -21,5 +26,9 @@ public class Article {
 
     public String getContents() {
         return contents;
+    }
+
+    public String getDate(){
+        return date.toString();
     }
 }
