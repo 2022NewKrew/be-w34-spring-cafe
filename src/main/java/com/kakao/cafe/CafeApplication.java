94fc468ed4b871175dfd490cfd5a5ac6aae79f77
vince.joe <vince.joe@kakaocorp.com>
2022-01-11 18:19:50 +0900

    chore: 안쓰는 코드 제거

--- src/main/java/com/kakao/cafe/CafeApplication.java
+++ src/main/java/com/kakao/cafe/CafeApplication.java
@@ -1,22 +1,11 @@
 package com.kakao.cafe;
 
-import com.kakao.cafe.domain.user.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootApplication
 public class CafeApplication {
-	public static List<User> users = new ArrayList<>();
-
 	public static void main(String[] args) {
-		// for test
-		users.add(new User("user1", "nickname1", "pass1"));
-		users.add(new User("user2", "nickname2", "pass2"));
-		users.add(new User("user3", "nickname3", "pass3"));
-
 		SpringApplication.run(CafeApplication.class, args);
 	}
 
