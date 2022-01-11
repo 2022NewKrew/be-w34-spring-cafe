eb3491ed4c7c0cdacfc5621ea5fbb605075b56b7
JuyeoungJun <wjfhs01@gmail.com>
2022-01-11 11:10:14 +0900

    chore: mustache 관련 properties 분리

--- src/main/java/com/kakao/cafe/CafeApplication.java
+++ src/main/java/com/kakao/cafe/CafeApplication.java
@@ -2,8 +2,10 @@ package com.kakao.cafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = {"classpath:mustache.properties"})
 public class CafeApplication {
 
 	public static void main(String[] args) {
