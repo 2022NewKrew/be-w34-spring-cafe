379fe64ded1c5c84d41822e286c9d32ef6147173
kusakina0608 <platina.kusakina@gmail.com>
2022-01-11 14:15:18 +0900

    AccountController 메서드명 변경

--- src/main/java/com/kakao/cafe/controller/AccountController.java
+++ src/main/java/com/kakao/cafe/controller/AccountController.java
@@ -1,9 +1,11 @@
 package com.kakao.cafe.controller;
 
+import com.kakao.cafe.dto.UserDto;
 import com.kakao.cafe.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -14,42 +16,44 @@ public class AccountController {
     private final UserService userService;
 
     @GetMapping("/login")
-    public String login() {
+    public String loginForm() {
         return "user/login";
     }
 
     @PostMapping("/login")
-    public String loginRequest() {
+    public String login() {
         return "redirect:/";
     }
 
     @GetMapping("/signup")
-    public String signup() {
+    public String signupForm() {
         return "user/signup";
     }
 
     @PostMapping("/signup")
-    public String signupRequest() {
+    public String signup(UserDto userDto, Model model) {
+        log.info("[signup] user: " + userDto);
+        model.addAttribute("user", userService.register(userDto));
         return "user/signup_success";
     }
 
     @GetMapping("/mypage/edit")
-    public String editMyAccount() {
+    public String editMyAccountForm() {
         return "todo"; // TODO
     }
 
     @PutMapping("/mypage/edit")
-    public String editMyAccountRequest() {
+    public String editMyAccount() {
         return "todo"; // TODO
     }
 
     @GetMapping("/mypage/delete")
-    public String deleteMyAccount() {
+    public String deleteMyAccountForm() {
         return "todo"; // TODO
     }
 
     @DeleteMapping("/mypage/edit")
-    public String deleteMyAccountRequest() {
+    public String deleteMyAccount() {
         return "todo"; // TODO
     }
 
