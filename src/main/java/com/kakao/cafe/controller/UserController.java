432004d5f32979c752b5390b676c74987f4688b1
ben.ny <ben.ny@kakaocorp.com>
2022-01-11 20:10:41 +0900

    Refactor : @RequestMapping 적용

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -8,18 +8,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
     Logger logger = LoggerFactory.getLogger(UserController.class);
     private final List<User> userList = new ArrayList<>();
 
-    @GetMapping("/user")
+    @GetMapping
     public String getUserList(Model model) {
         logger.info("[GET] 유저 리스트 페이지");
 
@@ -28,7 +30,16 @@ public class UserController {
         return "user/list";
     }
 
-    @GetMapping("/user/{id}")
+    @PostMapping
+    public String signUp(User user) {
+        logger.info("[POST] 회원가입 => 회원 정보 {}", user.toString());
+
+        userList.add(user);
+
+        return "redirect:/user";
+    }
+
+    @GetMapping("/{id}")
     public String profile(@PathVariable Long id, Model model) {
         logger.info("[GET] 회원 프로필 페이지");
 
@@ -39,14 +50,5 @@ public class UserController {
         model.addAttribute("user", findUser);
 
         return "/user/profile";
-    }
-
-    @PostMapping("/user/create")
-    public String signUp(User user) {
-        logger.info("[POST] 회원가입 => 회원 정보 {}", user.toString());
-
-        userList.add(user);
-
-        return "redirect:/user";
     }
 }
