16b9394602fc028e918395c56c6f906f18e64dd6
sukstar76 <sukstar76@gmail.com>
2022-01-11 17:59:00 +0900

    ArticleController

---
+++ src/main/java/com/kakao/cafe/articles/ArticleController.java
@@ -0,0 +1,44 @@
+package com.kakao.cafe.articles;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/articles")
+public class ArticleController {
+    private final ArticleService articleService;
+
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @PostMapping
+    public String createArticle(ArticleRequest articleRequest) {
+        articleService.createArticle(articleRequest);
+
+        return "redirect:/articles";
+    }
+
+    @GetMapping
+    public String getArticles(Model model) {
+        List<ArticleDto> articleDtoList = articleService.getArticles();
+
+        model.addAttribute("articles", articleDtoList);
+
+        return "article/list";
+    }
+
+    @GetMapping("/{id}")
+    public String getArticleById(@PathVariable("id") Long id, Model model) {
+        ArticleDto articleDto = articleService.getArticleById(id);
+        model.addAttribute("article", articleDto);
+
+        return "article/show";
+    }
+}
