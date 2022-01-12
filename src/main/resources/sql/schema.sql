f0344b5f41696d74b1f464a27476c7e0ecf324ad
edward.baek <edward.baek@kakaocorp.com>
2022-01-12 23:12:05 +0900

    chore : db config 파일 생성, schema.sql 정의

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,18 @@
+create table QUESTION_POST (
+    question_post_id bigint auto_increment,
+    title varchar(100),
+    content varchar(100),
+    created_at timestamp,
+    view_count int,
+    user_account_id bigint,
+    primary key (question_post_id)
+);
+
+create table USER_ACCOUNT (
+    user_account_id bigint auto_increment,
+    username varchar(100),
+    password varchar(100),
+    email varchar(100),
+    created_at timestamp,
+    primary key (user_account_id)
+);
