85db4e9de29996830c6c533c1462c51ab7012266
iron.shin <iron.shin@kakaocorp.com>
2022-01-12 13:34:39 +0900

    lombok 라이브러리 추가

--- src/main/java/com/kakao/cafe/config/MvcConfig.java
+++ src/main/java/com/kakao/cafe/config/MvcConfig.java
@@ -3,15 +3,16 @@ package com.kakao.cafe.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig extends WebMvcConfigurerAdapter {
+public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
 
-        registry.addViewController("index.html").setViewName("index");
-        registry.addViewController("user/form.html").setViewName("user/form");
+        registry.addViewController("/user/form.html").setViewName("user/form");
+        registry.addViewController("/user/login.html").setViewName("user/login");
+        registry.addViewController("/post/form.html").setViewName("post/form");
     }
 }
