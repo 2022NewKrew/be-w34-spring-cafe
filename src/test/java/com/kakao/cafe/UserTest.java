817baf32181e8056e4b04b217bfcb595ceb17171
soohunee <soohun96@g.skku.edu>
2022-01-12 17:23:53 +0900

    H2 DB 연결 없이 기능 구현 완료(로그인 기능 x)

--- src/test/java/com/kakao/cafe/UserTest.java
+++ src/test/java/com/kakao/cafe/UserTest.java
@@ -9,9 +9,21 @@ public class UserTest {
     private MockMvc mockMvc;
 
     @Test
+<<<<<<< HEAD
     @DisplayName("GET /users 요청 확인 - 회원 목록 가져오기")
     void getUserTest() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/users"))
+=======
+<<<<<<< HEAD
+    @DisplayName("GET /user 요청 확인 - 회원 목록 가져오기")
+    void getUserTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/user"))
+=======
+    @DisplayName("GET /list 요청 확인 - 회원 목록 가져오기")
+    void userTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/list"))
+>>>>>>> 374a4c5 (H2 DB 연결 없이 기능 구현 완료(로그인 기능 x))
+>>>>>>> 2e1613d (H2 DB 연결 없이 기능 구현 완료(로그인 기능 x))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
