12e4860ec97b37dd7280fc9f7225eed4dd083156
HYEONSEOK1 <gustir929@gmail.com>
2022-01-12 13:53:18 +0900

    - fix : repository map 에서 list 사용하도록 변경

--- src/main/java/com/kakao/cafe/repository/MemoryArticleRepository.java
+++ src/main/java/com/kakao/cafe/repository/MemoryArticleRepository.java
@@ -5,29 +5,34 @@ import com.kakao.cafe.domain.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class MemoryArticleRepository implements ArticleRepository{
 
-    private static final Map<Long, Article> store = new ConcurrentHashMap<>();
-    private static long sequence = 0L;
+    private static final List<Article> store = Collections.synchronizedList(new ArrayList<Article>());
+
+    private static final int FIRST_INDEX = 1;
+
+    private final AtomicLong atomicLong = new AtomicLong(FIRST_INDEX);
     @Override
     public Article save(Article article) {
-        article.setId(++sequence);
-        store.put(article.getId(), article);
+        article.setId(atomicLong.getAndIncrement());
+        store.add(article);
         return article;
     }
 
     @Override
     public List<Article> findAll() {
-        return new ArrayList<>(store.values());
+        return store;
     }
 
     @Override
     public Article findById(Long id) {
-        return store.get(id);
+        return store.get(id.intValue()-1);
     }
 }
--- src/main/java/com/kakao/cafe/repository/MemoryUserRepository.java
+++ src/main/java/com/kakao/cafe/repository/MemoryUserRepository.java
@@ -5,37 +5,42 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class MemoryUserRepository implements UserRepository{
-    private static final Map<Long,User> store = new ConcurrentHashMap<>();
-    private static long sequence = 0L;
+    private static final List<User> store = Collections.synchronizedList(new ArrayList<User>());
+
+    private static final int FIRST_INDEX = 1;
+
+    private final AtomicLong atomicLong = new AtomicLong(FIRST_INDEX);
     @Override
     public User save(User user) {
-        user.setId(++sequence);
-        store.put(user.getId(), user);
+        user.setId(atomicLong.getAndIncrement());
+        store.add(user);
         return user;
     }
 
     @Override
     public List<User> findAll() {
-        return new ArrayList<>(store.values());
+        return store;
     }
 
     @Override
     public Optional<User> findByUserId(String userId) {
-        return store.values().stream()
+        return store.stream()
                 .filter(member -> member.getUserId().equals(userId))
                 .findAny();
     }
 
     @Override
     public Optional<User> findById(Long id) {
-        return Optional.ofNullable(store.get(id));
+        return Optional.ofNullable(store.get(id.intValue() - 1));
     }
 
     @Override
     public void updateUser(Long id, User updateUser) {
-        store.replace(id,updateUser);
+        store.set(id.intValue()-1, updateUser);
     }
 }
