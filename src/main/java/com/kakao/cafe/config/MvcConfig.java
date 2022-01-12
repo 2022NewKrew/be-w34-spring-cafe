41064c221c55cce4aa725dabe7bdbdbbe42bde80
edward.baek <edward.baek@kakaocorp.com>
2022-01-12 15:11:20 +0900

    refactor : WebMvcConfigurer로 단순 get 요청 처리

---
+++ src/main/java/com/kakao/cafe/config/MvcConfig.java
@@ -0,0 +1,19 @@
+package com.kakao.cafe.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        WebMvcConfigurer.super.addViewControllers(registry);
+        registry.addViewController("/posts/form").setViewName("qnaform");
+        registry.addViewController("/users/signup").setViewName("form");
+        registry.addViewController("/users/login").setViewName("loginform");
+        registry.addViewController("/users/success").setViewName("success");
+    }
+}
