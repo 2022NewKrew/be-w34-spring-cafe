8ab53565a211a85cad20c47b620283ac08bdb307
mimseong <parkseongmin98@gmail.com>
2022-01-12 14:17:31 +0900

    [refactor] synchronizedList 사용

--- src/main/java/com/kakao/cafe/repository/UserRepository.java
+++ src/main/java/com/kakao/cafe/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.kakao.cafe.repository;
 
 import com.kakao.cafe.domain.User;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import org.springframework.http.HttpStatus;
@@ -11,7 +12,11 @@ import org.springframework.web.server.ResponseStatusException;
 @Repository
 public class UserRepository {
 
-    private final List<User> userList = new ArrayList<>();
+    private final List<User> userList;
+
+    public UserRepository() {
+        this.userList = Collections.synchronizedList(new ArrayList<>());
+    }
 
     public void save(User user) {
         userList.add(user);
