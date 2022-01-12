3a2c15e8dda7edc09f471b60635c8acd005d1218
charlie.p1 <charlie.p1@kakaocorp.com>
2022-01-12 17:56:14 +0900

    Implement UserController

--- src/main/java/com/kakao/cafe/user/controller/UserController.java
+++ src/main/java/com/kakao/cafe/user/controller/UserController.java
@@ -1,4 +1,37 @@
 package com.kakao.cafe.user.controller;
 
+import com.kakao.cafe.user.dao.UserDao;
+import com.kakao.cafe.user.dto.UserRegistrationDto;
+import com.kakao.cafe.user.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping(value = "/users")
 public class UserController {
+    private final UserDao dao;
+
+    @Autowired
+    public UserController(UserDao dao) {
+        this.dao = dao;
+    }
+
+    @PostMapping
+    public String register(UserRegistrationDto dto) {
+        User user = new User(dto.getUserId(), dto.getPassword(), dto.getName(), dto.getEmail());
+        dao.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        model.addAllAttributes(Map.of("users", dao.fetchAll()));
+        return "user/list";
+    }
 }
