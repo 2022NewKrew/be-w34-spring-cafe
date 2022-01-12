363f5f5eb339f4f4b66548a039a9f4e36c6dc371
edward.baek <edward.baek@kakaocorp.com>
2022-01-12 23:53:06 +0900

    feat : 테스트 순서에 영향을 받지 않기 위해 db 초기화 구문 작성

--- src/test/java/com/kakao/cafe/presentation/QuestionPostApiControllerTest.java
+++ src/test/java/com/kakao/cafe/presentation/QuestionPostApiControllerTest.java
@@ -1,9 +1,12 @@
 package com.kakao.cafe.presentation;
 
 import com.kakao.cafe.presentation.dto.request.QuestionPostWriteRequest;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -13,6 +16,15 @@ public class QuestionPostApiControllerTest {
 
     @LocalServerPort
     int port;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @AfterEach
+    void tearDown() {
+        jdbcTemplate.update("truncate table question_post");
+        jdbcTemplate.update("alter table question_post alter column question_post_id restart with 1");
+    }
 
     @Test
     void writeTest() {
