4f4264d2b7322bbbdd7227275853e48ea2a041a2
pride.chicken <pride.chicken@kakaocorp.com>
2022-01-12 18:50:28 +0900

    [feature] Article 클래스 생성
    - 질문들을 관리하는 Article 클래스를 생성함
    - article은 고유번호인 index, writer, title, time을 갖고, Content 리스트인 contents를 가짐
    - time은 작성된 시간을 String으로 변환해서 설정

---
+++ src/main/java/com/kakao/cafe/domain/article/Article.java
@@ -0,0 +1,44 @@
+package com.kakao.cafe.domain.article;
+
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class Article {
+    private static int index;
+    private String writer;
+    private String title;
+    private List<Content> contents;
+    private String time;
+
+    Article(String writer, String title, String contents) {
+        this.writer = writer;
+        this.title = title;
+        this.contents = createContents(contents);
+        index = ArticleList.getArticleList().size() + 1;
+        time = getCreatedTime();
+    }
+
+    private List<Content> createContents(String contents){
+        return Arrays.stream(contents.split("\n")).map(Content::new).collect(Collectors.toList());
+    }
+
+    private String getCreatedTime() {
+        LocalDate nowDate = LocalDate.now();
+        LocalTime nowTime = LocalTime.now();
+
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return nowDate.format(dateFormatter) + " " + nowTime.format(timeFormatter);
+    }
+
+    public List<Content> getContents(){
+        return contents;
+    }
+}
