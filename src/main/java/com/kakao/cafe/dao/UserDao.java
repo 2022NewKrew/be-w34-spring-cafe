325b768d595d9cee0cf769ff662504d532160227
tony.ahn <tony.ahn@kakaocorp.com>
2022-01-12 16:57:06 +0900

    stream에 findFirst, orElse 추가

--- src/main/java/com/kakao/cafe/dao/UserDao.java
+++ src/main/java/com/kakao/cafe/dao/UserDao.java
@@ -17,17 +17,11 @@ public class UserDao {
     }
 
     public User getUser(String userId) {
-        List<User> resultUsers = users.stream().filter(user -> user.getUserId().equals(userId)).collect(Collectors.toList());
-        if(resultUsers.size() == 0)
-            return null;
-        return resultUsers.get(0);
+        return users.stream().filter(user -> user.getUserId().equals(userId)).findFirst().orElse(null);
     }
 
     public int getIndex(String userId) {
-        List<Integer> resultIndex = users.stream().filter(user -> user.getUserId().equals(userId)).map(user -> users.indexOf(user)).collect(Collectors.toList());
-        if(resultIndex.size() == 0)
-            return -1;
-        return resultIndex.get(0);
+        return users.stream().filter(user -> user.getUserId().equals(userId)).map(user -> users.indexOf(user)).findFirst().orElse(-1);
     }
 
     public void addUser(User user) {
