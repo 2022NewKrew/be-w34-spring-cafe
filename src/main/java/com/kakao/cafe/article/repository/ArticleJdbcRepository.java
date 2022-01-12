08476de9bf30e5713d28a046f94e30a8d04c6a7c
yuchanleeme <yuchanlee.me@gmail.com>
2022-01-12 19:22:59 +0900

    feat: Article RowMapper 생성

--- src/main/java/com/kakao/cafe/article/repository/ArticleJdbcRepository.java
+++ src/main/java/com/kakao/cafe/article/repository/ArticleJdbcRepository.java
@@ -1,9 +1,11 @@
 package com.kakao.cafe.article.repository;
 
 import com.kakao.cafe.article.model.Article;
+import com.kakao.cafe.user.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -33,24 +35,22 @@ public class ArticleJdbcRepository implements ArticleRepository{
     @Override
     public List<Article> findAll(){
         String sql = "select * from article";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> new Article(
-                rs.getLong("id"),
-                rs.getString("author"),
-                rs.getString("title"),
-                rs.getString("contents"),
-                rs.getString("uploadTime")
-        ));
+        return jdbcTemplate.query(sql, articleRowMapper());
     }
 
     @Override
     public Article findOneById(Long id){
         String sql = "select * from article where id=?";
-        return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> new Article(
+        return jdbcTemplate.queryForObject(sql, articleRowMapper(), id);
+    }
+
+    public RowMapper<Article> articleRowMapper() {
+        return (rs, rowNum) -> new Article(
                 rs.getLong("id"),
                 rs.getString("author"),
                 rs.getString("title"),
                 rs.getString("contents"),
                 rs.getString("uploadTime")
-        ),id);
+        );
     }
 }
