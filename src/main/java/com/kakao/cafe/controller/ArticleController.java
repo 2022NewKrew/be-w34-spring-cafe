d137a59351e41c80f6be35bc118fb42d953eaa55
Jungyu Choi <devwithpug@kakao.com>
2022-01-12 11:23:21 +0900

    refactor: mapping url 규칙 통일

--- src/main/java/com/kakao/cafe/controller/ArticleController.java
+++ src/main/java/com/kakao/cafe/controller/ArticleController.java
@@ -9,7 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("articles")
+@RequestMapping("/articles")
 public class ArticleController {
 
     private final ArticleService articleService;
