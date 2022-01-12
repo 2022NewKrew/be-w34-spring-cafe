33b90a64cced7bf167f5dba8d4d0bed7a6cbdb82
JuyeoungJun <wjfhs01@gmail.com>
2022-01-12 16:08:16 +0900

    chore: sql 파일 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+drop table USER_PROFILE if exists;
+drop table QNA if exists;
+
+create table USER_PROFILE
+(
+    id       INT AUTO_INCREMENT,
+    user_id  VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    name     VARCHAR NOT NULL,
+    email    VARCHAR,
+    PRIMARY KEY (id)
+);
+
+create table QNA
+(
+    index    INT AUTO_INCREMENT,
+    writer   VARCHAR NOT NULL,
+    title    VARCHAR NOT NULL,
+    contents VARCHAR NOT NULL,
+    PRIMARY KEY (index)
+);
