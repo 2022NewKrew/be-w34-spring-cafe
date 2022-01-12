6cfef22d031ec4100e1d606a3fca7360e4f5ead5
ben.ny <ben.ny@kakaocorp.com>
2022-01-12 12:04:18 +0900

    Refactor : service, repository, dto 적용

---
+++ src/main/java/com/kakao/cafe/Repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.kakao.cafe.Repository;
+
+import com.kakao.cafe.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class UserRepository {
+    private final List<User> userList = new CopyOnWriteArrayList<>();
+    private final AtomicLong sequenceId = new AtomicLong();
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void add(User user) {
+        user.setId(sequenceId.incrementAndGet());
+        this.userList.add(user);
+    }
+}
