24aea0c656546d2d550decacfc0630b4acdd8172
pride.chicken <pride.chicken@kakaocorp.com>
2022-01-12 18:48:43 +0900

    [feature] domain에 user 패키지 생성
    - user와 article을 분리해서 관리하고 싶어서 패키지 분리함

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.kakao.cafe.controller;
 
-import com.kakao.cafe.domain.User;
-import com.kakao.cafe.domain.UserList;
+import com.kakao.cafe.domain.user.User;
+import com.kakao.cafe.domain.user.UserList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
