623c152ff157ca2fe05a78171f73e3530ac319ab
imeansu <gcnml10@naver.com>
2022-01-12 11:56:45 +0900

    [step1-3] put method 적용

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -8,10 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -59,9 +56,9 @@ public class UserController {
         return "user/updateForm";
     }
 
-    @PostMapping("/{userId}/update")
+    @PutMapping("/{userId}/update")
     public String updateUser(@PathVariable String userId, UserForm userForm){
-        logger.info("POST /{userId}/update");
+        logger.info("PUT /{userId}/update");
         userForm.setUserId(userId);
         userService.updateUser(User.of(userForm));
         return "redirect:/users";
