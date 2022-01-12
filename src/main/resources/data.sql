09497bf93eacaf54e9153899e5849fec7d19aaf3
yuchanleeme <yuchanlee.me@gmail.com>
2022-01-12 18:34:18 +0900

    feat: h2 JDBC 연동
    - h2 관련 gradle, application 설정
    - JDBC 연동 관련 config 파일 작성
    - data.sql, schema.sql 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into ARTICLE(author, title, contents, uploadTime) values ('grimm', '안녕하세요?', '질문드립니다.','2021-01-12');
+insert into ARTICLE(author, title, contents, uploadTime) values ('grimm2', '안녕하세요?', '질문드립니다.','2021-01-12');
