5b42021aa593f0314bebd63b9020691e95b91726
edward.baek <edward.baek@kakaocorp.com>
2022-01-12 23:11:14 +0900

    chore : import 삭제

--- src/main/java/com/kakao/cafe/config/MvcConfig.java
+++ src/main/java/com/kakao/cafe/config/MvcConfig.java
@@ -1,6 +1,5 @@
 package com.kakao.cafe.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
