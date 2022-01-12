f39252b0804cecfc2ab8b8e8de4d9db02fba7b0d
cih468 <cih468@naver,com>
2022-01-12 19:15:03 +0900

    Article 객체에 getter 메소드 추가

--- src/main/java/domain/article/Article.java
+++ src/main/java/domain/article/Article.java
@@ -1,11 +1,50 @@
 package domain.article;
 
+import java.time.LocalDate;
+
 public class Article {
     private final String title;
     private final String content;
+    private final String createUserId;
+    private final String createDate;
+    private final int views;
 
     public Article(String title, String content) {
         this.title = title;
         this.content = content;
+        this.createUserId = "unknown";
+        this.createDate = LocalDate.now().toString();
+        this.views = 0;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getCreateUserId() {
+        return createUserId;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", createUserId='" + createUserId + '\'' +
+                ", createDate='" + createDate + '\'' +
+                ", views=" + views +
+                '}';
     }
 }
