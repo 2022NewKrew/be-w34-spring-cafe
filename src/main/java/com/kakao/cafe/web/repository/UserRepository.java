8add71f477c7e8766b8825730fbefb827a00e3f6
dfjung4254 <rmsghk4254@naver.com>
2022-01-12 20:55:03 +0900

    로그인 접속 기록 update db 스키마 변경 및 로직 변경

--- src/main/java/com/kakao/cafe/web/repository/UserRepository.java
+++ src/main/java/com/kakao/cafe/web/repository/UserRepository.java
@@ -17,7 +17,6 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class UserRepository {
