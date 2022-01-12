1660ddcf7ac1ee24bcf65ea8b87eda0608486f2d
david.dh <david.dh@kakaocorp.com>
2022-01-12 18:36:32 +0900

    회원 정보 수정 어노테이션 PutMapping으로 변경

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -6,10 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -63,7 +60,7 @@ public class UserController {
         return "./user/updateForm";
     }
 
-    @PostMapping("/{userId}/update")
+    @PutMapping("/{userId}/update")
     public String updateUser(@PathVariable String userId, String curPassword, String newPassword, String name, String email) {
         UserDto userDto = new UserDto();
         userDto.setUserId(userId);
