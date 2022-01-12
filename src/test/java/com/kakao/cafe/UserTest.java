89ea199812e229c8021dc7a8b72c0d566cffb8fb
soohunee <soohun96@g.skku.edu>
2022-01-12 17:47:15 +0900

    Test code 수정

--- src/test/java/com/kakao/cafe/UserTest.java
+++ src/test/java/com/kakao/cafe/UserTest.java
@@ -10,10 +10,13 @@ public class UserTest {
 
     @Test
 <<<<<<< HEAD
+<<<<<<< HEAD
     @DisplayName("GET /users 요청 확인 - 회원 목록 가져오기")
     void getUserTest() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/users"))
 =======
+=======
+>>>>>>> 3f9e887 (Test code 수정)
 <<<<<<< HEAD
     @DisplayName("GET /user 요청 확인 - 회원 목록 가져오기")
     void getUserTest() throws Exception {
@@ -23,7 +26,15 @@ public class UserTest {
     void userTest() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/list"))
 >>>>>>> 374a4c5 (H2 DB 연결 없이 기능 구현 완료(로그인 기능 x))
+<<<<<<< HEAD
 >>>>>>> 2e1613d (H2 DB 연결 없이 기능 구현 완료(로그인 기능 x))
+=======
+=======
+    @DisplayName("GET /user 요청 확인 - 회원 목록 가져오기")
+    void getUserTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/user"))
+>>>>>>> 31bc618 (Test code 수정)
+>>>>>>> 3f9e887 (Test code 수정)
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
