b5e65e78f318e8c0ad5489265ad36d3e526c7c0c
tony.ahn <tony.ahn@kakaocorp.com>
2022-01-13 00:59:51 +0900

    Article DAO 구현

--- src/main/java/com/kakao/cafe/dao/ArticleDao.java
+++ src/main/java/com/kakao/cafe/dao/ArticleDao.java
@@ -1,8 +1,10 @@
 package com.kakao.cafe.dao;
 
 import com.kakao.cafe.vo.Article;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,18 +13,36 @@ public class ArticleDao {
 
     private List<Article> articles = new ArrayList<>();
 
+    private JdbcTemplate jdbcTemplate;
+
+    public ArticleDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
     public void addArticle(Article article) {
-        articles.add(article);
+        jdbcTemplate.update("insert into article(writer, title, contents) values(?,?,?)",
+                article.getWriter(), article.getTitle(), article.getContents());
     }
 
     public List<Article> getArticles() {
-        return articles;
+        return jdbcTemplate.query("select * from article",
+                (rs, rowNum) -> new Article(
+                        rs.getString("writer"),
+                        rs.getString("title"),
+                        rs.getString("contents")
+                )
+        );
     }
 
     public Article getArticle(int index) {
-        if(articles.size() >= index)
-            return articles.get(index - 1);
-        return null;
+        return jdbcTemplate.queryForObject("select * from article where id = ?",
+                (rs, rowNum) -> new Article(
+                        rs.getString("writer"),
+                        rs.getString("title"),
+                        rs.getString("contents")
+                ),
+                index
+        );
     }
 
 }
