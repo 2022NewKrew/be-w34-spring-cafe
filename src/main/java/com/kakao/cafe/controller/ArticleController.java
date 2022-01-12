368970ef6e8518a4ec303131a0312fe3434b8e91
cih468 <cih468@naver,com>
2022-01-12 19:20:42 +0900

    index에 article list를 출력하도록 수정.

--- src/main/java/com/kakao/cafe/controller/ArticleController.java
+++ src/main/java/com/kakao/cafe/controller/ArticleController.java
@@ -6,7 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -14,20 +16,40 @@ public class ArticleController {
     private final ArticleService articleService;
     Logger logger = LoggerFactory.getLogger(ArticleController.class);
 
+    @GetMapping(value = "/")
+    public String indexPage() {
+        return "/index";
+    }
+
     @Autowired
     public ArticleController(ArticleService articleService) {
         this.articleService = articleService;
     }
 
-    @GetMapping(value="/post/form.html")
-    public String createArticleForm(){
-        return "/post/form";
+    @GetMapping(value = "/article/form")
+    public String createArticleForm() {
+        return "/article/form";
     }
 
-
-    @PostMapping(value="/article/create")
-    public String postCreateArticle(Article article){
+    @PostMapping(value = "/article/create")
+    public String postCreateArticle(Article article) {
+        logger.info("article:{}", article);
         articleService.getArticleRepository().getArticleList().add(article);
-        return "redirect:/index.html";
+        return "redirect:/index";
+    }
+
+    @GetMapping(value = "/index")
+    public String getArticleList(Model model) {
+        logger.info("articleList:{}", articleService.getArticleRepository().getArticleList().getCopiedArticleList());
+        model.addAttribute("articleListSize", articleService.getArticleRepository().getArticleList().size());
+        model.addAttribute("articleList", articleService.getArticleRepository().getArticleList().getCopiedArticleList());
+        return "/index";
+    }
+
+    @GetMapping(value = "/articles/{index}")
+    public String getArticleShow(@PathVariable int index, Model model) {
+        Article article = articleService.getArticleRepository().getArticleList().getArticleByIndex(index);
+        model.addAttribute("article", article);
+        return "/article/show";
     }
 }
--- src/main/java/domain/article/ArticleList.java
+++ src/main/java/domain/article/ArticleList.java
@@ -7,7 +7,7 @@ public class ArticleList {
 
     private final List<Article> articleList;
 
-    private ArticleList(){
+    private ArticleList() {
         articleList = new ArrayList<>();
     }
 
@@ -15,19 +15,30 @@ public class ArticleList {
         return articleList.size();
     }
 
-    private static class LazyHolder{
-        private static final ArticleList USER_LIST =  new ArticleList();
+    private static class LazyHolder {
+        private static final ArticleList USER_LIST = new ArticleList();
     }
 
-    public static ArticleList getInstance(){
+    public static ArticleList getInstance() {
         return ArticleList.LazyHolder.USER_LIST;
     }
 
-    public List<Article> getUserList(){
-        return articleList;
+    public List<Article> getCopiedArticleList() {
+        return new ArrayList<>(articleList);
     }
 
     public void add(Article article) {
         articleList.add(article);
+    }
+
+    public Article getArticleByIndex(int index) {
+        return articleList.get(index - 1);
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleList{" +
+                "articleList=" + articleList +
+                '}';
     }
 }
