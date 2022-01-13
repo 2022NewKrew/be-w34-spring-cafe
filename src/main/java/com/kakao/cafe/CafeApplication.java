cfada0cda2ac390268ea3d4da0e3c2992afbd63f
YoonRyeol <yunyul3@gmail.com>
2022-01-13 17:57:35 +0900

    2022-01-13 | YR | pull kakao YoonRyeol

--- src/main/java/com/kakao/cafe/CafeApplication.java
+++ src/main/java/com/kakao/cafe/CafeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CafeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CafeApplication.class, args);
 	}
