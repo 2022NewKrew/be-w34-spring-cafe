fea4a604ca4401d051e39f88ed965098503b1a62
mimseong <parkseongmin98@gmail.com>
2022-01-13 17:11:16 +0900

    [feat] InvalidWritePostException 예외 추가

---
+++ src/main/java/com/kakao/cafe/exceptions/InvalidWritePostException.java
@@ -0,0 +1,8 @@
+package com.kakao.cafe.exceptions;
+
+public class InvalidWritePostException extends PostException {
+
+    public InvalidWritePostException(String message) {
+        super(message);
+    }
+}
