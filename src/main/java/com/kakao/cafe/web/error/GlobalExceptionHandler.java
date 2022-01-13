a7aac9bbc6408162105f86e5d8259c59af61a1d6
flip1945 <coghdus1016@naver.com>
2022-01-13 19:11:57 +0900

    refactor: 불필요한 logger 제거

--- src/main/java/com/kakao/cafe/web/error/GlobalExceptionHandler.java
+++ src/main/java/com/kakao/cafe/web/error/GlobalExceptionHandler.java
@@ -1,8 +1,6 @@
-package com.kakao.cafe.web;
+package com.kakao.cafe.web.error;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,12 +9,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Slf4j
 public class GlobalExceptionHandler {
 
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
     @ExceptionHandler(Exception.class)
     public ModelAndView handleGlobalException(Exception e) {
-        logger.info(e.getMessage());
-
         ModelAndView modelAndView = new ModelAndView("/error");
         modelAndView.addObject("errorMessage", e.getMessage());
         return modelAndView;
