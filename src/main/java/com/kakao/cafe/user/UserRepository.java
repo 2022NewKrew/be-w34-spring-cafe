beccf22ed07a5a3c3f1cd22f660b23e0a03a63e1
ato.mung <ato.mung@kakaocorp.com>
2022-01-13 11:46:27 +0900

    refactor: 동시성 관련 피드백 반영

--- src/main/java/com/kakao/cafe/user/UserRepository.java
+++ src/main/java/com/kakao/cafe/user/UserRepository.java
@@ -4,6 +4,7 @@ import com.kakao.cafe.user.domain.User;
 import com.kakao.cafe.user.domain.Users;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Repository;
 
@@ -13,12 +14,12 @@ public class UserRepository {
     private final Map<String, User> users = new ConcurrentHashMap<>();
 
     public void save(User user) {
-        saveValidate(user);
-        users.put(user.getUserId(), user);
+        User beforeUser = users.putIfAbsent(user.getUserId(), user);
+        saveValidate(beforeUser);
     }
 
     private void saveValidate(User user) {
-        if (users.containsKey(user.getUserId())) {
+        if (Objects.nonNull(user)) {
             throw new IllegalArgumentException("중복된 사용자입니다.");
         }
     }
@@ -28,12 +29,13 @@ public class UserRepository {
     }
 
     public User findById(String userId) {
-        validateUser(userId);
-        return users.get(userId);
+        User user = users.get(userId);
+        validateUser(user);
+        return user;
     }
 
-    private void validateUser(String userId) {
-        if (!users.containsKey(userId)) {
+    private void validateUser(User user) {
+        if (Objects.isNull(user)) {
             throw new IllegalArgumentException("존재하지 않는 아이디입니다.");
         }
     }
