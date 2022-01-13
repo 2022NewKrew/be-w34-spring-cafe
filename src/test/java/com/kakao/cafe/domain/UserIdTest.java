575260e0754a7c767d3cf09995369772ee08d659
ChaeHyeon <cohys7@gmail.com>
2022-01-13 14:25:27 +0900

    UserId 테스트 코드 에러 수정

--- src/test/java/com/kakao/cafe/domain/UserIdTest.java
+++ src/test/java/com/kakao/cafe/domain/UserIdTest.java
@@ -14,9 +14,8 @@ class UserIdTest {
     @Test
     @DisplayName("아이디는 NULL일 수 없다.")
     void validateNull() {
-        assertThatThrownBy(() -> {
-            new UserId(null);
-        }).isInstanceOf(UserException.class)
+        assertThatThrownBy(() -> new UserId(null))
+                .isInstanceOf(UserException.class)
                 .hasMessageMatching(ErrorCode.INVALID_NULL_VALUE.getErrorMessage());
     }
 
@@ -24,30 +23,27 @@ class UserIdTest {
     @ParameterizedTest
     @ValueSource(strings = {"abcd", "0123", "ab12", "01 23", "012345678901234567890"})
     void invalidateSize(String testId) {
-        assertThatThrownBy(() -> {
-            new UserId(testId);
-        }).isInstanceOf(UserException.class)
-                .hasMessageMatching(ErrorCode.INVALID_USER_NAME.getErrorMessage());
+        assertThatThrownBy(() -> new UserId(testId))
+                .isInstanceOf(UserException.class)
+                .hasMessageMatching(ErrorCode.INVALID_USER_ID.getErrorMessage());
     }
 
     @DisplayName("아이디는 공백을 포함할 수 없다.")
     @ParameterizedTest
     @ValueSource(strings = {"01 23"})
     void invalidateEmpty(String testId) {
-        assertThatThrownBy(() -> {
-            new UserId(testId);
-        }).isInstanceOf(UserException.class)
-                .hasMessageMatching(ErrorCode.INVALID_USER_NAME.getErrorMessage());
+        assertThatThrownBy(() -> new UserId(testId))
+                .isInstanceOf(UserException.class)
+                .hasMessageMatching(ErrorCode.INVALID_USER_ID.getErrorMessage());
     }
 
     @DisplayName("아이디는 -와 _를 제외한 특수문자를 사용할 수 없다.")
     @ParameterizedTest
     @ValueSource(strings = {"01*23", "1234@", "1234#", "~1234"})
     void invalidateAsterisk(String testId) {
-        assertThatThrownBy(() -> {
-            new UserId(testId);
-        }).isInstanceOf(UserException.class)
-                .hasMessageMatching(ErrorCode.INVALID_USER_NAME.getErrorMessage());
+        assertThatThrownBy(() -> new UserId(testId))
+                .isInstanceOf(UserException.class)
+                .hasMessageMatching(ErrorCode.INVALID_USER_ID.getErrorMessage());
     }
 
     @DisplayName("유효한 아이디 포맷이면 정상 생성한다.")
