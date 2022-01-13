a3f7d8e786cec8180b27d08977b2c5bdef03d928
jun.dev <jun.dev@kakaocorp.com>
2022-01-13 15:06:46 +0900

    feat: articleRepository 를 h2와 연동

--- src/main/java/com/kakao/cafe/article/service/ArticleService.java
+++ src/main/java/com/kakao/cafe/article/service/ArticleService.java
@@ -2,13 +2,21 @@ package com.kakao.cafe.article.service;
 
 import com.kakao.cafe.article.domain.Article;
 import com.kakao.cafe.article.dto.ArticleCreateDTO;
+import com.kakao.cafe.article.repository.ArticleJdbcRepository;
 import com.kakao.cafe.article.repository.ArticleMemoryRepository;
 import com.kakao.cafe.article.repository.ArticleRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ArticleService {
-    private ArticleRepository articleRepository = new ArticleMemoryRepository();
+    //private ArticleRepository articleRepository = new ArticleMemoryRepository();
+    private ArticleRepository articleRepository;
+
+    public ArticleService(ArticleRepository articleRepository){
+        this.articleRepository = articleRepository;
+    }
 
     public void articleCreate(ArticleCreateDTO articleCreateDTO){
         articleRepository.addArticle(articleCreateDTO);
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,2 @@
 insert into USERS (userid, password, name, email) values ('admin', 'admin', 'adminname', 'admin@kakaocorp.com');
-
-
-insert into ARTICLS (name, title, contents, date) values ('first', 'firsttitle', 'firstcontents', CURRENT_TIMESTAMP());
+insert into ARTICLES (name, title, contents, date) values ('first', 'firsttitle', 'firstcontents', CURRENT_TIMESTAMP());
