b9b4d442f606bf84ebbd4f85dd31e00f6b3c5ab9
LeeA0 <lay0711@naver.com>
2022-01-13 20:40:49 +0900

    feat: validation annotation 적용

--- src/main/java/com/kakao/cafe/user/dto/request/SignUpRequest.java
+++ src/main/java/com/kakao/cafe/user/dto/request/SignUpRequest.java
@@ -1,10 +1,19 @@
 package com.kakao.cafe.user.dto.request;
 
 import com.kakao.cafe.user.domain.User;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 public class SignUpRequest {
+    @Email
+    @NotBlank
     private final String email;
+
+    @NotBlank
     private final String nickname;
+
+    @NotNull
     private final String password;
 
     public SignUpRequest(String email, String nickname, String password) {
