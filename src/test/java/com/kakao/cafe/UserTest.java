14724d82993d41b4250b0c637a071ab46cd57a7b
tjddls1124 <tjddls1124@nate.com>
2022-01-14 02:36:18 +0900

    feature: user test 웹 테스트 추가

---
+++ src/test/java/com/kakao/cafe/UserTest.java
@@ -0,0 +1,52 @@
+package com.kakao.cafe;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+public class UserTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("user 정보 표시")
+    void userFormTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/form.html")).
+                andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @DisplayName("user form post Test")
+    void userCreateTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/create")
+                .param("email","justin@kakaocorp.com")
+                .param("userId", "justin123")
+                .param("name","justin")
+                .param("password","1234"))
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/users"));
+    }
+
+    @Test
+    @DisplayName("user List Test")
+    void userListTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/users"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @DisplayName("user create success & detail 확인")
+    void userDetailTest() throws Exception {
+        userCreateTest();
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/justin123"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
