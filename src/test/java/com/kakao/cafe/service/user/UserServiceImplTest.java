dc963b7d49a9efd5a663d9e5d362677c03779839
mylo.l <mylo.l@kakaocorp.com>
2022-01-13 22:14:18 +0900

    Step1-2 서비스 테스트코드 추가

--- src/test/java/com/kakao/cafe/service/user/UserServiceImplTest.java
+++ src/test/java/com/kakao/cafe/service/user/UserServiceImplTest.java
@@ -7,13 +7,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+
 class UserServiceImplTest {
 
     MemoryUserRepository memoryUserRepository;
