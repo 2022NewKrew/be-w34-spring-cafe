1e93aa5bb0d88fd12e7df3c7601c0e7b1357f0e7
mimseong <parkseongmin98@gmail.com>
2022-01-13 16:14:23 +0900

    [test] UserSignupRequest 테스트 추가

---
+++ src/test/java/com/kakao/cafe/domain/UserSignupRequestTest.java
@@ -0,0 +1,30 @@
+package com.kakao.cafe.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserSignupRequestTest {
+
+    private final String userId = "userId";
+    private final String password = "password";
+    private final String name = "name";
+    private final String email = "email";
+
+    @Test
+    @DisplayName("[성공] UserSignupRequest 객체를 생성한다")
+    void UserSignupRequest() {
+        new UserSignupRequest(userId, password, name, email);
+    }
+
+    @Test
+    @DisplayName("[성공] Entitty로 올바르게 변환한다")
+    void toEntity() {
+        UserSignupRequest userSignupRequest = new UserSignupRequest(userId, password, name, email);
+        User user_Answer = new User(userId, password, name, email);
+
+        User user = userSignupRequest.toEntity();
+
+        Assertions.assertEquals(user, user_Answer);
+    }
+}
