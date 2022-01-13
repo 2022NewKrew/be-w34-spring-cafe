be5b66e7e789b2c25815de72f4aaf97800192d65
mimseong <parkseongmin98@gmail.com>
2022-01-13 17:09:03 +0900

    [delete] @Size 제거

--- src/main/java/com/kakao/cafe/domain/WritePostRequest.java
+++ src/main/java/com/kakao/cafe/domain/WritePostRequest.java
@@ -1,17 +1,13 @@
 package com.kakao.cafe.domain;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 public class WritePostRequest {
     @NotBlank
-    @Size(min = 5, max = 10)
     private final String userId;
     @NotBlank
-    @Size(min = 1, max = 100)
     private final String title;
-    @NotNull
+    @NotBlank
     private final String content;
 
     public WritePostRequest(String userId, String title, String content) {
