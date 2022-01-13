f07236f242c90fec58b73fb90271b5319d17172e
Dongchan KIM <fromecha@gmail.com>
2022-01-13 18:31:23 +0900

    [test] mockito mockmvc 테스트 해보려했으나 실패

--- src/test/java/com/kakao/cafe/controller/UserControllerTest.java
+++ src/test/java/com/kakao/cafe/controller/UserControllerTest.java
@@ -1,55 +1,61 @@
-//package com.kakao.cafe.controller;
-//
-//import com.kakao.cafe.service.UserService;
-//import com.kakao.cafe.vo.User;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//import java.util.List;
-//
-//import static org.mockito.BDDMockito.given;
-//import static org.mockito.BDDMockito.then;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.times;
-//
-//@ExtendWith(MockitoExtension.class)
-//class UserControllerTest {
-//
-//    @Mock
-//    private UserService userService;
-//
-//    @InjectMocks
-//    private UserController userController;
-//
-//    private ModelAndView mav;
-//
-//    @BeforeEach
-//    void setUp() {
-//        mav = mock(ModelAndView.class);
-//    }
-//
-//    @Test
-//    @DisplayName("유저 리스트 반환 -> 정상")
-//    void userList() {
-//        // given
-////        given(userService.getUsers()).willReturn(List<User>);
-//
-//    }
-//
-//    @Test
-//    void createUser() {
-//        // given
-//        User user = new User("ABC", "1234", "test@naver.com");
-////        given(userService.join(user)).willReturn(void);
-//    }
-//
-//    @Test
-//    void user() {
-//    }
-//}
+package com.kakao.cafe.controller;
+
+import com.kakao.cafe.dto.UserRegistrationDto;
+import com.kakao.cafe.entity.User;
+import com.kakao.cafe.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.mockito.BDDMockito.given;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class UserControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserService userService;
+
+    private List<User> users;
+
+    @InjectMocks
+    private UserController userController;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+        this.users = new ArrayList<>();
+        this.users.add(new User("abc", "1234", "ab@naver.com"));
+        this.users.add(new User("abc2", "12342", "ab2@naver.com"));
+    }
+
+    @Test
+    @DisplayName("유저 리스트 반환 -> 정상")
+    void userList() throws Exception {
+//        given(userController.userList()).willReturn(users);
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/users")
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void createUser() throws Exception{
+    }
+
+    @Test
+    void user() {
+    }
+}
