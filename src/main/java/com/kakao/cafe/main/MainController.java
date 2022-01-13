a50235fe5716f328209fe28bb3df86e95db7f22b
WOC <tom558@naver.com>
2022-01-13 12:32:53 +0900

    refactor : change main controller from domain to dto
    MainController에서 Article도메인을 넘겨주는것을 dto로 바꿈

--- src/main/java/com/kakao/cafe/main/MainController.java
+++ src/main/java/com/kakao/cafe/main/MainController.java
@@ -1,13 +1,16 @@
 package com.kakao.cafe.main;
 
 import com.kakao.cafe.article.Article;
+import com.kakao.cafe.article.ArticleDto;
 import com.kakao.cafe.article.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
@@ -21,9 +24,9 @@ public class MainController {
 
     @GetMapping(path = {"/", "/index"})
     public String createUserForm(Model model) {
-        List<Article> articles = articleService.getAllArticles();
+        List<ArticleDto> articles = articleService.getAllArticles().stream().map(ArticleDto::new).collect(Collectors.toList());
         model.addAttribute("articles", articles);
-        return "/index";
+        return "index";
     }
 
 }
