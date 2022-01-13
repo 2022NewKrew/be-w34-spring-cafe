d3c06924789240ac4fbef3b1517799e21626d251
pi.raspberry <pi.raspberry@kakaocorp.com>
2022-01-13 22:10:42 +0900

    Save user data in DB
    - H2 memory DB에 유저 데이터를 저장하고 수정하도록 구현함.
    - userId를 primary key로 사용하여 중복된 userId는 사용이 불가함.
    - userId는 수정이 불가하도록 구현함.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO ARTICLE(writer, title, contents) VALUES ('mywriter', 'mytitle', 'mycontents');
 SELECT id, writer, title, contents FROM ARTICLE;
+INSERT INTO USERS(userid, password, name, email) VALUES ('myuserid', 'mypassword', 'myname', 'myemail@email.com');
+SELECT userid, password, name, email FROM USERS;
