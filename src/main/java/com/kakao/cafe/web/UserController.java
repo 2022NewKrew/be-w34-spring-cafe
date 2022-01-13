c2c70fd971a33d2781d4fff1e05bfe4f8db56c4c
keith.lee <keith.lee@kakaocorp.com>
2022-01-13 17:20:50 +0900

    회원정보 수정을 PUT 메소드로 수정

--- src/main/java/com/kakao/cafe/web/UserController.java
+++ src/main/java/com/kakao/cafe/web/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class UserController {
@@ -49,7 +50,7 @@ public class UserController {
         return "user/updateForm";
     }
 
-    @PostMapping("users/{userId}/update")
+    @PutMapping("users/{userId}/update")
     public String updateUser(@PathVariable String userId, UserModifyCommand umc) {
         userService.modifyUser(userId, umc);
         return "redirect:/users/";
