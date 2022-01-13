3f1980ccaf63e50c76e36948fe88444c961355c9
ethan <ethan.337@kakaocorp.com>
2022-01-13 16:01:00 +0900

    JDBC 연동
    H2 데이터베이스 연동
    schema.sql, data.sql 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO USER(userid, password, name, email, joinedat)
+VALUES ('id_001', 'pw_001', 'james', 'james1212@gmail.com', '2021-12-13'), ('id_002', 'pw_002', 'sam', 'samsam@gmail.com', '2022-01-02');
