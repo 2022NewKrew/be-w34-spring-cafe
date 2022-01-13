b58b6c4d5a7db544323dcd1bb70b266b98a2b493
jh Lim <jhwlim25@gmail.com>
2022-01-13 14:24:10 +0900

    fix : DB users 테이블 초기 데이터에 회원가입 일자 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO
-    users(email, nickname, password)
+    users(email, nickname, password, createdAt)
 VALUES
-    ('test1@test.com', '테스터1', '1234'),
-    ('test2@test.com', '테스터2', '1234'),
-    ('test3@test.com', '테스터3', '1234');
+    ('test1@test.com', '테스터1', '1234', '20210101 13:30:50' ),
+    ('test2@test.com', '테스터2', '1234', '20210102 14:30:50' ),
+    ('test3@test.com', '테스터3', '1234', '20210103 15:30:50' );
