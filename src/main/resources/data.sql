b59f862a6008cc4f3152642874bee0fe5d168a0e
chance.kk <chance.kk@kakaocorp.com>
2022-01-13 15:05:15 +0900

    Created cafe_user, cafe_article tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-INSERT INTO ARTICLE (WRITER, TITLE, CONTENTS) VALUES ('ksc', '삽질킹', '삽질만 합니다.');
+INSERT INTO cafe_user (user_id, password, name, email)
+    VALUES ('mylumiere', 'test1234', '김승찬', 'chance.kk@kakaocorp.com'),
+           ('chance', 'test1234', '승찬', 'matues00@google.com');
+INSERT INTO cafe_article (writer, title, contents)
+    VALUES ((SELECT user_id FROM cafe_user WHERE user_id = 'mylumiere'), '삽질킹을 아시나요?', '저는 삽질만 합니다.');
