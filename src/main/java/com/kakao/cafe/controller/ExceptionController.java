0dd3a40e4c0e58aef6653c1eb7ea077f7c437767
YoonRyeol <yunyul3@gmail.com>
2022-01-13 16:50:57 +0900

    2022-01-13 | YR | ExceptionController stackTrace 제거

--- src/main/java/com/kakao/cafe/controller/ExceptionController.java
+++ src/main/java/com/kakao/cafe/controller/ExceptionController.java
@@ -18,7 +18,6 @@ public class ExceptionController {
     @ExceptionHandler({UserNotFoundException.class, UserDuplicateException.class})
     String userError(HttpServletRequest req, RuntimeException e, Model model) {
         model.addAttribute("e", e);
-        model.addAttribute("stacktrace", e.getStackTrace());
         return "error/error";
     }
 
@@ -26,7 +25,6 @@ public class ExceptionController {
     @ExceptionHandler({PostNotFoundException.class})
     String userNotFound(HttpServletRequest req, RuntimeException e, Model model) {
         model.addAttribute("e", e);
-        model.addAttribute("stacktrace", e.getStackTrace());
         return "error/error";
     }
 
