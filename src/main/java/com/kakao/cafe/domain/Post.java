52b1c387cc0ffe2334b7489ec55a1f349a08f8be
mimseong <parkseongmin98@gmail.com>
2022-01-13 16:38:10 +0900

    [refactor] Post Id DB에서 설정하도록 변경

--- src/main/java/com/kakao/cafe/domain/Post.java
+++ src/main/java/com/kakao/cafe/domain/Post.java
@@ -1,20 +1,38 @@
 package com.kakao.cafe.domain;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 public class Post {
-    private final UUID id;
+    private long id;
     private final String userId;
     private final String title;
     private final String content;
     private final LocalDateTime createdAt;
 
-    public Post(String userId, String title, String content) {
-        this.id = UUID.randomUUID();
+    public Post( String userId, String title, String content) {
         this.userId = userId;
         this.title = title;
         this.content = content;
         this.createdAt = LocalDateTime.now();
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post that = (Post) o;
+        return id == that.id;
     }
 }
