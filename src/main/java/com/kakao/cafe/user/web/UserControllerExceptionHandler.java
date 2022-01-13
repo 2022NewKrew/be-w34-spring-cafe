82090a4114a59cc6df9fde08f41eeee4464c5698
miya.ong <miya.ong@kakaocorp.com>
2022-01-13 23:15:34 +0900

    feat: UserController에 ControllerAdvice 적용

--- src/main/java/com/kakao/cafe/user/web/UserControllerExceptionHandler.java
+++ src/main/java/com/kakao/cafe/user/web/UserControllerExceptionHandler.java
@@ -1,12 +1,16 @@
-/*package com.kakao.cafe.exception;
+package com.kakao.cafe.exception;
 
 import com.kakao.cafe.user.web.UserController;
+import java.security.InvalidParameterException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Slf4j
 @ControllerAdvice(basePackageClasses = UserController.class)
 public class UserControllerExceptionHandler {
 
-
-}*/
+    // @Valid 검증 실패 시
+    @ExceptionHandler(InvalidParameterException.class)
+    protected
+}
