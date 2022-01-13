e8db41a5feea848fd8fc0aa4e5cc38aabaceb79b
Dongchan KIM <fromecha@gmail.com>
2022-01-13 14:23:14 +0900

    security 기본 로그인 통과 설정 추가

--- src/main/java/com/kakao/cafe/config/SecurityConfig.java
+++ src/main/java/com/kakao/cafe/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,6 +11,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
