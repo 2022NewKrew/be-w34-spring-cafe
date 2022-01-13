d73620965bd110e9b4ab86e15c7bd229420c7d59
jb.jang <jb.jang@kakaocorp.com>
2022-01-13 21:06:02 +0900

    feat: JdbcUserRepository 추가

---
+++ src/main/java/com/kakao/cafe/repository/JdbcUserRepository.java
@@ -0,0 +1,51 @@
+package com.kakao.cafe.repository;
+
+import com.kakao.cafe.domain.user.User;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class JdbcUserRepository implements UserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+    private static Long idNumber = 0L;
+
+    public JdbcUserRepository(JdbcTemplate dataSource) {
+        this.jdbcTemplate = dataSource;
+    }
+
+    @Override
+    public Long generateId() {
+        return ++idNumber;
+    }
+
+    @Override
+    public void create(User user) {
+        String sql = "INSERT INTO users VALUES (?, ?, ?, ?, ?)";
+        jdbcTemplate.update(sql, user.getId(), user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
+    }
+
+    @Override
+    public List<User> findAll() {
+        String sql = "SELECT * FROM users";
+        return jdbcTemplate.query(sql, userRowMapper());
+    }
+
+    @Override
+    public User findByUserId(String userId) {
+        String sql = "SELECT * FROM users where userId = ?";
+        try {
+            return jdbcTemplate.queryForObject(sql, userRowMapper(), userId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new RuntimeException("해당하는 사용자 ID가 존재하지 않습니다.");
+        }
+    }
+
+    private RowMapper<User> userRowMapper() {
+        return (rs, rowNum) -> new User(rs.getLong("id"), rs.getString("userId"), rs.getString("password"), rs.getString("name"), rs.getString("email"));
+    }
+}
