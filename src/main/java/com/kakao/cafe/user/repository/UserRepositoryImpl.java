4651a5fd186e2258ef20daebc718fde213237796
LeeA0 <lay0711@naver.com>
2022-01-13 19:26:15 +0900

    feat: List와 Long타입을 동기화 가능한 타입으로 변경

--- src/main/java/com/kakao/cafe/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/kakao/cafe/user/repository/UserRepositoryImpl.java
@@ -2,21 +2,22 @@ package com.kakao.cafe.user.repository;
 
 import com.kakao.cafe.user.domain.User;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository{
-    private static final List<User> users = new ArrayList<>();
-    private static Long idx = 0L;
+    private static final List<User> users = Collections.synchronizedList(new ArrayList<>());
+    private static final AtomicLong idx = new AtomicLong(0);
     static {
         users.add(new User(0L, "aa@aa.com","aaa","aaaa"));
     }
 
     public Long autoIncrement(){
-        idx++;
-        return idx;
+        return idx.incrementAndGet();
     }
 
     public User save(User user) {
