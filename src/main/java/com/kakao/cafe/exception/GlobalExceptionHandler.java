d15a827d72b98d980d731bfc4eed6eba965ef827
anika.dovob <anika.dovob@kakaocorp.com>
2022-01-14 12:00:31 +0900

    feat: error 페이지로의 연결에 상태 코드 추가
    - CustomException이 아니라면 에러 메세지를 사용자에게 그대로 노출하는 것이 부적절하다 생각하여 handlerCustomException만 남김
    - RestControllerAdvice -> ControllerAdvice 변경하여 ModelAndView 사용하지않고 Model로만 처리

--- src/main/java/com/kakao/cafe/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kakao/cafe/exception/GlobalExceptionHandler.java
@@ -1,37 +1,29 @@
 package com.kakao.cafe.exception;
 
-import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.ModelAndView;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
-
-    private static final String ERROR_VIEW = "errors/error";
-    private final ModelAndView mv = new ModelAndView(ERROR_VIEW);
 
     Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(CustomException.class)
-    public ModelAndView handleCustomException(HttpServletRequest request, CustomException e) {
+    public String handleCustomException(CustomException e, HttpServletRequest request, HttpServletResponse response, Model model) {
         logger.error("CustomException status: {}", e.getErrorCode().getStatus());
         logger.error("CustomException errorCode: {}", e.getErrorCode().getErrorCode());
         logger.error("CustomException message: {}", e.getErrorCode().getMessage());
 
-        setModelAndView(request, e.getErrorCode());
+        response.setStatus(e.getErrorCode().getStatus());
 
-        return mv;
-    }
+        model.addAttribute("message", e.getErrorCode().getMessage());
+        model.addAttribute("referer", request.getHeader("referer"));
 
-    private void setModelAndView(HttpServletRequest request, ErrorCode errorCode) {
-        Map<String, Object> model = mv.getModel();
-        ErrorResponse response = new ErrorResponse(errorCode);
-        String referer = request.getHeader("referer");
-        model.put("response", response);
-        model.put("referer", referer);
+        return "errors/error";
     }
 }
