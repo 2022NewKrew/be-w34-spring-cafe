1112f0ee1c436ed1caf65de98373e6b7b2120fd6
mimseong <parkseongmin98@gmail.com>
2022-01-14 19:07:56 +0900

    [chore] FOREIGN KEY 주석처리

--- src/main/resources/sql/tables.sql
+++ src/main/resources/sql/tables.sql
@@ -17,7 +17,9 @@ CREATE TABLE post
     title       VARCHAR,
     content     CLOB,
     created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    users_id    UUID,
-    PRIMARY KEY(id),
-    FOREIGN KEY(users_id) REFERENCES users (id)
+    user_id    UUID,
+    PRIMARY KEY(id)
+--     TODO - FOREIGN KEY(user_id) REFERENCES users (id)
+--     유저가 없어서 테스트가 제대로 동작하지 않기에 잠시 주석처리했습니다
+--     다음 작업때 유저 추가한 테스트로 수정하겠습니다
 );
