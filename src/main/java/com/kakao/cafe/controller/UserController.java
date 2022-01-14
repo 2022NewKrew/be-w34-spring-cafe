9fd95da36622a8606daec3d1848e74b050a2e3be
faust.like <faust.like@kakaocorp.com>
2022-01-14 13:26:11 +0900

    feat: SignUp이 실패 할 경우 Bad Request를 보내도록 수정
    UserController.SignUp 메소드에서 SignUp이 실패할 경우 Bad Request Response를 보내도록 수정
    다만, 이럴 때 정적 페이지의 대응을 구현하지 않음

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -4,15 +4,19 @@ import com.kakao.cafe.DTO.SignInDTO;
 import com.kakao.cafe.domain.UserDB;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
     private static final UserDB userDB = new UserDB();
     Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    String ALREADY_EXISTS_ID_MESSAGE = "This ID is already exists";
 
     @PostMapping("/create")
     public String SignUp(@ModelAttribute SignInDTO userInfo) {
@@ -24,7 +28,7 @@ public class UserController {
         }
 
         logger.debug("SignUp Failed. already exist ID={}", userInfo.getUserId());
-        return "redirect:/user/list";
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ALREADY_EXISTS_ID_MESSAGE);
     }
 
     @GetMapping("/list")
