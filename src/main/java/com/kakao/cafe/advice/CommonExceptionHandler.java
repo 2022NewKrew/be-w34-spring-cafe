5dc4df6f83ff368821e1cdab20e3cd5805e3598e
Kimgeunwook <wook961206@naver.com>
2022-01-14 09:28:20 +0900

    Feature: ExceptionHandler추가

---
+++ src/main/java/com/kakao/cafe/advice/CommonExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.kakao.cafe.advice;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class CommonExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ModelAndView handleArithmeticException(Exception e){
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("errorMessage",e.getMessage());
+        mav.setViewName("/error/error");
+        return mav;
+    }
+}
