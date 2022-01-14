4c577a1f27e74f3cb67065f6093cf85bf5535282
rienkim <rienkim25@gmail.com>
2022-01-14 22:10:10 +0900

    스프링 카페 1-3단계 구현 (#272)
    * Save article to DB
    
    - H2 memory DB에 게시글을 저장하고 조회하도록 구현함.
    
    * Save user data in DB
    
    - H2 memory DB에 유저 데이터를 저장하고 수정하도록 구현함.
    - userId를 primary key로 사용하여 중복된 userId는 사용이 불가함.
    - userId는 수정이 불가하도록 구현함.
    
    Co-authored-by: pi.raspberry <pi.raspberry@kakaocorp.com>

--- src/main/java/com/kakao/cafe/UserController.java
+++ src/main/java/com/kakao/cafe/UserController.java
@@ -8,25 +8,60 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.sql.DataSource;
+import java.sql.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Controller
-public class UserController {
-    Logger logger = LoggerFactory.getLogger(UserController.class);
-    private final List<User> users = new ArrayList<>();
+public class UserController { Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    private final DataSource dataSource;
+
+    public UserController(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    private void insertUser(User user) {
+        final String sql = "INSERT INTO USERS(userid, password, name, email) VALUES (?,?,?,?)";
+        try(Connection conn = dataSource.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, user.getUserId());
+            stmt.setString(2, user.getPassword());
+            stmt.setString(3, user.getName());
+            stmt.setString(4, user.getEmail());
+            stmt.executeUpdate();
+            conn.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     @PostMapping("/user/create")
     public String postUserCreate(String userId, String password, String name, String email) {
         logger.info("[postUserCreate] userId = {}, password = {}, name = {}, email = {}", userId, password, name, email);
-        users.add(new User(userId, password, name, email));
+        insertUser(new User(userId, password, name, email));
         return "redirect:/users";
+    }
+
+    private List<User> selectAllUsers() {
+        List<User> users = new ArrayList<>();
+        try(Connection conn = dataSource.getConnection();
+            Statement stmt = conn.createStatement()) {
+            ResultSet resultSet = stmt.executeQuery("SELECT userid, password, name, email FROM USERS");
+            while(resultSet.next()) {
+                users.add(new User(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     @GetMapping("/users")
     public String getUsers(Model model) {
         logger.info("[getUsers]");
         List<Map<String, String>> userList = new ArrayList<>();
+        List<User> users = selectAllUsers();
         for(int i = 0; i < users.size(); i++) {
             userList.add(Map.of("index", Integer.toString(i+1),
                     "userId", users.get(i).getUserId(),
@@ -38,12 +73,28 @@ public class UserController {
         return "user/list";
     }
 
+    private User findUserById(String userId) {
+        final String sql = "SELECT password, name, email FROM USERS WHERE userid = ?";
+        try(Connection conn = dataSource.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, userId);
+            ResultSet resultSet = stmt.executeQuery();
+            if(resultSet.next()) {
+                return new User(userId, resultSet.getString(1), resultSet.getString(2), resultSet.getString(3));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     @GetMapping("/users/{userId}")
     public String getUserProfile(@PathVariable String userId, Model model) {
         logger.info("[getUserProfile] userId = {}", userId);
-        List<User> matchUsers = users.stream().filter(x -> x.getUserId().equals(userId)).collect(Collectors.toList());
-        logger.info("matchUsers = {}", matchUsers);
-        User matchUser = matchUsers.get(0);
+        User matchUser = findUserById(userId);
+        if(matchUser == null) {
+            return "user/profile";
+        }
         model.addAttribute("name", matchUser.getName());
         model.addAttribute("email", matchUser.getEmail());
         return "user/profile";
@@ -52,7 +103,10 @@ public class UserController {
     @GetMapping("/user/{id}/form")
     public String updateForm(@PathVariable String id, Model model) {
         logger.info("[updateForm] id = {}", id);
-        User selectedUser = users.stream().filter(x -> x.getUserId().equals(id)).collect(Collectors.toList()).get(0);
+        User selectedUser = findUserById(id);
+        if(selectedUser == null) {
+            return "user/updateForm";
+        }
         model.addAttribute("userId", selectedUser.getUserId());
         model.addAttribute("password", selectedUser.getPassword());
         model.addAttribute("name", selectedUser.getName());
@@ -60,20 +114,25 @@ public class UserController {
         return "user/updateForm";
     }
 
-    @PostMapping("/user/{oldUserId}/update")
-    public String updateUserInfo(@PathVariable String oldUserId, String userId, String password, String name, String email) {
-        logger.info("[updateUserInfo] oldUserId = {}, newUserId = {}, password = {}, name = {}, email = {}", oldUserId, userId, password, name, email);
-        for(int i = 0; i < users.size(); i++) {
-            updateMatchedUser(i, oldUserId, new User(userId, password, name, email));
+    private void updateUserById(String userId, String password, String name, String email) {
+        final String sql = "UPDATE USERS SET password=?, name=?, email=? WHERE userid=?";
+        try(Connection conn = dataSource.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, password);
+            stmt.setString(2, name);
+            stmt.setString(3, email);
+            stmt.setString(4, userId);
+            stmt.executeUpdate();
+            conn.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        logger.info("[updateUserInfo] cannot found user - {}", oldUserId);
-        return "redirect:/users";
     }
 
-    private void updateMatchedUser(int i, String oldUserId, User newUser) {
-        if(users.get(i).getUserId().equals(oldUserId)) {
-            logger.info("[updateUserInfo] user info updated");
-            users.set(i, newUser);
-        }
+    @PostMapping("/user/{userId}/update")
+    public String updateUserInfo(@PathVariable String userId, String password, String name, String email) {
+        logger.info("[updateUserInfo] UserId = {}, password = {}, name = {}, email = {}", userId, password, name, email);
+        updateUserById(userId, password, name, email);
+        return "redirect:/users";
     }
 }
