106b88825c8784975c25ffab4470fdaf15012cf8
frank.burger <frank.burger@kakaocorp.com>
2022-01-14 16:23:23 +0900

    fix: Generated Key 읽어오기 작동 오류 수정

--- src/main/java/com/kakao/cafe/user/SpringJdbcUserRepository.java
+++ src/main/java/com/kakao/cafe/user/SpringJdbcUserRepository.java
@@ -21,15 +21,18 @@ public class SpringJdbcUserRepository implements UserRepository {
     public Long add(User user) {
         KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement("INSERT INTO user (email, username, password, type, display_name) VALUES (?, ?, ?, ?, ?)");
+            PreparedStatement ps = connection.prepareStatement(
+                    "INSERT INTO user (email, username, password, type, display_name) VALUES (?, ?, ?, ?, ?)",
+                    PreparedStatement.RETURN_GENERATED_KEYS);
             ps.setString(1, user.getEmail());
             ps.setString(2, user.getUsername());
             ps.setString(3, user.getPassword());
             ps.setString(4, "type");
             ps.setString(5, "display_name");
-            return ps;}, keyHolder);
+            return ps;
+        }, keyHolder);
 
-        return keyHolder.getKeyAs(Long.class);
+        return (Long) keyHolder.getKeys().get("id");
     }
 
     @Override
@@ -49,13 +52,15 @@ public class SpringJdbcUserRepository implements UserRepository {
     }
 
     private RowMapper<User> userRowMapper() {
-        return (rs, rowNum) -> new User(rs.getString("username"), rs.getString("password"), rs.getString("email"));
+        return (rs, rowNum) -> new User(rs.getLong("id"), rs.getString("username"), rs.getString("password"),
+                                        rs.getString("email"));
     }
 
     @Override
     public void update(User user) {
-        jdbcTemplate.update("UPDATE user SET email = ?, username = ?, password = ?, type = ?, display_name = ? WHERE id = ?;",
-                            user.getEmail(), user.getUsername(), user.getPassword(), "type", "display_name");
+        jdbcTemplate.update(
+                "UPDATE user SET email = ?, username = ?, password = ?, type = ?, display_name = ? WHERE id = ?;",
+                user.getEmail(), user.getUsername(), user.getPassword(), "type", "display_name");
     }
 
     @Override
