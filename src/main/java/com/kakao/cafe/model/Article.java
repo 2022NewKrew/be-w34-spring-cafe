bd21e71d092f28179bb6ee6930e475ff7b231bf1
green.day <green.day@kakaocorp.com>
2022-01-14 17:03:26 +0900

    refactor: 게시글 입력정보에 대한 검증을 DTO에서 하도록 변경

--- src/main/java/com/kakao/cafe/model/Article.java
+++ src/main/java/com/kakao/cafe/model/Article.java
@@ -1,15 +1,11 @@
 package com.kakao.cafe.model;
 
-import java.util.Objects;
-
 public class Article {
     private final int id;
     private String title;
     private String content;
 
     public Article(int id, ArticleSaveDTO articleSaveDTO) {
-        validateArticleSaveDTO(articleSaveDTO);
-
         this.id = id;
         this.title = articleSaveDTO.getTitle();
         this.content = articleSaveDTO.getContent();
@@ -25,18 +21,5 @@ public class Article {
 
     public String getContent() {
         return content;
-    }
-
-    private void validateArticleSaveDTO(ArticleSaveDTO articleSaveDTO){
-        validateTitle(articleSaveDTO.getTitle());
-        validateContent(articleSaveDTO.getContent());
-    }
-
-    private void validateTitle(String title){
-        if(Objects.isNull(title)) throw new IllegalArgumentException();
-    }
-
-    private void validateContent(String content){
-        if(Objects.isNull(content)) throw new IllegalArgumentException();
     }
 }
--- src/main/java/com/kakao/cafe/model/ArticleSaveDTO.java
+++ src/main/java/com/kakao/cafe/model/ArticleSaveDTO.java
@@ -1,10 +1,15 @@
 package com.kakao.cafe.model;
 
+import java.util.Objects;
+
 public class ArticleSaveDTO {
     private final String title;
     private final String content;
 
     public ArticleSaveDTO(String title, String content) {
+        validateTitle(title);
+        validateContent(content);
+
         this.title = title;
         this.content = content;
     }
@@ -12,7 +17,17 @@ public class ArticleSaveDTO {
     public String getTitle() {
         return title;
     }
+
     public String getContent() {
         return content;
+    }
+
+
+    private void validateTitle(String title) {
+        if (Objects.isNull(title) || title.length() <= 0) throw new IllegalArgumentException();
+    }
+
+    private void validateContent(String content) {
+        if (Objects.isNull(content) || content.length() <= 0) throw new IllegalArgumentException();
     }
 }
