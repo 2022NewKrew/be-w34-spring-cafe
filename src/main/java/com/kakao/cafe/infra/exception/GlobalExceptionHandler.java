2f7c0866f64bf087f1ca41a64dc019e75974377c
HyunJi <hyunjilim98@gmail.com>
2022-01-14 14:04:23 +0900

    사용하지 않는 import 제거

--- src/main/java/com/kakao/cafe/infra/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kakao/cafe/infra/exception/GlobalExceptionHandler.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
