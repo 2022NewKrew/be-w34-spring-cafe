1099f22c0722178d07b275ae9e37a73eaf7bd432
kusakina0608 <platina.kusakina@gmail.com>
2022-01-14 23:19:03 +0900

    로그 추가

--- src/main/java/com/kakao/cafe/config/CafeAdvice.java
+++ src/main/java/com/kakao/cafe/config/CafeAdvice.java
@@ -4,34 +4,41 @@ import com.kakao.cafe.exception.user.LoginFailedException;
 import com.kakao.cafe.exception.post.PostNotFoundException;
 import com.kakao.cafe.exception.user.UserNotFoundException;
 import com.kakao.cafe.exception.user.UserRegisterFailedException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class CafeAdvice {
 
     @ExceptionHandler(LoginFailedException.class)
     public String handleLoginFailedException() {
+        log.info("Login failed");
         return "user/login";
     }
 
     @ExceptionHandler(UserRegisterFailedException.class)
     public String handleUserRegisterFailedException() {
+        log.info("User register failed");
         return "error/500";
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public String handleUserNotFoundException() {
+        log.info("User not found");
         return "error/500";
     }
 
     @ExceptionHandler(PostNotFoundException.class)
     public String handlePostNotFoundException() {
+        log.info("Post not found");
         return "error/404";
     }
 
     @ExceptionHandler(Exception.class)
-    public String handleMethodArgumentTypeMismatchException() {
+    public String handleException() {
+        log.info("Exception");
         return "error/404";
     }
 }
