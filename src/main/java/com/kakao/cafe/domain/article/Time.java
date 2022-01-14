a66390c6a94683630a725bbfea0ae98df6dfa4ab
rubam.kong <rubam.kong@kakaocorp.com>
2022-01-14 16:33:29 +0900

    text, title 테스트 코드 및 작성시간 도메인 Time 구현

---
+++ src/main/java/com/kakao/cafe/domain/article/Time.java
@@ -0,0 +1,37 @@
+package com.kakao.cafe.domain.article;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class Time {
+
+    private final String time;
+
+    protected Time(String time) {
+        this.time = time;
+    }
+
+    public static Time now() {
+        String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        return new Time(time);
+    }
+
+    @Override
+    public String toString() {
+        return time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Time time1 = (Time) o;
+        return Objects.equals(time, time1.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(time);
+    }
+}
