76dec3c7745dcc421c0f9071aef8613f6e85759c
green.day <green.day@kakaocorp.com>
2022-01-16 17:19:15 +0900

    refactor: User 모델 생성자 lombok 어노테이션으로 변경

--- src/main/java/com/kakao/cafe/domain/model/User.java
+++ src/main/java/com/kakao/cafe/domain/model/User.java
@@ -1,29 +1,15 @@
 package com.kakao.cafe.domain.model;
 
-import com.kakao.cafe.domain.dto.UserSignUpDTO;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class User {
     private String userId;
     private String password;
     private String name;
     private String email;
-
-    public User(String userId, String password, String name, String email) {
-        this.userId = userId;
-        this.password = password;
-        this.name = name;
-        this.email = email;
-    }
-
-
-    public User(UserSignUpDTO userSignUpDTO) {
-        this.userId = userSignUpDTO.getUserId();
-        this.password = userSignUpDTO.getPassword();
-        this.name = userSignUpDTO.getName();
-        this.email = userSignUpDTO.getEmail();
-    }
 }
 
 
