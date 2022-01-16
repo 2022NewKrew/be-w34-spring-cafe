cac5881f7ab5dbb6999bc9e13490a255e3982371
green.day <green.day@kakaocorp.com>
2022-01-16 17:27:55 +0900

    feat: userViewDTO 생성 시 전달된 user객체 null인지 검사

--- src/main/java/com/kakao/cafe/domain/dto/UserViewDTO.java
+++ src/main/java/com/kakao/cafe/domain/dto/UserViewDTO.java
@@ -3,7 +3,9 @@ package com.kakao.cafe.domain.dto;
 
 import com.kakao.cafe.domain.model.User;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.Objects;
 
 @Getter
 public class UserViewDTO {
@@ -12,9 +14,15 @@ public class UserViewDTO {
     private final String email;
 
     public UserViewDTO(User user){
+        validateUser(user);
+
         this.userId = user.getUserId();
         this.name = user.getName();
         this.email = user.getEmail();
+    }
+
+    private void validateUser(User user){
+        if(Objects.isNull(user)) throw new IllegalArgumentException();
     }
 }
 
