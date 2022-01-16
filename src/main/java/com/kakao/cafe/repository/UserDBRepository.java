77218f5f4e56e7f67b7da1178cca769b8ec8a152
green.day <green.day@kakaocorp.com>
2022-01-16 16:55:43 +0900

    feat: UserDBRepository 기본 형태 추가

---
+++ src/main/java/com/kakao/cafe/repository/UserDBRepository.java
@@ -0,0 +1,39 @@
+package com.kakao.cafe.repository;
+
+import com.kakao.cafe.domain.dto.UserSignUpDTO;
+import com.kakao.cafe.domain.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class UserDBRepository implements UserRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public UserDBRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public void signUp(UserSignUpDTO userSignUpDTO) {
+
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return null;
+    }
+
+    @Override
+    public User findUserByUserId(String userId) {
+        return null;
+    }
+}
