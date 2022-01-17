af11b49e249e16c8808e2eb9dac8f13bfbb30d77
hoodie.b612 <hoodie.b612@kakaocorp.com>
2022-01-17 20:08:01 +0900

    H2 DB 설치

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO `USER`
+VALUES ('1231245-wqerqwer-234234', 'hoodie.b612@kakaocorp.com', 'hoodie', 'secret');
+
+INSERT INTO `ARTICLE` (ID, TITLE, CONTENT)
+VALUES (1, 'hello', 'world asdf');
+
+
