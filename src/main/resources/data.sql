2324d8c5e0c99690a3ed757f348ee4583b33c905
ben.ny <ben.ny@kakaocorp.com>
2022-01-17 17:04:24 +0900

    Chore : 테스트 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,6 +6,8 @@ FROM POST;
 
 INSERT INTO USERS
 VALUES (0, '제이지', '힙찔이', 'jayz@kakaocorp.com', '123123');
+INSERT INTO USERS
+VALUES (1, '벤', 'ben', 'benlee73@naver.com', 'aa');
 
 SELECT ID, USERID, NICKNAME, EMAIL, PASSWORD
 FROM USERS;
