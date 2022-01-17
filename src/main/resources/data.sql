1f1e0563dac729b138f4a03abe2babf3933067ff
jun.dev <jun.dev@kakaocorp.com>
2022-01-17 13:42:01 +0900

    docs: 유저테이블에  테스트 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 insert into USERS (userid, password, name, email) values ('admin', 'admin', 'adminname', 'admin@kakaocorp.com');
+insert into USERS (userid, password, name, email) values ('firstid', '1234', 'firstname', 'first@kakaocorp.com');
 insert into ARTICLES (name, title, contents, date) values ('first', 'firsttitle', 'firstcontents', CURRENT_TIMESTAMP());
