5e5ce48b69bc0b94a9bdc0aa90296475a5d4b04e
jh Lim <jhwlim25@gmail.com>
2022-01-17 18:41:29 +0900

    fix : 유효성 검사시 이전 페이지의 경로가 null인 경우 root path로 이동하도록 재수정

--- src/main/java/com/kakao/cafe/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kakao/cafe/exception/GlobalExceptionHandler.java
@@ -5,38 +5,36 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @ControllerAdvice
-public class ExceptionHandler {
+public class GlobalExceptionHandler {
     private static final String HEADER_REFERER = "Referer";
     private static final String PREFIX_FOR_REDIRECTION = "redirect:";
     private static final String ROOT_PATH = "/";
 
     // @Valid 유효성을 통과하지 못하면 BindException 이 발생한다.
-    @org.springframework.web.bind.annotation.ExceptionHandler(BindException.class)
+    @ExceptionHandler(BindException.class)
     public String handleInvalidRequestException(BindException e, HttpServletRequest request, RedirectAttributes rttr) {
-        String pathOfRedirection = getPathOfRedirectionFromRequest(request);
-
         Errors errors = e.getBindingResult();
         errors.getFieldErrors().forEach(error -> rttr.addFlashAttribute(error.getField(), error.getDefaultMessage()));
-
-        rttr.addFlashAttribute("msg", e.getMessage());
-        return PREFIX_FOR_REDIRECTION + pathOfRedirection;
+        rttr.addFlashAttribute("msg", "잘못된 입력 값입니다.");
+        return PREFIX_FOR_REDIRECTION + getPathOfRedirectionFromRequest(request);
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+    @ExceptionHandler(Exception.class)
     public String handleException(Exception e, HttpServletRequest request, RedirectAttributes rttr) {
         e.printStackTrace();
-        String pathOfRedirection = getPathOfRedirectionFromRequest(request);
         rttr.addFlashAttribute("msg", e.getMessage());
-        return PREFIX_FOR_REDIRECTION + (StringUtils.hasLength(pathOfRedirection) ? pathOfRedirection : ROOT_PATH);
+        return PREFIX_FOR_REDIRECTION + getPathOfRedirectionFromRequest(request);
     }
 
     private String getPathOfRedirectionFromRequest(HttpServletRequest request) {
-        return request.getHeader(HEADER_REFERER);
+        String path = request.getHeader(HEADER_REFERER);
+        return StringUtils.hasLength(path) ? path : ROOT_PATH;
     }
 }
