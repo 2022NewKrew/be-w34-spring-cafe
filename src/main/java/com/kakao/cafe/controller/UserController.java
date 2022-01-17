2e3c42fc530ce328b3fd65735b123f5a33f36697
김남현 <kssrcn@naver.com>
2022-01-17 13:03:29 +0900

    로그인 UI 추가

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.kakao.cafe.controller;
 
+import com.kakao.cafe.controller.dto.request.UserLoginRequestDto;
 import com.kakao.cafe.controller.dto.response.UserProfileResponseDto;
 import com.kakao.cafe.controller.dto.response.UserQueryResponseDto;
 import com.kakao.cafe.controller.dto.request.UserSignUpRequestDto;
+import com.kakao.cafe.controller.dto.session.UserLoginSession;
 import com.kakao.cafe.domain.User;
 import com.kakao.cafe.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -45,15 +49,24 @@ public class UserController {
     @PostMapping()
     public String signUp(@Validated @ModelAttribute UserSignUpRequestDto userSignUpRequestDto, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            log.info("회원가입 에러 발생");
-            throw new IllegalArgumentException();
+            log.error("회원가입 실패: ", IllegalArgumentException.class);
         }
 
         userService.signUp(userSignUpRequestDto);
         return "redirect:/users";
     }
 
+    @PostMapping("/login")
+    public String login(@ModelAttribute UserLoginRequestDto userLoginRequestDto, BindingResult bindingResult, HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            log.error("로그인 실패: ", IllegalArgumentException.class);
+        }
 
+        User loginUser = userService.login(userLoginRequestDto);
+        HttpSession session = request.getSession();
+        session.setAttribute("loginUser", new UserLoginSession(loginUser));
+        return "redirect:/";
+    }
 }
 
 
--- src/main/java/com/kakao/cafe/controller/dto/request/UserLoginRequestDto.java
+++ src/main/java/com/kakao/cafe/controller/dto/request/UserLoginRequestDto.java
@@ -3,11 +3,15 @@ package com.kakao.cafe.controller.dto.request;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Builder
 @Getter
 public class UserLoginRequestDto {
 
+    @NotBlank
     private final String userId;
+    @NotBlank
     private final String password;
 
 }
