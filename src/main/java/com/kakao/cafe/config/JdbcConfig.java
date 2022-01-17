a31bc1e5bd38ba4a58b2b72bac01a822cae85b51
mimseong <parkseongmin98@gmail.com>
2022-01-17 19:28:54 +0900

    [setting] 환경변수 (dev, local) 추가

--- src/main/java/com/kakao/cafe/config/JdbcConfig.java
+++ src/main/java/com/kakao/cafe/config/JdbcConfig.java
@@ -3,6 +3,8 @@ package com.kakao.cafe.config;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -10,12 +12,29 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 public class JdbcConfig {
 
     @Bean
+    @Profile("local")
     public DataSource dataSource() {
+        System.out.println("------local------");
+        System.out.println();
         return new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
                 .setName("kakaodb")
                 .addScript("classpath:/h2-sql/settings.sql")
                 .addScript("classpath:/h2-sql/tables.sql")
                 .build();
+    }
+
+    @Bean
+    @Profile("dev")
+    public DataSource mysqlDataSource() {
+        System.out.println("------dev------");
+        System.out.println();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/kakaodb");
+        dataSource.setUsername("kakao");
+        dataSource.setPassword("kakao");
+
+        return dataSource;
     }
 }
