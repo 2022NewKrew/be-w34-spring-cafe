a8dc8c6bab79645e31f57327f8840a3e171f1430
hoodie.b612 <hoodie.b612@kakaocorp.com>
2022-01-17 21:41:50 +0900

    유저 id 길이 50으로 변경 및 data.sql 내용 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO `USER`
-VALUES ('1231245-wqerqwer-234234', 'hoodie.b612@kakaocorp.com', 'hoodie', 'secret');
+VALUES ('2af18e13-2d98-4216-85c2-92ab394b9184', 'hoodie.b612@kakaocorp.com', 'hoodie', 'secret');
 
-INSERT INTO `ARTICLE` (ID, TITLE, CONTENT)
-VALUES (1, 'hello', 'world asdf');
+INSERT INTO `ARTICLE` (TITLE, CONTENT)
+VALUES ('리뷰 감사합니다 나노', '많은 도움이 되고 있습니다 :)');
 
 
