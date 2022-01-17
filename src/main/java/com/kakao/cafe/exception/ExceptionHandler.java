0fa6dcf67a87f7e762fe02ca5a8e5095a5f992f2
jh Lim <jhwlim25@gmail.com>
2022-01-17 13:42:13 +0900

    fix : 에러 발생시 이전 페이지의 경로가 null인 경우 root path로 이동하도록 수정

--- src/main/java/com/kakao/cafe/exception/ExceptionHandler.java
+++ src/main/java/com/kakao/cafe/exception/ExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.kakao.cafe.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,9 +9,12 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Slf4j
 @ControllerAdvice
 public class ExceptionHandler {
     private static final String HEADER_REFERER = "Referer";
+    private static final String PREFIX_FOR_REDIRECTION = "redirect:";
+    private static final String ROOT_PATH = "/";
 
     // @Valid 유효성을 통과하지 못하면 BindException 이 발생한다.
     @org.springframework.web.bind.annotation.ExceptionHandler(BindException.class)
@@ -20,7 +25,7 @@ public class ExceptionHandler {
         errors.getFieldErrors().forEach(error -> rttr.addFlashAttribute(error.getField(), error.getDefaultMessage()));
 
         rttr.addFlashAttribute("msg", e.getMessage());
-        return "redirect:" + pathOfRedirection;
+        return PREFIX_FOR_REDIRECTION + pathOfRedirection;
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
@@ -28,7 +33,7 @@ public class ExceptionHandler {
         e.printStackTrace();
         String pathOfRedirection = getPathOfRedirectionFromRequest(request);
         rttr.addFlashAttribute("msg", e.getMessage());
-        return "redirect:" + pathOfRedirection;
+        return PREFIX_FOR_REDIRECTION + (StringUtils.hasLength(pathOfRedirection) ? pathOfRedirection : ROOT_PATH);
     }
 
     private String getPathOfRedirectionFromRequest(HttpServletRequest request) {
