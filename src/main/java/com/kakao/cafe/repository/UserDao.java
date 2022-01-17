c84e09399000c98187e6272f944a116f24ab7187
= <=>
2022-01-17 17:59:49 +0900

    로그인 후 보여지는 메뉴 수정

--- src/main/java/com/kakao/cafe/repository/UserDao.java
+++ src/main/java/com/kakao/cafe/repository/UserDao.java
@@ -74,7 +74,6 @@ public class UserDao implements UserRepository {
 
     public List<User> findAll() {
 
-        System.out.println(count());
         return jdbcTemplate.query(
                 "select * from MEMBER",
                 new UserMapper()
