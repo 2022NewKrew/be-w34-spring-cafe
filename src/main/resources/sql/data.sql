f3d4c8f13008e9590ca0df112fa9c22c77610b47
Jungyu Choi <devwithpug@kakao.com>
2022-01-17 17:21:32 +0900

    refactor: data.sql 테스트를 위한 엔티티 추가

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,3 +1,3 @@
-insert into users ( username, nickname, email, password ) values ( 'admin', 'admin', 'admin@mail.com', 'admin' );
+insert into users ( username, nickname, email, password ) values ( 'admin', 'admin', 'admin@mail.com', 'admin' ), ( 'pug.gg', 'pug', 'pug.gg@kakaocorp.com', '123' );
 
 insert into article ( title, description )values ( '테스트 게시글', '테스트로 생성된 게시글 입니다.' );
