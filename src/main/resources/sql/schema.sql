0cf2136d3ae442943dbc68c08c3bac57559031d7
hglee93 <jeuslhg@gmail.com>
2022-01-17 13:19:44 +0900

    Modify ddl.sql
    - user 테이블의 user_id Unique 속성 추가

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id long NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id varchar(20) NOT NULL,
+    user_id varchar(20) UNIQUE NOT NULL,
     password varchar(50) NOT NULL,
     user_name varchar(10) NOT NULL,
     email varchar(50) NOT NULL
