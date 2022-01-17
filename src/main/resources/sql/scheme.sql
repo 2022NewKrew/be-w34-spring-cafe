ab08d44bd415f9ab5d9ae21bb2246c239a22d282
GoonLee <auteur.kor@gmail.com>
2022-01-17 10:57:08 +0900

    Improve: add not null constraint to db scheme

--- src/main/resources/sql/scheme.sql
+++ src/main/resources/sql/scheme.sql
@@ -6,19 +6,19 @@ CREATE TABLE users
     users_id UUID DEFAULT UUID() NOT NULL,
     username VARCHAR UNIQUE NOT NULL,
     password VARCHAR NOT NULL,
-    name VARCHAR,
-    email VARCHAR,
+    name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
     PRIMARY KEY(users_id)
 );
 
 CREATE TABLE articles
 (
     articles_id UUID DEFAULT UUID() NOT NULL,
-    title VARCHAR,
-    content CLOB,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    view_count INT DEFAULT 0,
-    users_id UUID,
+    title VARCHAR NOT NULL,
+    content CLOB NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    view_count INT DEFAULT 0 NOT NULL,
+    users_id UUID NOT NULL,
     PRIMARY KEY(articles_id),
     FOREIGN KEY(users_id) REFERENCES users
 );
