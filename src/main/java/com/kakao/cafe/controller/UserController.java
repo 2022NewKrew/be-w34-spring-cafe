dc8f5e55be70338b3537c78eab5ddd47e9ffbb36
faust.like <faust.like@kakaocorp.com>
2022-01-17 18:26:54 +0900

    refactor: controller에서의 코드 꼬임 해결

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -5,9 +5,11 @@ import com.kakao.cafe.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,15 +20,17 @@ public class UserController {
 
     @PostMapping("/create")
     public String SignUp(@ModelAttribute SignUpDTO userInfo) {
-        logger.info("Attempt to SignUp; ID={}", userInfo.getUserId());
+        logger.debug("Attempt to SignUp; ID={}", userInfo.getUserId());
+
+        String ALREADY_EXISTS_ID_MESSAGE = "This ID is already exists";
 
         if (userService.signUp(userInfo)) {
-            logger.info("Successful SignUp; ID={}", userInfo.getUserId());
+            logger.debug("Successful SignUp; ID={}", userInfo.getUserId());
             return "redirect:/user/list";
         }
 
-        logger.info("SignUp Failed. already exist ID={}", userInfo.getUserId());
-        return "redirect:/user/list";
+        logger.debug("SignUp Failed. already exist ID={}", userInfo.getUserId());
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ALREADY_EXISTS_ID_MESSAGE);
     }
 
     @GetMapping("/list")
@@ -37,7 +41,7 @@ public class UserController {
 
     @GetMapping("/prof/{userId}")
     public String getUserProfile(Model model, @PathVariable String userId) {
-        logger.info("User profile request; ID={}", userId);
+        logger.debug("User profile request; ID={}", userId);
         model.addAttribute("userProfile", userService.getUserProfile(userId));
         return "user/profile";
     }
