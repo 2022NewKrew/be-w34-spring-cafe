6746f31397c49945bfb6d90f53c99d0b1fcbc22d
Heejoo Shin <48991173+ev-moon@users.noreply.github.com>
2022-01-17 22:11:36 +0900

    스프링 카페 2-1단계 구현 - jayde.s (#404)
    * deploy: add missing build settings
    
    * feat: spring cafe step 2-1
    
    - 테스트 데이터 추가
    - 유저 세션 기능 구현

--- src/main/java/com/kakao/cafe/web/controller/UsersApiController.java
+++ src/main/java/com/kakao/cafe/web/controller/UsersApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -23,7 +24,7 @@ public class UsersApiController {
     }
 
     @GetMapping
-    String getUsers(Model model) {
+    public String getUsers(Model model) {
         List<Map<String, String>> userList = new ArrayList<>();
         List<Users> queriedUsers = userService.getUsers();
         for (int i = 0; i < queriedUsers.size(); i++) {
@@ -45,9 +46,31 @@ public class UsersApiController {
         return "users/form";
     }
 
+    @PostMapping("/login")
+    String login(String userId, String password, HttpSession session) {
+        try {
+            Users user = userService.getByUserName(userId);
+            if (!user.getPassword().equals(password)) {
+                System.out.println("패스워드가 일치하지 않습니다.");
+                return "redirect:/users/login";
+            }
+            session.setAttribute("sessionedUser", user);
+            return "redirect:/";
+        } catch (Exception e) {
+            System.out.println("아이디가 존재하지 않습니다."); // TODO: 경고창 띄우는 것으로 변경
+            return "users/login";
+        }
+    }
+
     @GetMapping("/login")
-    String login() {
+    String getLoginForm() {
         return "users/login";
+    }
+
+    @GetMapping("/logout")
+    String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/";
     }
 
     @GetMapping("/{id}")
@@ -64,21 +87,23 @@ public class UsersApiController {
     }
 
     @PostMapping
-    String createUser(Users user) {
+    String createUser(Users user, HttpSession session) {
         userService.addUser(user);
+        session.setAttribute("sessionedUser", user);
         return "redirect:/users";
     }
 
-    @GetMapping("/update/{id}")
-    String getUpdateForm(@PathVariable int id, Model model) {
-        Users user = userService.getByUserId(id);
+    @GetMapping("/update")
+    String getUpdateForm(Model model, HttpSession session) {
+        Users user = (Users) session.getAttribute("sessionedUser");
         model.addAttribute("user", user);
         return "users/updateForm";
     }
 
-    @PostMapping("/update/{id}")
-    String updateUserProfile(@PathVariable int id, String newPassword, Users updateUser) {
-        userService.updateUser(id, updateUser, newPassword);
+    @PostMapping("/update")
+    String updateUserProfile(String newPassword, Users updateUser, HttpSession session) {
+        Users currentUser = (Users) session.getAttribute("sessionedUser");
+        userService.updateUser(currentUser.getId(), updateUser, newPassword);
         return "redirect:/users";
     }
 }
