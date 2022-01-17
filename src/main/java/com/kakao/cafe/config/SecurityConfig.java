ecc475b153a6204500affd6af5286d94435f65b5
Dongchan KIM <fromecha@gmail.com>
2022-01-17 10:27:30 +0900

    [setting] h2 접속을 위한 security 추가

--- src/main/java/com/kakao/cafe/config/SecurityConfig.java
+++ src/main/java/com/kakao/cafe/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.kakao.cafe.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2-console/**");
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
