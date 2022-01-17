39ed80467bf1ae8b2b5f44e60d729af44aeeccec
jake.0208 <jake.0208@kakaocorp.com>
2022-01-17 12:56:49 +0900

    Refactor: 서버 연결 확인할 수 있게 ApiController추가

---
+++ src/main/java/com/kakao/cafe/web/ApiController.java
@@ -0,0 +1,18 @@
+package com.kakao.cafe.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    Logger logger = LoggerFactory.getLogger(ApiController.class);
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "server test page";
+    }
+
+}
