00e76029ac8c5a87210cce41b859c7c193584118
david.dh <david.dh@kakaocorp.com>
2022-01-17 18:35:58 +0900

    회원 관련 Dto 세분화

---
+++ src/main/java/com/kakao/cafe/dto/user/UserSessionDto.java
@@ -0,0 +1,13 @@
+package com.kakao.cafe.dto.user;
+
+public class UserSessionDto {
+    private String userId;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
