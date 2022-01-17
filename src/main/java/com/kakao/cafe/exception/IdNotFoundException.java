0987a06b63539f9d1e79691e3a5a75fced85bbbe
edward.baek <edward.baek@kakaocorp.com>
2022-01-17 22:58:53 +0900

    chore : 필요없는 클래스 삭제

--- src/main/java/com/kakao/cafe/exception/IdNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.kakao.cafe.exception;
-
-public class IdNotFoundException extends RuntimeException{
-
-    public IdNotFoundException(String message) {
-        super(message);
-
-    }
-}
