9d0f1dcffc2cdf9ee99d7e8af3a298b08d05a7bb
charlie.p1 <charlie.p1@kakaocorp.com>
2022-01-17 13:52:56 +0900

    Add PASSWORD column at USER table

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO `USER` (USER_ID, NAME, EMAIL)
-VALUES ('test1', 'One Test', 'test1@test.com');
-INSERT INTO `USER` (USER_ID, NAME, EMAIL)
-VALUES ('test2', 'Two Test', 'test2@test.com');
-INSERT INTO `USER` (USER_ID, NAME, EMAIL)
-VALUES ('test3', 'Three Test', 'test3@test.com');
-INSERT INTO `USER` (USER_ID, NAME, EMAIL)
-VALUES ('test4', 'Four Test', 'test4@test.com');
+INSERT INTO `USER` (USER_ID, PASSWORD, NAME, EMAIL)
+VALUES ('test1', '$2a$12$P.Gy4gbCUIFPiUmDUEDEa.7LW47bJToGS6k6.SI22xsmzbjsno/gq', 'One Test', 'test1@test.com');
+INSERT INTO `USER` (USER_ID, PASSWORD, NAME, EMAIL)
+VALUES ('test2', '$2a$12$P.Gy4gbCUIFPiUmDUEDEa.7LW47bJToGS6k6.SI22xsmzbjsno/gq', 'Two Test', 'test2@test.com');
+INSERT INTO `USER` (USER_ID, PASSWORD, NAME, EMAIL)
+VALUES ('test3', '$2a$12$P.Gy4gbCUIFPiUmDUEDEa.7LW47bJToGS6k6.SI22xsmzbjsno/gq', 'Three Test', 'test3@test.com');
+INSERT INTO `USER` (USER_ID, PASSWORD, NAME, EMAIL)
+VALUES ('test4', '$2a$12$P.Gy4gbCUIFPiUmDUEDEa.7LW47bJToGS6k6.SI22xsmzbjsno/gq', 'Four Test', 'test4@test.com');
 COMMIT;
