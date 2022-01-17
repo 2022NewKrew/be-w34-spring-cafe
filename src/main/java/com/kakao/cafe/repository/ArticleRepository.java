b8070b0462653b332b5a8c62efc94bafdf5e3524
kusakina0608 <platina.kusakina@gmail.com>
2022-01-17 15:26:33 +0900

    Repository 인터페이스 추가

--- src/main/java/com/kakao/cafe/repository/ArticleRepository.java
+++ src/main/java/com/kakao/cafe/repository/ArticleRepository.java
@@ -3,43 +3,13 @@ package com.kakao.cafe.repository;
 import com.kakao.cafe.entity.Article;
 import com.kakao.cafe.util.Page;
 import com.kakao.cafe.util.Pageable;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
-@Slf4j
-@Repository
-public class ArticleRepository {
-    private List<Article> articleList;
-    private Long articleId;
+public interface ArticleRepository {
+    public Long save(Article entity);
 
-    public ArticleRepository() {
-        articleList = new ArrayList<>();
-        articleId = 1L;
-    }
+    public Optional<Article> findById(Long articleId);
 
-    public Long save(Article entity) {
-        articleList.add(0, entity.init(articleId));
-        return articleId++;
-    }
-
-    public Optional<Article> findById(Long articleId) {
-        return articleList.stream()
-                .filter(article -> article.getArticleId().equals(articleId))
-                .findFirst();
-    }
-
-    public Page<Article> findAll(Pageable pageable) {
-        int totalPage = (int) Math.ceil(articleList.size() / (double) pageable.getSize());
-        int fromIndex = pageable.getPage() * pageable.getSize();
-        if (fromIndex >= articleList.size())
-            return new Page<Article>(new ArrayList<>(), totalPage, articleList.size(), pageable);
-        int toIndex = fromIndex + pageable.getSize();
-        if (toIndex >= articleList.size())
-            toIndex = articleList.size();
-        return new Page<Article>(articleList.subList(fromIndex, toIndex), totalPage, articleList.size(), pageable);
-    }
+    public Page<Article> findAll(Pageable pageable);
 }
---
+++ src/main/java/com/kakao/cafe/repository/UserRepositoryImpl.java
@@ -0,0 +1,98 @@
+package com.kakao.cafe.repository;
+
+import com.kakao.cafe.entity.User;
+import com.kakao.cafe.util.Page;
+import com.kakao.cafe.util.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+    private List<User> userList;
+
+    public UserRepositoryImpl() {
+        userList = new ArrayList<>();
+
+        userList.add(User.builder()
+                .email("kina.lee@kakaocorp.com")
+                .username("키나")
+                .password("1234")
+                .build()
+                .init());
+        userList.add(User.builder()
+                .email("eroica.b@kakaocorp.com")
+                .username("에로이카")
+                .password("1234")
+                .build()
+                .init());
+        userList.add(User.builder()
+                .email("june.kim@kakaocorp.com")
+                .username("준")
+                .password("1234")
+                .build()
+                .init());
+        IntStream.rangeClosed(1, 50).forEach(value -> {
+            userList.add(User.builder()
+                    .email(String.format("tester.%d@kakaocorp.com", value))
+                    .username(String.format("테스터%d", value))
+                    .password("1234")
+                    .build()
+                    .init());
+        });
+    }
+
+    @Override
+    public void save(User entity) {
+        userList.stream()
+                .filter(user -> user.getEmail().equals(entity.getEmail()))
+                .findFirst()
+                .ifPresentOrElse(
+                        user -> {
+                            user.changeUsername(entity.getUsername());
+                            user.changePassword(entity.getPassword());
+                        },
+                        () -> userList.add(entity)
+                );
+    }
+
+    @Override
+    public void update(User entity) {
+        Optional<User> result = userList.stream()
+                .filter(user -> user.getEmail().equals(entity.getEmail()))
+                .filter(user -> user.getPassword().equals(entity.getPassword()))
+                .findFirst();
+        result.ifPresent(user -> {
+        });
+    }
+
+    @Override
+    public Optional<User> findByEmail(User entity) {
+        return userList.stream()
+                .filter(user -> user.getEmail().equals(entity.getEmail()))
+                .findFirst();
+    }
+
+    @Override
+    public Optional<User> findByEmailAndPassword(User entity) {
+        return userList.stream()
+                .filter(user -> user.getEmail().equals(entity.getEmail()))
+                .filter(user -> user.getPassword().equals(entity.getPassword()))
+                .findFirst();
+    }
+
+    @Override
+    public Page<User> findAll(Pageable pageable) {
+        int totalPage = (int) Math.ceil(userList.size() / (double) pageable.getSize());
+        int fromIndex = pageable.getPage() * pageable.getSize();
+        if (fromIndex >= userList.size())
+            return new Page<User>(new ArrayList<>(), totalPage, userList.size(), pageable);
+        int toIndex = fromIndex + pageable.getSize();
+        if (toIndex >= userList.size())
+            toIndex = userList.size();
+        return new Page<User>(userList.subList(fromIndex, toIndex), totalPage, userList.size(), pageable);
+    }
+}
