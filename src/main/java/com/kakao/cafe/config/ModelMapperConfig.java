2aa43ba58afd620cca671e7ee36762c96b267653
yuchanleeme <yuchanlee.me@gmail.com>
2022-01-17 19:52:50 +0900

    feat: modelMapper config 설정

---
+++ src/main/java/com/kakao/cafe/config/ModelMapperConfig.java
@@ -0,0 +1,20 @@
+package com.kakao.cafe.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.modelmapper.config.Configuration.*;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldAccessLevel(AccessLevel.PRIVATE)
+                .setFieldMatchingEnabled(true);
+        return modelMapper;
+    }
+}
