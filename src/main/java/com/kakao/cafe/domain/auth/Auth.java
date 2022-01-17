14fed0e1bc378986899a2dca7137d44e0ffaf7d0
Jungyu Choi <devwithpug@kakao.com>
2022-01-17 10:30:42 +0900

    step-2-1: Auth 클래스 추가

---
+++ src/main/java/com/kakao/cafe/domain/auth/Auth.java
@@ -0,0 +1,14 @@
+package com.kakao.cafe.domain.auth;
+
+public class Auth {
+
+    private final Long id;
+
+    public Auth(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
