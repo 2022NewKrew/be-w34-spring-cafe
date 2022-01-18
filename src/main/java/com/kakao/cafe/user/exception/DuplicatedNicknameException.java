bfe4de174c009317286d606dcb6678741a05e8f5
jin.jang <jin.jang@kakaocorp.com>
2022-01-18 13:53:26 +0900

    [fix] 예외 슈퍼 클래스 재정의

--- src/main/java/com/kakao/cafe/user/exception/DuplicatedNicknameException.java
+++ src/main/java/com/kakao/cafe/user/exception/DuplicatedNicknameException.java
@@ -1,8 +1,8 @@
 package com.kakao.cafe.user.exception;
 
-import com.kakao.cafe.common.exception.NotFoundException;
+import com.kakao.cafe.common.exception.BadRequestException;
 
-public class DuplicatedNicknameException extends NotFoundException {
+public class DuplicatedNicknameException extends BadRequestException {
 
     public DuplicatedNicknameException() {
         super("해당 닉네임이 이미 존재합니다.");
