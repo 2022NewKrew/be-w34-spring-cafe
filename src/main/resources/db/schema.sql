fad49b719a4472fc3a497817c0a1cc435fcd7764
Hongjeongmin <wjdals0471@naver.com>
2022-01-18 19:55:50 +0900

    init sql database 설정

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,3 +1,6 @@
+SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS member;
+
 create table member(
        id bigint NOT NULL AUTO_INCREMENT,
        user_id varchar(255),
@@ -8,13 +11,104 @@ create table member(
        PRIMARY KEY(id)
 );
 
+DROP TABLE IF EXISTS question;
+
 create table question(
-     id bigint NOT NULL AUTO_INCREMENT,
-     member_id bigint,
-     writer varchar(255),
-     title varchar(255),
-     contents varchar(255),
-     create_time timestamp DEFAULT current_timestamp,
-     PRIMARY KEY(id),
-     FOREIGN KEY(member_id) REFERENCES member(id)
+         id bigint NOT NULL AUTO_INCREMENT,
+         member_id bigint,
+         writer varchar(255),
+         title varchar(255),
+         contents varchar(255),
+         create_time timestamp DEFAULT current_timestamp,
+         PRIMARY KEY(id),
+         FOREIGN KEY(member_id) REFERENCES member(id)
 );
+
+insert into member(user_id, password, name, email)
+values ('jm.hong', 'jm.hong', 'Hongjeongmin', 'jm.hong@kakaocorp.com');
+insert into member(user_id, password, name, email)
+values ('finn.kakao', 'finn.kakao', 'Kimkakao', 'finn.kakao@kakaocorp.com');
+insert into member(user_id, password, name, email, role)
+values ('admin', 'admin', '운영자', 'admin@kakaocorp.com', 'ADMIN');
+
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (1, 'jm.hong', '안녕하세요 반갑습니다.', '오늘 날씨가 좋네요. 정말 날시가 좋은것 같아요!');
+insert into question(member_id, writer, title, contents)
+values (2, 'finn.kakao', '오늘 점심 먹었습니다..', '점심밥으로는 초밥이 제일 무난한것 같아요!');
+SET foreign_key_checks = 1;
