5fd125fe56ffb31e12efcb07c100a856fa94b068
JuyeoungJun <wjfhs01@gmail.com>
2022-01-18 16:29:06 +0900

    chore: 불필요한 sql 파일 삭제

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-select * from USER_PROFILE;
-
-insert into USER_PROFILE(user_id, password, name, email)
-values ('lucas', '123', 'tt', 'tt@test.com'), ('lucas2', '123', 'ttt', 'ttt@test.com');
-
-insert into QNA(writer, title, contents)
-values ('lucas', 'test1', 'test1 contents'), ('lucas2', 'test2', 'test2 contents');
