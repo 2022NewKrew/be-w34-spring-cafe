e9afd90a591a06901af67c9bef002125c564bce8
danny.an <danny.an@kakaocorp.com>
2022-01-18 22:27:12 +0900

    로그인 테스트코드 추가

--- src/test/java/com/kakao/cafe/controller/UserControllerTest.java
+++ src/test/java/com/kakao/cafe/controller/UserControllerTest.java
@@ -3,6 +3,7 @@ package com.kakao.cafe.controller;
 import com.kakao.cafe.exception.CafeExceptionHandler;
 import com.kakao.cafe.exception.user.DuplicateUserIdException;
 import com.kakao.cafe.exception.user.IncorrectPasswordException;
+import com.kakao.cafe.exception.user.UserNotFoundException;
 import com.kakao.cafe.model.dto.UserDto;
 import com.kakao.cafe.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -99,6 +101,56 @@ class UserControllerTest {
             view().name("error"),
             model().attribute("errorStatus", HttpStatus.BAD_REQUEST),
             model().attribute("errorMessage", "잘못된 비밀번호입니다.")
+        );
+    }
+
+    @Test
+    @DisplayName("로그인 성공")
+    public void loginTest() throws Exception {
+        UserDto user = generateUser();
+        doReturn(user).when(userService).loginUser(user.getUserId(), user.getPassword());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/users/login")
+            .param("userId", user.getUserId())
+            .param("password", user.getPassword())
+        ).andExpectAll(
+            status().is3xxRedirection(),
+            request().sessionAttribute("sessionedUser", user),
+            redirectedUrl("/")
+        );
+    }
+
+    @Test
+    @DisplayName("로그인 실패 - 비밀번호 불일치")
+    public void loginIncorrectPasswordTest() throws Exception {
+        UserDto user = generateUser();
+        doThrow(new IncorrectPasswordException()).when(userService).loginUser(any(), any());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/users/login")
+            .param("userId", user.getUserId())
+            .param("password", "2345")
+        ).andExpectAll(
+            status().isOk(),
+            view().name("error"),
+            model().attribute("errorStatus", HttpStatus.BAD_REQUEST),
+            model().attribute("errorMessage", "잘못된 비밀번호입니다.")
+        );
+    }
+
+    @Test
+    @DisplayName("로그인 실패 - 아이디 존재X")
+    public void loginUserNotFoundTest() throws Exception {
+        UserDto user = generateUser();
+        doThrow(new UserNotFoundException()).when(userService).loginUser(any(), any());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/users/login")
+            .param("userId", "test100")
+            .param("password", "2345")
+        ).andExpectAll(
+            status().isOk(),
+            view().name("error"),
+            model().attribute("errorStatus", HttpStatus.NOT_FOUND),
+            model().attribute("errorMessage", "존재하지 않는 계정입니다.")
         );
     }
 
