42805ac4b1f52f767acc98e1aa9785989a79dd99
rogers.t <rogers.t@kakaocorp.com>
2022-01-18 16:35:30 +0900

    test : TestConfiguration 외부로 추출, ControllerTest와 JdbcRepositoryTest를 만들고 이를 상속하도록 구조를 변경

---
+++ src/test/java/com/kakao/cafe/config/ControllerTestConfig.java
@@ -0,0 +1,22 @@
+package com.kakao.cafe.config;
+
+import com.kakao.cafe.user.presentation.mapper.JoinRequestToUserConverter;
+import com.kakao.cafe.user.presentation.mapper.UpdateUserInfoRequestToUserInfoConverter;
+import com.kakao.cafe.user.presentation.mapper.UserToUserDtoConverter;
+import org.modelmapper.Converter;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@TestConfiguration
+public class ControllerTestConfig {
+    @Bean
+    List<Converter<?,?>> converters(){
+        return List.of(
+                new UserToUserDtoConverter(),
+                new JoinRequestToUserConverter(),
+                new UpdateUserInfoRequestToUserInfoConverter()
+        );
+    }
+}
