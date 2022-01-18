d272b90cf1eb93b49f1dd07049fd573ada8f9c50
roger.lee <2wjdwo97@gmail.com>
2022-01-18 13:08:22 +0900

    Implement auth
    Create Auth Interceptor
    Create HttpRequest class

---
+++ src/main/java/com/kakao/cafe/interceptor/HttpRequest.java
@@ -0,0 +1,41 @@
+package com.kakao.cafe.interceptor;
+
+import java.util.List;
+
+public class HttpRequest {
+    private String method;
+    private String url;
+
+    public HttpRequest() {
+    }
+
+    public HttpRequest(String method, String url) {
+        this.method = method;
+        this.url = url;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public boolean equals(String method, String requestURI) {
+        String[] parsedURI = url.split("/");
+        String[] parsedRequestURI = requestURI.split("/");
+
+        if (!this.method.equals(method) || parsedRequestURI.length != parsedURI.length)
+            return false;
+
+        for (int i = 0; i < parsedURI.length; i++) {
+            if (parsedURI[i].equals("{}"))
+                continue;
+
+            if (!parsedRequestURI[i].equals(parsedURI[i]))
+                return false;
+        }
+        return true;
+    }
+}
