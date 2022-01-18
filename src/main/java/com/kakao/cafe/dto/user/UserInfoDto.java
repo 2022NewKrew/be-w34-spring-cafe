9a5d618e461257b2a3ed3abe90c7eb50f44ecc4c
david.dh <david.dh@kakaocorp.com>
2022-01-18 18:56:05 +0900

    롬복 사용

--- src/main/java/com/kakao/cafe/dto/user/UserInfoDto.java
+++ src/main/java/com/kakao/cafe/dto/user/UserInfoDto.java
@@ -1,31 +1,16 @@
 package com.kakao.cafe.dto.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserInfoDto {
     private String userId;
     private String name;
     private String email;
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
--- src/main/java/com/kakao/cafe/dto/user/UserProfileDto.java
+++ src/main/java/com/kakao/cafe/dto/user/UserProfileDto.java
@@ -1,22 +1,15 @@
 package com.kakao.cafe.dto.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserProfileDto {
     private String name;
     private String email;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
