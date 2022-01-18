1b640aa0a340924bd920dea8fab6d1c39337a01e
jh Lim <jhwlim25@gmail.com>
2022-01-18 16:58:26 +0900

    feat : 로그인 하지 않은 사용자는 게시글의 목록만 볼 수 있도록 인터셉터 경로 수정

--- src/main/java/com/kakao/cafe/config/MvcConfig.java
+++ src/main/java/com/kakao/cafe/config/MvcConfig.java
@@ -24,7 +24,7 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
-                .addPathPatterns("/users/{id}/**", "/posts/write", "/posts");
+                .addPathPatterns("/users/{id}/**", "/posts/**");
         registry.addInterceptor(new UserInterceptor())
                 .addPathPatterns("/users/{id}/**");
     }
