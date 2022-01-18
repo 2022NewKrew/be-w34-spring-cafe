ccab0586c1102fd85198f498028c1db145d297a5
skye.kim <skye.kim@kakaocorp.com>
2022-01-18 17:45:33 +0900

    mysql 설정

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,8 @@
+Insert into member(userid, password, name, email)
+values ('cbsky1234', 'test', 'Skye', 'Skye@daum.net');
+
+Insert into member(userid, password, name, email)
+values ('test', 'test', 'blue', 'blue@daum.net');
+
+insert into article(title, contents, writer, active)
+values ('test', 'test test test', 'cbsky1234', true);
