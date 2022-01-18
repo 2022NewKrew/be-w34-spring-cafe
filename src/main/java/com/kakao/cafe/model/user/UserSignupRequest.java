977fbd80d2e48965bab7fe0e13e2ff951838a2c0
jh Lim <jhwlim25@gmail.com>
2022-01-18 14:57:50 +0900

    fix : white space에 대한 유효성 검사 안되는 현상 수정

--- src/main/java/com/kakao/cafe/model/user/UserSignupRequest.java
+++ src/main/java/com/kakao/cafe/model/user/UserSignupRequest.java
@@ -3,18 +3,18 @@ package com.kakao.cafe.model.user;
 import lombok.*;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 
 @ToString
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserSignupRequest {
-    @NotEmpty(message = "이메일은 필수 입력 값 입니다.")
+    @NotBlank(message = "이메일은 필수 입력 값 입니다.")
     @Email(message = "이메일이 형식이 맞지 않습니다.")
     private String email;
-    @NotEmpty(message = "닉네임은 필수 입력 값 입니다.")
+    @NotBlank(message = "닉네임은 필수 입력 값 입니다.")
     private String nickname;
-    @NotEmpty(message = "비밀번호는 필수 입력 값 입니다.")
+    @NotBlank(message = "비밀번호는 필수 입력 값 입니다.")
     private String password;
 
     @Builder
