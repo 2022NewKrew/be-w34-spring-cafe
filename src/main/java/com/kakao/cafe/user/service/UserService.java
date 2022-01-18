e30673903a8a3ab647a09c19c61da06d36a6ffa1
LeeA0 <lay0711@naver.com>
2022-01-19 02:48:38 +0900

    feat: 로그인, 로그아웃 기능 개발

--- src/main/java/com/kakao/cafe/user/service/UserService.java
+++ src/main/java/com/kakao/cafe/user/service/UserService.java
@@ -2,6 +2,7 @@ package com.kakao.cafe.user.service;
 
 import com.kakao.cafe.user.domain.User;
 import com.kakao.cafe.user.dto.request.SignUpRequest;
+import com.kakao.cafe.user.dto.request.loginRequest;
 import com.kakao.cafe.user.dto.response.UserResponse;
 import com.kakao.cafe.user.dto.response.UsersResponse;
 import com.kakao.cafe.user.repository.UserRepository;
@@ -11,12 +12,22 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private static final String DUPLICATED_EMAIL = "이미 존재하는 이메일입니다.";
+    private static final String INVALID_PASSWORD = "올바르지 않은 비밀번호입니다.";
     private static final String USER_NOT_FOUNT_MESSAGE = "해당 회원정보를 찾을 수 없습니다.";
 
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public UserResponse login(loginRequest loginRequest) {
+        User user = userRepository.findByEmail(loginRequest.getEmail())
+            .orElseThrow(() -> new IllegalArgumentException(USER_NOT_FOUNT_MESSAGE));
+        if(!user.getPassword().equals(loginRequest.getPassword())){
+            throw new IllegalArgumentException(INVALID_PASSWORD);
+        }
+        return UserResponse.of(user);
     }
 
     public UsersResponse findAll() {
