0042aad3f0095c68f5269f85c400972d1b00ae4c
jb.jang <jb.jang@kakaocorp.com>
2022-01-18 11:23:44 +0900

    refactor: dto 생성자 인자 및 변수 키워드 추가

--- src/main/java/com/kakao/cafe/web/dto/ArticleResponseDto.java
+++ src/main/java/com/kakao/cafe/web/dto/ArticleResponseDto.java
@@ -7,45 +7,22 @@ import java.util.List;
 
 public class ArticleResponseDto {
 
-    private Long id;
-    private String writer;
-    private String title;
+    private final Long id;
+    private final String writer;
+    private final String title;
 
-    private ArticleResponseDto() {}
+    private ArticleResponseDto(Long id, String writer, String title) {
+        this.id = id;
+        this.writer = writer;
+        this.title = title;
+    }
 
     public static List<ArticleResponseDto> from(List<Article> articleList) {
         List<ArticleResponseDto> result = new ArrayList<>();
         for (Article article : articleList) {
-            ArticleResponseDto responseDto = new ArticleResponseDto();
-            responseDto.setId(article.getId());
-            responseDto.setWriter(article.getWriter());
-            responseDto.setTitle(article.getTitle());
+            ArticleResponseDto responseDto = new ArticleResponseDto(article.getId(), article.getWriter(), article.getTitle());
             result.add(responseDto);
         }
         return result;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getWriter() {
-        return writer;
-    }
-
-    public void setWriter(String writer) {
-        this.writer = writer;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 }
