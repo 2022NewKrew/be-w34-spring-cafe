98fe7db1744574b5025924b9b964c113c3a078ee
GoonLee <auteur.kor@gmail.com>
2022-01-19 01:02:33 +0900

    Implement mission 2-2: control jdbc config in application propertiees and change extension to yml

---
+++ src/test/resources/sql/test-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (users_id, username, password, name, email) VALUES ( 'c87725e6-c3f7-48a3-a7f1-afdc55e775bc', 'nathan.de', 'testpassword', 'nathan', 'nathan.de@kakaocorp.com' );
+INSERT INTO users (users_id, username, password, name, email) VALUES ( '1dc23681-5a71-43bf-9d4c-56def9b8365e', 'ed.nahtan', 'drowssaptset', 'nahtan', 'ed.nahtan@procoakak.moc' );
+
+INSERT INTO articles (articles_id, title, content, users_id) VALUES ( '0a5a3b37-2346-4bf2-8fe5-dc5b4feb9b14', 'testtitle', 'testcontent', 'c87725e6-c3f7-48a3-a7f1-afdc55e775bc' );
+INSERT INTO articles (articles_id, title, content, users_id) VALUES ( '1004d21f-6082-43ee-a274-e86225016958', 'eltittset', 'tnetnoctset', '1dc23681-5a71-43bf-9d4c-56def9b8365e' );
