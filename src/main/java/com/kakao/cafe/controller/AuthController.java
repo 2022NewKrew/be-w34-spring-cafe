b3727caa954c754ee29b89410cd63f4b676a3805
ShinjoWang <dhkdtlswh@gmail.com>
2022-01-19 00:32:33 +0900

    Add authentication logic

---
+++ src/main/java/com/kakao/cafe/controller/AuthController.java
@@ -0,0 +1,50 @@
+package com.kakao.cafe.controller;
+
+import com.kakao.cafe.domain.Auth;
+import com.kakao.cafe.domain.dtos.AuthLoginDto;
+import com.kakao.cafe.service.AuthService;
+import com.kakao.cafe.utils.SessionName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+@Controller
+public class AuthController {
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "user/loginForm";
+    }
+
+    @PostMapping("/login")
+    public String login(
+            @Valid @ModelAttribute AuthLoginDto dto,
+            BindingResult bindingResult,
+            HttpSession session
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "redirect:/login";
+        }
+        Auth auth = authService.login(dto);
+        session.setAttribute(SessionName.AUTH, auth);
+        return "redirect:/";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/";
+    }
+}
