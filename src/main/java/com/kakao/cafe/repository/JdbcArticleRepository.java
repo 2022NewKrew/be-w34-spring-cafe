22055188ddd900b831310b98863072b0ae84af12
Dongchan KIM <fromecha@gmail.com>
2022-01-18 14:37:29 +0900

    [setting] article table db 추가

--- src/main/java/com/kakao/cafe/repository/JdbcArticleRepository.java
+++ src/main/java/com/kakao/cafe/repository/JdbcArticleRepository.java
@@ -2,7 +2,23 @@ package com.kakao.cafe.repository;
 
 import com.kakao.cafe.entity.Article;
 
-//public class JdbcArticleRepository implements Repository<Article, String> {
-//
-//
-//}
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcArticleRepository implements Repository<Article, String> {
+
+    @Override
+    public void create(Article entity) {
+
+    }
+
+    @Override
+    public List<Article> readAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Article> readById(String id) {
+        return Optional.empty();
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,4 @@
 INSERT INTO MEMBER(USERID, password, email) values ('chen', '1234', 'chen.kim@kakaocorp.com');
+
+INSERT INTO ARTICLE(title, content)
+values ('TITLE CHAN', 'TITLE CONTENT');
