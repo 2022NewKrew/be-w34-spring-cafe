672c59d99be152599072f775ca4688b57e7f2f33
chiveloper <wodlr2007@naver.com>
2022-01-18 17:30:47 +0900

    [feature] Article 클래스 변경
    - Article 클래스를 용도에 맞게 변경

--- src/main/java/com/kakao/cafe/domain/article/Article.java
+++ src/main/java/com/kakao/cafe/domain/article/Article.java
@@ -1,17 +1,13 @@
 package com.kakao.cafe.domain.article;
 
 import lombok.Getter;
-<<<<<<< HEAD
 import lombok.Setter;
-=======
->>>>>>> 314ff13 ([feature] Article 클래스 생성)
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 @Getter
-<<<<<<< HEAD
 @Setter
 public class Article {
     private int index;
@@ -24,19 +20,6 @@ public class Article {
         this.writer = writer;
         this.title = title;
         this.contents = contents;
-=======
-public class Article {
-    private static int index;
-    private String writer;
-    private String title;
-    private List<Content> contents;
-    private String time;
-
-    Article(String writer, String title, String contents) {
-        this.writer = writer;
-        this.title = title;
-        this.contents = createContents(contents);
->>>>>>> 314ff13 ([feature] Article 클래스 생성)
         index = ArticleList.getArticleList().size() + 1;
         time = getCreatedTime();
     }
@@ -50,11 +33,7 @@ public class Article {
         return nowDate.format(dateFormatter) + " " + nowTime.format(timeFormatter);
     }
 
-<<<<<<< HEAD
     public String getContents(){
-=======
-    public List<Content> getContents(){
->>>>>>> 314ff13 ([feature] Article 클래스 생성)
         return contents;
     }
 }
