68d29be5bfd8fec7e85d1bda9893a82d2408d37c
david.dh <david.dh@kakaocorp.com>
2022-01-18 18:57:22 +0900

    초기 데이터 구분을 위한 수정

--- src/main/resources/jdbc/data.sql
+++ src/main/resources/jdbc/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO QNA_USER VALUES ( 'david.dh','1234','김대환','david.dh@kakaocorp.com' );
-INSERT INTO QNA_USER VALUES ( 'david1.dh','1234','김대환','david.dh@kakaocorp.com' );
+INSERT INTO QNA_USER VALUES ( 'kdh5163','1234','데이비드','kdh5163@kakaocorp.com' );
