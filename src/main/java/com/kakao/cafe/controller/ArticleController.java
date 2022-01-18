fbb7f2e6c1255bf405bfd3a934e300b8ba349beb
김남현 <kssrcn@naver.com>
2022-01-18 16:44:53 +0900

    OwnershipValidator 추가

--- src/main/java/com/kakao/cafe/controller/ArticleController.java
+++ src/main/java/com/kakao/cafe/controller/ArticleController.java
@@ -6,18 +6,17 @@ import com.kakao.cafe.controller.dto.request.ArticleUpdateRequestDto;
 import com.kakao.cafe.controller.dto.response.ArticleQueryDetailResponseDto;
 import com.kakao.cafe.controller.dto.response.ArticleUpdateFormResponseDto;
 import com.kakao.cafe.controller.dto.session.UserLoginSession;
+import com.kakao.cafe.controller.validator.OwnershipValidator;
 import com.kakao.cafe.domain.Article;
 import com.kakao.cafe.service.ArticleService;
 import com.kakao.cafe.service.dto.ArticleUpdateDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping("/articles")
@@ -26,6 +25,7 @@ import org.springframework.web.server.ResponseStatusException;
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final OwnershipValidator ownershipValidator;
 
     @PostMapping
     public String register(@Validated @ModelAttribute ArticleRegisterRequestDto articleRegisterRequestDto, BindingResult bindingResult) {
@@ -46,7 +46,7 @@ public class ArticleController {
     @PutMapping("/{id}")
     public String update(@PathVariable Long id, @ModelAttribute ArticleUpdateRequestDto articleUpdateRequestDto,
                          @SessionAttribute(name = Constants.loginUser) UserLoginSession userLoginSession) {
-        validateArticleAuthority(userLoginSession.getUserId(), articleUpdateRequestDto.getWriterId());
+        ownershipValidator.validate(userLoginSession.getUserId(), articleUpdateRequestDto.getWriterId());
         articleService.update(new ArticleUpdateDto(id, articleUpdateRequestDto));
         return "redirect:/articles/" + id;
     }
@@ -55,7 +55,7 @@ public class ArticleController {
     public String getUpdateForm(@PathVariable Long id, Model model,
                                 @SessionAttribute(name = Constants.loginUser) UserLoginSession userLoginSession) {
         Article foundArticle = articleService.findById(id);
-        validateArticleAuthority(userLoginSession.getUserId(), foundArticle.getWriter().getUserId());
+        ownershipValidator.validate(userLoginSession.getUserId(), foundArticle.getWriter().getUserId());
         ArticleUpdateFormResponseDto articleUpdateFormResponseDto = new ArticleUpdateFormResponseDto(id, foundArticle.getWriter().getUserId());
         model.addAttribute("article", articleUpdateFormResponseDto);
         return "article/updateForm";
@@ -65,14 +65,8 @@ public class ArticleController {
     public String delete(@PathVariable Long id, @SessionAttribute(name = Constants.loginUser) UserLoginSession userLoginSession) {
 
         Article foundArticle = articleService.findById(id);
-        validateArticleAuthority(userLoginSession.getUserId(), foundArticle.getWriter().getUserId());
+        ownershipValidator.validate(userLoginSession.getUserId(), foundArticle.getWriter().getUserId());
         articleService.deleteById(id);
         return "redirect:/";
-    }
-
-    private void validateArticleAuthority(String loginUserId, String ownerUserId) {
-        if (!loginUserId.equals(ownerUserId)) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "권한이 없습니다", new IllegalArgumentException());
-        }
     }
 }
