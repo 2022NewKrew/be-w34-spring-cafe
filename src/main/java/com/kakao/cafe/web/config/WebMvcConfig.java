bbfb882eee7d2a5c13a2d0049cbe070089b8ce8b
flip1945 <coghdus1016@naver.com>
2022-01-18 15:47:05 +0900

    feat: LoginInterceptor 기능 추가

---
+++ src/main/java/com/kakao/cafe/web/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.kakao.cafe.web.config;
+
+import com.kakao.cafe.web.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LoginInterceptor loginInterceptor = new LoginInterceptor();
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns(loginInterceptor.loginPath);
+    }
+}
---
+++ src/main/java/com/kakao/cafe/web/interceptor/LoginInterceptor.java
@@ -0,0 +1,29 @@
+package com.kakao.cafe.web.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    public List<String> loginPath = Arrays.asList("/articles/**", "/articleForm");
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        Long loginId = (Long) session.getAttribute("memberId");
+
+        if (loginId != null) {
+            return true;
+        }
+
+        response.sendRedirect("/login");
+        return false;
+    }
+}
