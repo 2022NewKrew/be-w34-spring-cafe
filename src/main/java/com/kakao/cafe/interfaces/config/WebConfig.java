f58f79845ba67ed9f728c5a409b8ab4b816da848
binary.yun <binary.yun@kakaocorp.com>
2022-01-18 11:07:59 +0900

    feat: 세션 로그인 및 인증 인터셉터 구현

---
+++ src/main/java/com/kakao/cafe/interfaces/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.kakao.cafe.interfaces.config;
+
+import com.kakao.cafe.interfaces.common.AuthenticationInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    private final AuthenticationInterceptor authenticationInterceptor;
+
+    public WebConfig(AuthenticationInterceptor authenticationInterceptor) {
+        this.authenticationInterceptor = authenticationInterceptor;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor).addPathPatterns("/users/**");
+    }
+}
