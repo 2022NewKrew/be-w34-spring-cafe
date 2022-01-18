c121925e217b4727373caa534183200d777809f2
skye.kim <skye.kim@kakaocorp.com>
2022-01-18 17:46:51 +0900

    feat: Article 기능 구현

--- src/main/java/com/kakao/cafe/article/controller/ArticleController.java
+++ src/main/java/com/kakao/cafe/article/controller/ArticleController.java
@@ -2,15 +2,25 @@ package com.kakao.cafe.article.controller;
 
 import com.kakao.cafe.article.entity.Article;
 import com.kakao.cafe.article.service.ArticleService;
+import com.kakao.cafe.user.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Controller
 public class ArticleController {
-    private final ArticleService articleService = new ArticleService();
+    private final ArticleService articleService;
+
+    @Autowired
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
 
     @GetMapping(path = "/")
     public String home() {
@@ -24,23 +34,60 @@ public class ArticleController {
     }
 
     @GetMapping("/articles/{articleId}")
-    public String getOneArticle(Model model, @PathVariable long articleId) {
-        Article article = articleService.findById(articleId);
-        if (article == null) {
+    public String getOneArticle(HttpSession session, Model model, @PathVariable long articleId) {
+        Optional<Article> article = articleService.findOneById(articleId);
+        if (article.isEmpty()) {
             return "redirect:/";
         }
-        model.addAttribute("articleInfo", article);
+
+        User user = (User) session.getAttribute("sessionedUser");
+        if (user == null || !article.get().getWriter().equals(user.getUserId())) {
+            return "redirect:/";
+        }
+
+        model.addAttribute("articleInfo", article.get());
         return "qna/show";
     }
 
-    //----------------------
+    @GetMapping("/articles/{articleId}/form")
+    public String updateArticleForm(HttpSession session, Model model, @PathVariable long articleId) {
+        Optional<Article> article = articleService.findOneById(articleId);
+        if (article.isEmpty()) {
+            return "redirect:/";
+        }
+
+        User user = (User) session.getAttribute("sessionedUser");
+        if (user == null || !article.get().getWriter().equals(user.getUserId())) {
+            return "redirect:/";
+        }
+
+        model.addAttribute("article", article.get());
+        return "qna/updateForm";
+    }
 
     @PostMapping("/articles")
-    public String createUser(String title, String contents, String writer) {
-        contents.replace("\r\n", "<br>");
-        Article article = new Article(0, title, contents, writer);
+    public String createArticle(@ModelAttribute("article") Article article) {
+        String contents = article.getContents().replace("\r\n", "<br>");
+        article.setContents(contents);
+        article.setCreatedTime(Timestamp.valueOf(LocalDateTime.now()));
+        article.setUpdatedTime(Timestamp.valueOf(LocalDateTime.now()));
         articleService.addArticle(article);
 
         return "redirect:/articles/" + article.getId();
+    }
+
+    @PatchMapping("/articles")
+    public String updateArticle(long id, String title, String contents, String writer) {
+        articleService.update(id, title, contents, writer);
+        return "redirect:/articles/" + id;
+    }
+
+    @DeleteMapping("/articles/{articleId}")
+    public String deleteArticle(@PathVariable long articleId, String sessionUser, String articleUser) {
+        if (sessionUser.equals(articleUser)) {
+            articleService.delete(articleId);
+        }
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/kakao/cafe/article/service/ArticleService.java
+++ src/main/java/com/kakao/cafe/article/service/ArticleService.java
@@ -2,14 +2,20 @@ package com.kakao.cafe.article.service;
 
 import com.kakao.cafe.article.entity.Article;
 import com.kakao.cafe.article.repository.ArticleRepository;
-import com.kakao.cafe.article.repository.MemoryArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ArticleService {
-    private ArticleRepository articleRepository = new MemoryArticleRepository();
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
 
     public void addArticle(Article article) {
         articleRepository.save(article);
@@ -19,8 +25,16 @@ public class ArticleService {
         return articleRepository.findAll();
     }
 
-    public Article findById(long id) {
-        return articleRepository.findById(id);
+    public Optional<Article> findOneById(long id) {
+        return articleRepository.findOneById(id);
+    }
+
+    public void update(long id, String title, String contents, String writer) {
+        articleRepository.update(id, title, contents, writer);
+    }
+
+    public void delete(long id) {
+        articleRepository.delete(id);
     }
 
 }
