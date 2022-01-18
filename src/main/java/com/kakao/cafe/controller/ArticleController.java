19aa2069f0bbfc1e042fc37ec95235a6101026e4
Jungyu Choi <devwithpug@kakao.com>
2022-01-18 10:17:08 +0900

    step-2-2: 로그인 여부에 따라 게시글 접근가능 여부 변경

--- src/main/java/com/kakao/cafe/controller/ArticleController.java
+++ src/main/java/com/kakao/cafe/controller/ArticleController.java
@@ -1,11 +1,14 @@
 package com.kakao.cafe.controller;
 
+import com.kakao.cafe.domain.auth.Auth;
 import com.kakao.cafe.dto.article.ArticleDto;
 import com.kakao.cafe.dto.article.ArticleRequest;
 import com.kakao.cafe.service.ArticleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/articles")
@@ -25,7 +28,11 @@ public class ArticleController {
     }
 
     @GetMapping("/{articleId}")
-    public String getArticle(@PathVariable Long articleId, Model model) {
+    public String getArticle(@PathVariable Long articleId, Model model, HttpSession session) {
+        Auth auth = (Auth) session.getAttribute("auth");
+        if (auth == null) {
+            return "redirect:/";
+        }
         ArticleDto article = articleService.findById(articleId);
         model.addAttribute("article", article);
 
