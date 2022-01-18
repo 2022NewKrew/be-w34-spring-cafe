26c8ca403b161c0c786ac04d6d09a96bc95ea33c
edward.baek <edward.baek@kakaocorp.com>
2022-01-18 21:57:42 +0900

    refactor : 필요없는 import 제거

--- src/main/java/com/kakao/cafe/post/application/port/out/DeleteQuestionPostPort.java
+++ src/main/java/com/kakao/cafe/post/application/port/out/DeleteQuestionPostPort.java
@@ -1,7 +1,5 @@
 package com.kakao.cafe.post.application.port.out;
 
-import com.kakao.cafe.post.application.dto.command.QuestionPostDeleteCommand;
-
 public interface DeleteQuestionPostPort {
 
     void delete(Long id);
--- src/main/java/com/kakao/cafe/user/adapter/in/web/UserAccountApiController.java
+++ src/main/java/com/kakao/cafe/user/adapter/in/web/UserAccountApiController.java
@@ -1,18 +1,10 @@
 package com.kakao.cafe.user.adapter.in.web;
 
 import com.kakao.cafe.user.adapter.in.web.dto.request.UserAccountEnrollRequest;
-import com.kakao.cafe.user.adapter.in.web.dto.request.UserAccountLoginRequest;
 import com.kakao.cafe.user.adapter.in.web.dto.response.UserAccountEnrollResponse;
-import com.kakao.cafe.user.adapter.in.web.dto.response.UserAccountLoginResponse;
-import com.kakao.cafe.user.application.dto.command.UserAccountCheckCommand;
-import com.kakao.cafe.user.application.dto.result.UserAccountCheckResult;
-import com.kakao.cafe.user.application.dto.result.UserAccountDetailResult;
 import com.kakao.cafe.user.application.dto.result.UserAccountEnrollResult;
-import com.kakao.cafe.user.application.port.in.CheckUserAccountUseCase;
 import com.kakao.cafe.user.application.port.in.EnrollUserAccountUseCase;
-import com.kakao.cafe.user.application.port.in.GetUserAccountUseCase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.util.UriComponents;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on;
