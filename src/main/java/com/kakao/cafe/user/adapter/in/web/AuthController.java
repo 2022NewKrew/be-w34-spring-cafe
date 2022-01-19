0c32925192362767dc25fcc1dbfb47869cbc8019
edward.baek <edward.baek@kakaocorp.com>
2022-01-19 10:32:33 +0900

    refactor : 필요없는 import 제거

--- src/main/java/com/kakao/cafe/user/adapter/in/web/AuthController.java
+++ src/main/java/com/kakao/cafe/user/adapter/in/web/AuthController.java
@@ -2,7 +2,6 @@ package com.kakao.cafe.user.adapter.in.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
