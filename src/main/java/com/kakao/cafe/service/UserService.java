205fc4b9680f825390653e8b5c333cf951463683
김남현 <kssrcn@naver.com>
2022-01-19 17:11:36 +0900

    profile별 db 설정 추가

--- src/main/java/com/kakao/cafe/service/UserService.java
+++ src/main/java/com/kakao/cafe/service/UserService.java
@@ -2,7 +2,6 @@ package com.kakao.cafe.service;
 
 import com.kakao.cafe.controller.dto.request.UserLoginRequestDto;
 import com.kakao.cafe.controller.dto.request.UserSignUpRequestDto;
-import com.kakao.cafe.controller.dto.request.UserUpdateRequestDto;
 import com.kakao.cafe.domain.User;
 import com.kakao.cafe.exception.UserNotFoundException;
 import com.kakao.cafe.repository.user.UserRepository;
