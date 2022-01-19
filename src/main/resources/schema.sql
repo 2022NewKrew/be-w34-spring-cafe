1bab978442a418017ef47feeca5ccc0367c7bf0c
jake.0208 <jake.0208@kakaocorp.com>
2022-01-19 20:17:46 +0900

    Refactor: db에 not null추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,19 @@
 DROP TABLE IF EXISTS USERLIST;
 CREATE TABLE IF NOT EXISTS USERLIST
 (
-    ID       LONG PRIMARY KEY auto_increment,
-    USERID   VARCHAR(255),
-    PASSWORD VARCHAR(255),
-    NAME     VARCHAR(255),
-    EMAIL    VARCHAR(255)
+    ID       LONG PRIMARY KEY auto_increment NOT NULL,
+    USERID   VARCHAR(255)                    NOT NULL,
+    PASSWORD VARCHAR(255)                    NOT NULL,
+    NAME     VARCHAR(255)                    NOT NULL,
+    EMAIL    VARCHAR(255)                    NOT NULL
 );
 
 DROP TABLE IF EXISTS ARTICLE;
 CREATE TABLE IF NOT EXISTS ARTICLE
 (
-    ID        LONG PRIMARY KEY auto_increment,
-    WRITETIME VARCHAR(255),
-    WRITER    VARCHAR(255),
-    TITLE     VARCHAR(255),
-    CONTENTS  VARCHAR(255)
+    ID        LONG PRIMARY KEY auto_increment NOT NULL,
+    WRITETIME VARCHAR(255)                    NOT NULL,
+    WRITER    VARCHAR(255)                    NOT NULL,
+    TITLE     VARCHAR(255)                    NOT NULL,
+    CONTENTS  VARCHAR(255)                    NOT NULL
 );
