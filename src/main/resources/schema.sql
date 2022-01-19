1f465a0726f7f48aaece4baa0b4aea8901bd3b8d
aiden.jang <aiden.jang@kakaocorp.com>
2022-01-20 01:48:15 +0900

    Use MySQL

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,22 @@
+DROP TABLE IF EXISTS USERS;
+DROP TABLE IF EXISTS ARTICLE;
+
 CREATE TABLE IF NOT EXISTS USERS
 (
-    id             BIGINT  NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    string_id      VARCHAR NOT NULL UNIQUE,
-    password       VARCHAR NOT NULL,
-    name           VARCHAR,
-    email          VARCHAR,
-    sign_up_date   DATETIME
+    id           BIGINT      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    string_id    VARCHAR(20) NOT NULL UNIQUE,
+    password     VARCHAR(20) NOT NULL,
+    name         VARCHAR(20),
+    email        VARCHAR(20),
+    sign_up_date DATETIME
 );
 
 CREATE TABLE IF NOT EXISTS ARTICLE
 (
     id         BIGINT  NOT NULL PRIMARY KEY AUTO_INCREMENT,
     author     BIGINT  NOT NULL,
-    title      VARCHAR NOT NULL,
+    title      VARCHAR(50) NOT NULL,
     write_date DATETIME,
-    content    CLOB,
+    content    TEXT,
     hits       INT
 );
