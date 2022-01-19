d35393ad100a791112cfefe6a451e5c4e4bf3c52
david.dh <david.dh@kakaocorp.com>
2022-01-19 20:17:56 +0900

    reply 테이블 추가

--- src/main/resources/jdbc/schema.sql
+++ src/main/resources/jdbc/schema.sql
@@ -7,12 +7,26 @@ create table qna_user (
 );
 
 create table article (
-    id int NOT NULL AUTO_INCREMENT,
+    articleId int NOT NULL AUTO_INCREMENT,
     writer varchar(255) NOT NULL ,
     title varchar(255) NOT NULL ,
     contents varchar(255) NOT NULL ,
     date varchar(255) NOT NULL ,
     userId varchar(255) ,
-    PRIMARY KEY (id) ,
+    PRIMARY KEY (articleId) ,
     FOREIGN KEY (userId) REFERENCES qna_user(userId)
+);
+
+create table reply (
+    replyId int NOT NULL AUTO_INCREMENT,
+    writer varchar(255) NOT Null ,
+    comment varchar(255) NOT NULL ,
+    date varchar(255) NOT NULL ,
+    articleId int NOT NULL ,
+    userId varchar(255) NOT NULL ,
+    PRIMARY KEY (replyId) ,
+    FOREIGN KEY (articleId) REFERENCES article(articleId)
+                   ON DELETE CASCADE ,
+    FOREIGN KEY (userId) REFERENCES qna_user(userId)
+        ON DELETE CASCADE
 );
