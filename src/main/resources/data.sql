9267609e05225e92f30b84dcf6ba9a7b404ab4a0
Dongchan KIM <fromecha@gmail.com>
2022-01-19 16:17:13 +0900

    [setting] session application property

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-INSERT INTO MEMBER(USERID, password, email) values ('chen', '1234', 'chen.kim@kakaocorp.com');
+INSERT INTO MEMBER(ID, USERID, password, email) values (1, 'chen', '1234', 'chen.kim@kakaocorp.com');
+INSERT INTO MEMBER(ID, USERID, password, email) values (2, 'chen2', '12342', 'chen2.kim@kakaocorp.com');
 
 INSERT INTO ARTICLE(title, content)
 values ('TITLE CHAN', 'TITLE CONTENT');
