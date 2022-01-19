a1138ecf57c05fe0d0bb4a69813d26b64ced0552
david.dh <david.dh@kakaocorp.com>
2022-01-19 20:17:40 +0900

    게시글 상세 조회 시 댓글도 가져오도록 변경

--- src/main/java/com/kakao/cafe/controller/ArticleController.java
+++ src/main/java/com/kakao/cafe/controller/ArticleController.java
@@ -2,11 +2,14 @@ package com.kakao.cafe.controller;
 
 import com.kakao.cafe.annotation.Auth;
 import com.kakao.cafe.dto.article.ArticleDto;
+import com.kakao.cafe.dto.reply.ReplyDto;
 import com.kakao.cafe.dto.user.UserSessionDto;
 import com.kakao.cafe.exception.InputDataException;
 import com.kakao.cafe.exception.NullSessionException;
 import com.kakao.cafe.exception.UserMismatchException;
 import com.kakao.cafe.service.ArticleService;
+import com.kakao.cafe.service.ReplyService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -19,18 +22,16 @@ import javax.validation.Valid;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class ArticleController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final ArticleService articleService;
-
-    public ArticleController(ArticleService articleService) {
-        this.articleService = articleService;
-    }
+    private final ReplyService replyService;
 
     @GetMapping("/articles/post")
     @Auth
-    public String form(HttpSession session) {
+    public String form() {
         return "./qna/form";
     }
 
@@ -54,18 +55,21 @@ public class ArticleController {
         return "./qna/list";
     }
 
-    @GetMapping("/articles/{id}")
+    @GetMapping("/articles/{articleId}")
     @Auth
-    public String showArticle(@PathVariable int id, Model model) {
-        ArticleDto articleDto = articleService.getArticle(id);
+    public String showArticle(@PathVariable int articleId, Model model) {
+        ArticleDto articleDto = articleService.getArticle(articleId);
+        List<ReplyDto> replyList = replyService.getReplyList(articleId);
         model.addAttribute("article", articleDto);
+        model.addAttribute("replyList",replyList);
+        model.addAttribute("countOfReply",replyList.size());
         return "./qna/show";
     }
 
-    @GetMapping("/articles/{id}/update")
-    public String getArticleForm(@PathVariable int id, Model model, HttpSession session) throws UserMismatchException {
+    @GetMapping("/articles/{articleId}/update")
+    public String getArticleForm(@PathVariable int articleId, Model model, HttpSession session) throws UserMismatchException {
         UserSessionDto sessionedUser = (UserSessionDto) session.getAttribute("sessionedUser");
-        ArticleDto article = articleService.getArticle(id);
+        ArticleDto article = articleService.getArticle(articleId);
         if (!article.getUserId().equals(sessionedUser.getUserId())) {
             throw new UserMismatchException("다른 사용자의 게시글을 수정할 수 없습니다.");
         }
@@ -73,20 +77,20 @@ public class ArticleController {
         return "./qna/updateForm";
     }
 
-    @PutMapping("/articles/{id}/update")
-    public String updateArticle(@PathVariable int id, ArticleDto articleDto) {
-        articleDto.setId(id);
+    @PutMapping("/articles/{articleId}/update")
+    public String updateArticle(@PathVariable int articleId, ArticleDto articleDto) {
+        articleDto.setArticleId(articleId);
         articleService.updateArticle(articleDto);
         return "redirect:/";
     }
 
-    @DeleteMapping("/articles/{id}/delete")
-    public String deleteArticle(@PathVariable int id, String userId, HttpSession session) throws UserMismatchException {
+    @DeleteMapping("/articles/{articleId}/delete")
+    public String deleteArticle(@PathVariable int articleId, String userId, HttpSession session) throws UserMismatchException {
         UserSessionDto sessionedUser = (UserSessionDto) session.getAttribute("sessionedUser");
         if (!userId.equals(sessionedUser.getUserId())) {
             throw new UserMismatchException("다른 사용자의 게시글을 삭제할 수 없습니다.");
         }
-        articleService.deleteArticle(id);
+        articleService.deleteArticle(articleId);
         return "redirect:/";
     }
 
