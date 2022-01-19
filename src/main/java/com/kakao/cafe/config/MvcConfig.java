31852c0379a37779f19aced1a171f11264f169d0
anika.dovob <anika.dovob@kakaocorp.com>
2022-01-19 15:34:40 +0900

    feat: 로그인 여부에 따라 접근 권한 수정
    - 로그인 하지 않은 사용자는 게시글 상세 조회, 작성 불가(로그인 페이지로 redirect)
    - 로그인 하지 않은 사용자도 게시글 목록은 조회 가능

--- src/main/java/com/kakao/cafe/config/MvcConfig.java
+++ src/main/java/com/kakao/cafe/config/MvcConfig.java
@@ -1,7 +1,10 @@
 package com.kakao.cafe.config;
 
+import com.kakao.cafe.common.AuthHandlerInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +17,19 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("posts");
         registry.addViewController("/users/form").setViewName("users/form");
         registry.addViewController("/posts/form").setViewName("posts/form");
+    }
+
+    @Bean
+    public AuthHandlerInterceptor authHandlerInterceptor() {
+        return new AuthHandlerInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authHandlerInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/fonts/**", "/images/**", "/js/**")
+                .excludePathPatterns("/users", "/users/form", "/users/login")
+                .excludePathPatterns("/", "/posts");
     }
 }
