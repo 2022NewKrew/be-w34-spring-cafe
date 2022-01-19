9aef0780e1433a5da76517ed64e692f4a15b5b62
faust.like <faust.like@kakaocorp.com>
2022-01-20 00:52:21 +0900

    feat: profile address 기능 추가
    writer와 같은 이름의 User가 있을 경우 그 사람의 프로필 사진을 가져온다.
    아직 이와 관련된 문제점이 보이긴 하지만 요구사항이 없으므로 일단 이렇게 해놓는다.
    
    추가로 TODO 마크를 추가하였다.

--- src/main/java/com/kakao/cafe/qna/service/QuestionService.java
+++ src/main/java/com/kakao/cafe/qna/service/QuestionService.java
@@ -3,6 +3,8 @@ package com.kakao.cafe.qna.service;
 import com.kakao.cafe.qna.DTO.*;
 import com.kakao.cafe.qna.domain.Article;
 import com.kakao.cafe.qna.domain.ArticleRepository;
+import com.kakao.cafe.user.domain.User;
+import com.kakao.cafe.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class QuestionService {
     private final ArticleRepository articleRepository;
+    private final UserRepository userRepository;
 
     public void submitArticle(QuestionDTO packedArgs) {
         articleRepository.submitArticle(packedArgs.getWriter(), packedArgs.getTitle(), packedArgs.getContents());
@@ -24,7 +27,6 @@ public class QuestionService {
         return articleLst.stream()
                 .map(this::makeArticleSummaryDTOFromArticle)
                 .collect(Collectors.toList());
-
     }
 
     private ArticleSummaryDTO makeArticleSummaryDTOFromArticle(Article article) {
@@ -33,21 +35,32 @@ public class QuestionService {
     }
 
     public PackedArticleDTO getPackedArticle(String idx) {
-        ArticleDTO article = makeWrittenThingDTOFromArticle(articleRepository.getArticleLst().get(Integer.parseInt(idx) - 1));
+        Article foundArticle = articleRepository.getArticleLst().get(Integer.parseInt(idx) - 1);
+        ArticleDTO articleDTO = makeArticleDTOFromArticle(foundArticle,
+                getWriterProfileAddress(foundArticle));
+
+        //TODO : Reply 객체가 구현되면 로직 구현
         List<ReplyDTO> replies = new ArrayList<>();
 
-        return makePackedArticle(article, replies);
+        return makePackedArticle(articleDTO, replies);
     }
 
-    private ArticleDTO makeWrittenThingDTOFromArticle(Article article) {
-        //TODO addr 변경
-        return new ArticleDTO(article.getWriter(), "https://graph.facebook.com/v2.3/1324855987/picture",
+    private String getWriterProfileAddress(Article foundArticle) {
+        //TODO : Optional
+        User foundUser = userRepository.getUser(foundArticle.getWriter());
+        if (foundUser == null) {
+            return "https://graph.facebook.com/v2.3/1324855987/picture";
+        }
+
+        return foundUser.getPictureAddress();
+    }
+
+    private ArticleDTO makeArticleDTOFromArticle(Article article, String profileAddress) {
+        return new ArticleDTO(article.getWriter(), profileAddress,
                 article.getWrittenTime(), article.getContent(), article.getTitle(), String.valueOf(article.getId()));
     }
 
     private PackedArticleDTO makePackedArticle(ArticleDTO article, List<ReplyDTO> replies) {
         return new PackedArticleDTO(article, replies, replies.size());
     }
-
-
 }
