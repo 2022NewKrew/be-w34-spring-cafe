c35cef60f2d7b71306b68c379b49bb1b850e637a
vince.joe <vince.joe@kakaocorp.com>
2022-01-19 18:14:23 +0900

    test: Controller 테스트 추가

--- src/main/java/com/kakao/cafe/config/MvcConfig.java
+++ src/main/java/com/kakao/cafe/config/MvcConfig.java
@@ -1,32 +1,13 @@
 package com.kakao.cafe.config;
 
-import com.kakao.cafe.domain.post.PostMapper;
-import com.kakao.cafe.domain.post.PostRepository;
-import com.kakao.cafe.domain.post.impl.JdbcTemplatePostRepository;
-import com.kakao.cafe.domain.user.UserMapper;
-import com.kakao.cafe.domain.user.UserRepository;
-import com.kakao.cafe.domain.user.impl.JdbcTemplateUserRepository;
 import com.kakao.cafe.interceptor.LoginInterceptor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-
-    @Bean
-    public PostRepository getPostRepositoryBean(DataSource dataSource, PostMapper postMapper) {
-        return new JdbcTemplatePostRepository(dataSource, postMapper);
-    }
-
-    @Bean
-    public UserRepository getUserRepositoryBean(DataSource dataSource, UserMapper userMapper) {
-        return new JdbcTemplateUserRepository(dataSource, userMapper);
-    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
