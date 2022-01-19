50f4c4725b22fcaee6c2522462f535423a0bfa3f
jun.dev <jun.dev@kakaocorp.com>
2022-01-19 11:15:34 +0900

    refactor: 댓글작성 후 현재페이지 redirect 로 수정

--- src/main/java/com/kakao/cafe/article/controller/ArticleController.java
+++ src/main/java/com/kakao/cafe/article/controller/ArticleController.java
@@ -168,6 +168,6 @@ public class ArticleController {
         }
 
         articleService.replyCreate(replyCreateDTO);
-        return "redirect:/";
+        return "redirect:/qnas/" + replyCreateDTO.getArticleSeq();
     }
 }
