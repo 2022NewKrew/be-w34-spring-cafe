1d6314b9fc791a35528e95e6bbc81e46aca8a39b
Dongchan KIM <fromecha@gmail.com>
2022-01-19 17:22:04 +0900

    [test] AuthService login 실패

--- src/test/java/com/kakao/cafe/service/AuthServiceTest.java
+++ src/test/java/com/kakao/cafe/service/AuthServiceTest.java
@@ -16,6 +16,8 @@ class AuthServiceTest {
 
     private final String userId = "chen";
     private final String password = "1234";
+    private final String notUserId = "notChen";
+    private final String notPassword = "not1234";
 
     @Autowired
     JdbcUserRepository jdbcUserRepository;
@@ -37,19 +39,29 @@ class AuthServiceTest {
         User login = authService.login(userLoginDto);
 
         // then
-        Assertions.assertEquals("chen", login.getUserId());
-        Assertions.assertEquals("1234", login.getPassword());
+        assertEquals("chen", login.getUserId());
+        assertEquals("1234", login.getPassword());
     }
 
     @Test
     @DisplayName("[실패] 로그인 닉네임 실패")
     void login_Nickname_Fail() {
+        // given
+        AuthService authService = new AuthService(jdbcUserRepository);
+        UserLoginDto userLoginDto = new UserLoginDto(notUserId, password);
 
+        // when & then
+        assertThrows(RuntimeException.class, () -> authService.login(userLoginDto));
     }
 
     @Test
-    @DisplayName("[성공] 로그인 성공해서 user 객체 return")
+    @DisplayName("[실패] 로그인 비밀번호 실패")
     void login_Password_Fail() {
+        // given
+        AuthService authService = new AuthService(jdbcUserRepository);
+        UserLoginDto userLoginDto = new UserLoginDto(userId, notPassword);
 
+        // when & then
+        assertThrows(RuntimeException.class, () -> authService.login(userLoginDto));
     }
 }
