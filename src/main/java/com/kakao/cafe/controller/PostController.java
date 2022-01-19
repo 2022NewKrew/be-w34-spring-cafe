b6146e1104fec8e146c463930883bdea0d0969be
Joonhyeong-Park <55684184+Joonhyeong-Park@users.noreply.github.com>
2022-01-19 16:41:01 +0900

    2주차 미션 1까지 커밋입니다

--- src/main/java/com/kakao/cafe/controller/PostController.java
+++ src/main/java/com/kakao/cafe/controller/PostController.java
@@ -1,26 +1,33 @@
-package com.kakao.cafe.posts;
+package com.kakao.cafe.controller;
 
 
-import com.kakao.cafe.users.User;
+import com.kakao.cafe.domain.Post;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Objects;
 
-@RestController
+@Controller
 public class PostController {
     private ArrayList<Post> posts = new ArrayList<>();
     Logger logger = LoggerFactory.getLogger(PostController.class);
 
+    @GetMapping("/questions")
+    public String getPostForm(Model model){
+        logger.info("GET /questions : 글쓰기");
+        return "post/form";
+    }
+
     @PostMapping("/questions")
-    public String createPost(String title, String writer, int year, int month, int day, int count){
+    public String createPost(String title, String writer, LocalDate createdDate){
         logger.info("POST /questions : {} 생성", title);
-        Post post = new Post(title, writer, year, month, day, count, posts.size()+1);
+        Post post = new Post(title, writer, createdDate, posts.size()+1);
         posts.add(post);
         return "redirect:/";
     }
@@ -37,7 +44,7 @@ public class PostController {
         logger.info("GET /users/{} : {} 게시글 조회",ID,posts.get(ID).getTitle());
         Post post = null;
         for(Post p : posts){
-            if(Objects.equals(p.getID(), ID)){
+            if(Objects.equals(p.getPostId(), ID)){
                 post = p;
             }
         }
---
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -0,0 +1,127 @@
+package com.kakao.cafe.controller;
+
+
+import com.kakao.cafe.domain.LoginForm;
+import com.kakao.cafe.domain.User;
+import com.kakao.cafe.form.UserCreationForm;
+import com.kakao.cafe.form.UserEditForm;
+import com.kakao.cafe.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+
+@Controller
+public class UserController {
+    private final UserService userService;
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/createAccount")
+    public String createForm(){
+        return "user/form";
+    }
+
+    @PostMapping("/users")
+    public String createAccount(UserCreationForm form){
+        logger.info("POST /users : {} 생성", form.getEmail());
+        User user = new User();
+        user.setPassword(form.getPassword());
+        user.setNickname(form.getNickname());
+        user.setEmail(form.getEmail());
+        userService.join(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String getUsers(Model model){
+        logger.info("GET /users : 유저 전체목록 조회");
+        List<User> users = userService.findMembers();
+        model.addAttribute("users", users);
+        return "user/list";
+    }
+
+    @GetMapping("/users/{ID}")
+    public String getUser(@PathVariable int ID, Model model){
+        logger.info("GET /users/{} : {} 유저 조회",ID,ID);
+        Optional<User> user = userService.findOne(ID);
+        model.addAttribute("user", user);
+        return "user/profile";
+    }
+
+    @GetMapping("/users/{ID}/update")
+    public String getUserEditForm(@PathVariable int ID, Model model){
+        logger.info("GET /users/{}/form : {} 유저 개인정보 수정",ID,ID);
+        Optional<User> user = userService.findOne(ID);
+        try{
+            User u = user.get();
+            model.addAttribute("user",u);
+        } catch(Exception e){
+            logger.info("null user 에러");
+            return "user/unknown-user";
+        }
+
+        return "/user/updateForm";
+    }
+
+//    @PostMapping("/users/{ID}/update")
+//    public String editUser(@PathVariable int ID, UserEditForm form){
+//        logger.info("POST /users/{}/update : {} 유저 개인정보 수정",ID,ID);
+//        Optional<User> user = userService.findOne(ID);
+//        try{
+//            User u = user.get();
+//            u.setNickname(form.getNickname());
+//            u.setEmail(form.getEmail());
+//        } catch (Exception e){
+//            logger.info("null user 에러");
+//            return "user/unknown-user";
+//        }
+//
+//        return "user/list";
+//    }
+
+    @GetMapping("/login")
+    public String getLogin(){
+        return "user/login";
+    }
+
+    @PostMapping("/login")
+    public String login(HttpSession session, LoginForm form){
+        logger.info("{} 로그인", form.getEmail());
+        if(userService.validateLogin(form.getEmail(),form.getPassword())){
+            session.setAttribute("sessionedUser",userService.findOneByEmail(form.getEmail()));
+            return "redirect:/";
+        } else{
+            logger.info("로그인 에러");
+            return "redirect:/user/login-error";
+        }
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpSession session){
+        Object value = session.getAttribute("sessionedUser");
+        if(value != null){
+            User user = (User)value;
+            logger.info("{} 로그아웃 : ", user.getEmail());
+            session.invalidate();
+            return "redirect:/";
+        } else{
+            logger.info("logout error");
+            return "user/login-error";
+        }
+    }
+}
