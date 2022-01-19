d002abcc718784dee14d2228df2e11b355f7506c
yoonmyunghoon <52689727+yoonmyunghoon@users.noreply.github.com>
2022-01-19 22:08:02 +0900

    카페 2-2 MySQL 연결 (#534)
    - 서버에 설치한 MySQL 에 연결
    - 연결 정보는 따로 분리
    
    Co-authored-by: reynold-yoon <reynold.yoon@kakaocorp.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO USERS (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('javajigi', 'test', '자바지기', 'javajigi@slipp.net');
-INSERT INTO USERS (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('sanjigi', 'test', '산지기', 'sanjigi@slipp.net');
+INSERT INTO users (user_id, password, name, email) VALUES ('javajigi', 'test', '자바지기', 'javajigi@slipp.net');
+INSERT INTO users (user_id, password, name, email) VALUES ('sanjigi', 'test', '산지기', 'sanjigi@slipp.net');
