3b09ed43974f17b37e23871fdda8e4d0ba6469be
GoonLee <auteur.kor@gmail.com>
2022-01-19 11:13:45 +0900

    Apply code review: return list of the new object in InMemoryRepository

--- src/main/java/com/kakao/cafe/repository/InMemoryArticleRepository.java
+++ src/main/java/com/kakao/cafe/repository/InMemoryArticleRepository.java
@@ -6,6 +6,7 @@ import com.kakao.cafe.repository.mapper.InMemoryArticleMapper;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -26,7 +27,9 @@ public class InMemoryArticleRepository implements ArticleRepository {
 
     @Override
     public List<Article> findAll() {
-        return List.copyOf(articles.getArticleList());
+        return articles.getArticleList().stream()
+                .map(articleMapper::mapResult)
+                .collect(Collectors.toList());
     }
 
     @Override
