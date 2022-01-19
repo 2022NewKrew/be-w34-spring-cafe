215ffc2fa51ea9fbfe14a07fcdff4bd52bae5d65
jake.0208 <jake.0208@kakaocorp.com>
2022-01-19 21:00:42 +0900

    FIX		: Interceptor사용하여 session 없을시 로그인창으로 가게함

---
+++ src/main/java/com/kakao/cafe/web/LoginInterceptor.java
@@ -0,0 +1,26 @@
+package com.kakao.cafe.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    private final Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        Object obj = session.getAttribute("sessionedUser");
+
+        if (obj == null) {
+            response.sendRedirect("/user/login");
+            return false;
+        }
+        return true;
+    }
+}
