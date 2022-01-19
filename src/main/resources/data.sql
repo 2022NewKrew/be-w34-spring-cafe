1572e30622b6b3cdb5bfb2210a2f3cd893b80f06
danny.an <danny.an@kakaocorp.com>
2022-01-19 18:13:06 +0900

    ModelMapper 의존성 추가 & 빈 등록

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO users (id, user_id, password, name, email) VALUES ( 1, 'test1', 'test', 'test1', 'test1@hanmail.net' );
+INSERT INTO users (user_id, password, name, email) VALUES ( 'test1', 'test', 'test1', 'test1@hanmail.net' );
 
-INSERT INTO users (id, user_id, password, name, email) VALUES ( 2, 'test2', 'test', 'test2', 'test2@hanmail.net' );
+INSERT INTO users (user_id, password, name, email) VALUES ( 'test2', 'test', 'test2', 'test2@hanmail.net' );
 
-INSERT INTO users (id, user_id, password, name, email) VALUES ( 3, 'test3', 'test', 'test3', 'test3@hanmail.net' );
+INSERT INTO users (user_id, password, name, email) VALUES ( 'test3', 'test', 'test3', 'test3@hanmail.net' );
 
 INSERT INTO articles (TITLE, CONTENTS, WRITER_ID) VALUES ( 'title1', 'contents1', 1 );
 
