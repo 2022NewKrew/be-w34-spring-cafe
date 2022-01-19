0295351aa4d0d9d18d95eb9becd5d175545b94b1
champ.ion <champ.ion@kakaocorp.com>
2022-01-19 12:54:54 +0900

    feat(스프링 카페 2-2 단계): AuthHandlerInterceptor 구현
    - 글 상세보기, 글 쓰기 등은 로그인 한 사용자만 가능하도록 interceptor를 통해 session 검증 로직 추가
    -
    로그인 하지 않은 사용자가 위의 기능에 접근할 경우 login page로 redirection

--- src/main/java/com/kakao/cafe/config/MvcConfig.java
+++ src/main/java/com/kakao/cafe/config/MvcConfig.java
@@ -1,7 +1,10 @@
 package com.kakao.cafe.config;
 
+import com.kakao.cafe.adapter.in.presentation.SessionCheckInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,5 +23,17 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/articles/error").setViewName("article/form");
         registry.addViewController("/user/login").setViewName("user/login");
         registry.addViewController("login/error").setViewName("user/login");
+    }
+
+    @Bean
+    public SessionCheckInterceptor sessionCheckInterceptor() {
+        return new SessionCheckInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(sessionCheckInterceptor())
+                .addPathPatterns("/users/**/form")
+                .addPathPatterns("/articles/**");
     }
 }
