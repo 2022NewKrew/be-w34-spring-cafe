4807765127bf9440decc319035e8c007c7b50aa7
GoonLee <auteur.kor@gmail.com>
2022-01-19 10:37:09 +0900

    Implement mission 2-2: remove unnecssary field of ArticleRegisterRequestDto

--- src/main/java/com/kakao/cafe/dto/article/ArticleRegisterRequestDto.java
+++ src/main/java/com/kakao/cafe/dto/article/ArticleRegisterRequestDto.java
@@ -4,21 +4,14 @@ import javax.validation.constraints.NotBlank;
 
 public class ArticleRegisterRequestDto {
 
-    @NotBlank(message = "필수 입력 항목이 누락되었습니다: 작성자 아이디")
-    private final String userName;
     @NotBlank(message = "필수 입력 항목이 누락되었습니다: 제목")
     private final String title;
     @NotBlank(message = "필수 입력 항목이 누락되었습니다: 내용")
     private final String content;
 
-    public ArticleRegisterRequestDto(String userName, String title, String content) {
-        this.userName = userName;
+    public ArticleRegisterRequestDto(String title, String content) {
         this.title = title;
         this.content = content;
-    }
-
-    public String getUserName() {
-        return userName;
     }
 
     public String getTitle() {
