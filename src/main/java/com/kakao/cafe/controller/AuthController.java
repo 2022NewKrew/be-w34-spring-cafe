8e119fa8b17d4b1803306b064a5c7bfbe7f162b1
Dongchan KIM <fromecha@gmail.com>
2022-01-19 16:45:18 +0900

    [feat] authcontroller login 시 session 에 로그인된 user 객체 추가

---
+++ src/main/java/com/kakao/cafe/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.kakao.cafe.controller;
+
+import com.kakao.cafe.dto.UserLoginDto;
+import com.kakao.cafe.entity.User;
+import com.kakao.cafe.service.AuthService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public String login(UserLoginDto userLoginDto, HttpSession httpSession) {
+        User loginUser = authService.login(userLoginDto);
+        httpSession.setAttribute("auth", loginUser);
+        return "redirect:/";
+    }
+
+
+
+}
