8db6e9909dfcda080d129a00809b1c9ad1cd2439
Hongjeongmin <wjdals0471@naver.com>
2022-01-20 14:38:46 +0900

    REPACTORING : Dto ModelMapper 제거 후 구현

--- src/main/java/com/kakao/cafe/controller/QuestionController.java
+++ src/main/java/com/kakao/cafe/controller/QuestionController.java
@@ -1,5 +1,18 @@
 package com.kakao.cafe.controller;
 
+import java.sql.SQLException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.modelmapper.ModelMapper;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
 import com.kakao.cafe.common.exception.BaseException;
 import com.kakao.cafe.controller.common.SessionLoginUser;
 import com.kakao.cafe.question.Question;
@@ -9,32 +22,33 @@ import com.kakao.cafe.question.dto.QuestionDto;
 import com.kakao.cafe.question.dto.QuestionUpdateDto;
 import com.kakao.cafe.user.User;
 import com.kakao.cafe.user.dto.UserDto;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 @RequestMapping("/questions")
 @RequiredArgsConstructor
 public class QuestionController {
-
+    private static final Long NOT_FOUND_MEMBER_ID = -1L;
     private final QuestionService questionService;
     private final ModelMapper modelMapper;
     private final SessionLoginUser sessionLoginUser;
-    private static final Long NOT_FOUND_MEMBER_ID = -1L;
+
+    @DeleteMapping("/{id}")
+    public String deleteQuestion(@PathVariable("id") Long id, Model model) throws BaseException {
+        Long memberId = getMemberId();
+
+        questionService.deleteOne(id, memberId);
+
+        return "redirect:/questions";
+    }
 
     @PostMapping("/create")
-    public String insertQuestion(@ModelAttribute("question") @Valid QuestionCreateDto questionCreateDto, Model model) throws BaseException, SQLException {
-
+    public String insertQuestion(@ModelAttribute("question")
+                                 @Valid QuestionCreateDto questionCreateDto, Model model) throws BaseException, SQLException {
         User user = modelMapper.map(sessionLoginUser.getLoginUser(), User.class);
         Question question = modelMapper.map(questionCreateDto, Question.class);
 
@@ -45,62 +59,9 @@ public class QuestionController {
         return "redirect:/questions";
     }
 
-    @GetMapping
-    public String viewQuestionList(Model model) {
-
-        Long id = getMemberId();
-        List<QuestionDto> questions = getQuestionDtos(id);
-
-        model.addAttribute("questions", questions);
-        model.addAttribute("size", questions.size());
-
-        return "qna/list";
-
-    }
-
-    private List<QuestionDto> getQuestionDtos(Long id) {
-
-        return questionService.findAll()
-                .stream()
-                .map(q -> {
-                    QuestionDto question = modelMapper.map(q, QuestionDto.class);
-                    question.setThisIsMine(id.equals(q.getMemberId()));
-                    return question;
-                })
-                .collect(Collectors.toList());
-    }
-
-    @GetMapping("/{id}")
-    public String viewQuestionDetail(@PathVariable("id") Long id, Model model) {
-
-        Long memberId = getMemberId();
-        QuestionDto question = modelMapper.map(questionService.findOne(id), QuestionDto.class);
-
-        question.setThisIsMine(memberId.equals(question.getMemberId()));
-        model.addAttribute("question", question);
-
-        return "qna/detail";
-
-    }
-
-    @GetMapping("/create")
-    public String viewQuestionForm(Model model) {
-        return "qna/form";
-    }
-
-    @DeleteMapping("/{id}")
-    public String deleteQuestion(@PathVariable("id") Long id, Model model) throws BaseException {
-
-        Long memberId = getMemberId();
-
-        questionService.deleteOne(id, memberId);
-
-        return "redirect:/questions";
-    }
-
     @PutMapping("/update/{id}")
-    public String updateQuestion(@PathVariable("id") Long id, @Valid @ModelAttribute("question") QuestionUpdateDto questionUpdateDto, Model model) throws BaseException {
-
+    public String updateQuestion(@PathVariable("id") Long id, @Valid
+    @ModelAttribute("question") QuestionUpdateDto questionUpdateDto, Model model) throws BaseException {
         Long memberId = getMemberId();
         Question question = new Question();
 
@@ -108,20 +69,46 @@ public class QuestionController {
         question.setMemberId(memberId);
         question.setTitle(questionUpdateDto.getTitle());
         question.setContents(questionUpdateDto.getContents());
-
         questionService.updateOne(question);
 
         return String.format("redirect:/questions/%d", id);
     }
 
+    @GetMapping("/{id}")
+    public String viewQuestionDetail(@PathVariable("id") Long id, Model model) {
+        Long memberId = getMemberId();
+        QuestionDto question = modelMapper.map(questionService.findOne(id), QuestionDto.class);
+
+        question.setThisIsMine(memberId.equals(question.getMemberId()));
+        model.addAttribute("question", question);
+
+        return "qna/detail";
+    }
+
+    @GetMapping("/create")
+    public String viewQuestionForm(Model model) {
+        return "qna/form";
+    }
+
+    @GetMapping
+    public String viewQuestionList(Model model) {
+
+        Long memberId = getMemberId();
+        List<QuestionDto> questions = QuestionDto.of(questionService.findAll(), memberId);
+
+        model.addAttribute("questions", questions);
+        model.addAttribute("size", questions.size());
+
+        return "qna/list";
+    }
+
     @GetMapping("/update/{id}")
     public String viewUpdateQuestionForm(@PathVariable("id") Long id, Model model) throws BaseException {
-
         Question origin = questionService.findOne(id);
         QuestionUpdateDto question = modelMapper.map(origin, QuestionUpdateDto.class);
         UserDto loginUser = (UserDto) sessionLoginUser.getLoginUser();
 
-        if (origin != null && !origin.getMemberId().equals(loginUser.getId())) {
+        if ((origin != null) && !origin.getMemberId().equals(loginUser.getId())) {
             throw new BaseException("게시글의 권한이 없는 사용자 입니다.");
         }
 
@@ -131,8 +118,27 @@ public class QuestionController {
     }
 
     private Long getMemberId() {
-
         UserDto loginUser = (UserDto) sessionLoginUser.getLoginUser();
-        return loginUser == null ? NOT_FOUND_MEMBER_ID : loginUser.getId();
+
+        return (loginUser == null)
+                ? NOT_FOUND_MEMBER_ID
+                : loginUser.getId();
+    }
+
+    private List<QuestionDto> getQuestionDtos(Long memberId) {
+        return questionService.findAll()
+                .stream()
+                .map(
+                        q -> {
+                            QuestionDto question = modelMapper.map(q, QuestionDto.class);
+
+                            question.setThisIsMine(memberId.equals(q.getMemberId()));
+
+                            return question;
+                        })
+                .collect(Collectors.toList());
     }
 }
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
--- src/main/java/com/kakao/cafe/question/dto/QuestionDto.java
+++ src/main/java/com/kakao/cafe/question/dto/QuestionDto.java
@@ -1,9 +1,12 @@
 package com.kakao.cafe.question.dto;
 
+import com.kakao.cafe.question.Question;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 질문글 리스트에 대한 DTO 입니다.
@@ -20,4 +23,24 @@ public class QuestionDto {
     private String contents;
     private LocalDateTime createTime;
     private boolean thisIsMine;
+
+    public static List<QuestionDto> of(List<Question> questions, Long memberId) {
+        return questions.stream()
+                .map(q ->
+                        QuestionDto.of(q, memberId)
+                )
+                .collect(Collectors.toList());
+    }
+
+    public static QuestionDto of(Question q, Long memberId) {
+        QuestionDto questionDto = new QuestionDto();
+        questionDto.setId(q.getId());
+        questionDto.setContents(q.getContents());
+        questionDto.setMemberId(q.getMemberId());
+        questionDto.setTitle(q.getTitle());
+        questionDto.setWriter(q.getWriter());
+        questionDto.setCreateTime(q.getCreateTime());
+        questionDto.setThisIsMine(memberId.equals(q.getMemberId()));
+        return questionDto;
+    }
 }
