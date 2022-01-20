ca84320c749f4cf3ed8ee75c16fe4daf799b542d
jh Lim <jhwlim25@gmail.com>
2022-01-20 16:06:41 +0900

    feat : 댓글 삭제 기능 구현

--- src/main/java/com/kakao/cafe/controller/PostCommentController.java
+++ src/main/java/com/kakao/cafe/controller/PostCommentController.java
@@ -6,6 +6,7 @@ import com.kakao.cafe.utils.SessionUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -25,6 +26,14 @@ public class PostCommentController {
         long currentUserId = SessionUtils.getCurrentUserId(session);
         commentService.saveComment(postId, currentUserId, request);
         rttr.addFlashAttribute("msg", "댓글을 등록하였습니다.");
+        return "redirect:/posts/" + postId;
+    }
+
+    @DeleteMapping("/posts/{postId}/comments/{id}")
+    public String deleteComment(@PathVariable long postId, @PathVariable long id, HttpSession session, RedirectAttributes rttr) {
+        long currentUserId = SessionUtils.getCurrentUserId(session);
+        commentService.deleteById(id, postId, currentUserId);
+        rttr.addFlashAttribute("msg", "댓글을 삭제하였습니다.");
         return "redirect:/posts/" + postId;
     }
 
