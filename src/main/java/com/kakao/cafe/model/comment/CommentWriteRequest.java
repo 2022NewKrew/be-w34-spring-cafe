5c9ef6576684caf96e4d00f09a39a116336eb1af
jh Lim <jhwlim25@gmail.com>
2022-01-20 15:13:13 +0900

    feat : 댓글 등록 기능 구현

--- src/main/java/com/kakao/cafe/model/comment/CommentWriteRequest.java
+++ src/main/java/com/kakao/cafe/model/comment/CommentWriteRequest.java
@@ -2,10 +2,13 @@ package com.kakao.cafe.model.comment;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+
 @ToString
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CommentWriteRequest {
+    @NotBlank(message = "댓글 내용을 입력해주세요.")
     private String content;
 
     @Builder
