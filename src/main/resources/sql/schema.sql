769fbb33052c46c30054afc04fd240d17c6ef211
GoonLee <auteur.kor@gmail.com>
2022-01-21 02:07:56 +0900

    Implement mission 2-3: add database schema of reply

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,7 +1,4 @@
-DROP TABLE IF EXISTS articles;
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users
+CREATE TABLE IF NOT EXISTS users
 (
     users_id UUID DEFAULT UUID() NOT NULL,
     username VARCHAR UNIQUE NOT NULL,
@@ -11,15 +8,28 @@ CREATE TABLE users
     PRIMARY KEY(users_id)
 );
 
-CREATE TABLE articles
+CREATE TABLE IF NOT EXISTS articles
 (
     articles_id UUID DEFAULT UUID() NOT NULL,
     title VARCHAR NOT NULL,
     content CLOB NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     view_count INT DEFAULT 0 NOT NULL,
-    deleted BOOLEAN DEFAULT FALSE,
+    deleted BOOLEAN DEFAULT FALSE NOT NULL,
     users_id UUID NOT NULL,
     PRIMARY KEY(articles_id),
     FOREIGN KEY(users_id) REFERENCES users
 );
+
+CREATE TABLE IF NOT EXISTS replies
+(
+    replies_id UUID DEFAULT UUID() NOT NULL,
+    comment VARCHAR NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    deleted BOOLEAN DEFAULT FALSE NOT NULL,
+    users_id UUID NOT NULL,
+    articles_id UUID NOT NULL,
+    PRIMARY KEY (replies_id),
+    FOREIGN KEY (users_id) REFERENCES users,
+    FOREIGN KEY (articles_id) REFERENCES articles
+)
