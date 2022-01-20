119b68934e4558abc04a41a391ec33e2c6c23d18
Taekyung2 <ltk3934@daum.net>
2022-01-20 15:33:58 +0900

    feat : 게시글, 댓글 Entity 생성 및 SCHEMA 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,71 @@
 DROP TABLE IF EXISTS USERS;
-CREATE TABLE IF NOT EXISTS USERS (
-                                     id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                                     user_id VARCHAR NOT NULL UNIQUE,
-                                     password VARCHAR NOT NULL,
-                                     name VARCHAR,
-                                     email VARCHAR
+CREATE TABLE IF NOT EXISTS USERS
+(
+    id
+        BIGINT
+        NOT NULL
+        PRIMARY KEY AUTO_INCREMENT,
+    user_id
+        VARCHAR
+        NOT NULL
+        UNIQUE,
+    password
+        VARCHAR
+        NOT NULL,
+    name
+        VARCHAR,
+    email
+        VARCHAR
 );
 
 DROP TABLE IF EXISTS ARTICLE;
-CREATE TABLE IF NOT EXISTS ARTICLE (
-                                       id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                                       author VARCHAR NOT NULL,
-                                       title VARCHAR NOT NULL,
-                                       content CLOB,
-                                       created_at DATETIME,
-                                       updated_at DATETIME
+CREATE TABLE IF NOT EXISTS ARTICLES
+(
+    id
+        BIGINT
+        NOT NULL
+        PRIMARY KEY AUTO_INCREMENT,
+    writer_id
+        BIGINT
+        NOT NULL
+        REFERENCES USERS (id),
+    title
+        VARCHAR
+        NOT NULL,
+    content
+        CLOB
+        NOT NULL,
+    created_at
+        DATETIME
+        DEFAULT NOW(),
+    updated_at
+        DATETIME
+        DEFAULT NOW(),
+    deleted
+        BOOLEAN
+        NOT NULL
+        DEFAULT FALSE
+);
+
+DROP TABLE IF EXISTS REPLYS;
+CREATE TABLE IF NOT EXISTS REPLYS
+(
+    id
+        BIGINT
+        NOT NULL
+        PRIMARY KEY AUTO_INCREMENT,
+    writer_id
+        BIGINT
+        NOT NULL
+        REFERENCES USERS (id),
+    content
+        VARCHAR
+        NOT NULL,
+    created_at
+        DATETIME
+        DEFAULT NOW(),
+    deleted
+        BOOLEAN
+        NOT NULL
+        DEFAULT FALSE
 );
