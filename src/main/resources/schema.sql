3fb4e2d57f1df4f89ab2146f0342903d0edb29e0
jh Lim <jhwlim25@gmail.com>
2022-01-20 16:11:50 +0900

    chore : 게시글 삭제시 참조하고 있는 comment 삭제를 위한 comment 테이블 스키마 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,31 +2,34 @@ DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE IF NOT EXISTS users (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    nickname VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS users
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    email      VARCHAR(100) UNIQUE NOT NULL,
+    nickname   VARCHAR(50)         NOT NULL,
+    password   VARCHAR(255)        NOT NULL,
     created_at DATETIME,
     updated_at DATETIME
 );
 
-CREATE TABLE IF NOT EXISTS posts (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    writer_id BIGINT NOT NULL,
-    title VARCHAR(100) NOT NULL,
-    content TEXT NOT NULL,
+CREATE TABLE IF NOT EXISTS posts
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    writer_id  BIGINT       NOT NULL,
+    title      VARCHAR(100) NOT NULL,
+    content    TEXT         NOT NULL,
     created_at DATETIME,
     updated_at DATETIME,
-    FOREIGN KEY (writer_id) REFERENCES users(id)
+    FOREIGN KEY (writer_id) REFERENCES users (id)
 );
 
-CREATE TABLE IF NOT EXISTS comments (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    post_id BIGINT NOT NULL,
-    writer_id BIGINT NOT NULL,
-    content TEXT NOT NULL,
+CREATE TABLE IF NOT EXISTS comments
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    post_id    BIGINT NOT NULL,
+    writer_id  BIGINT NOT NULL,
+    content    TEXT   NOT NULL,
     created_at DATETIME,
-    FOREIGN KEY (post_id) REFERENCES posts(id),
-    FOREIGN KEY (writer_id) REFERENCES users(id)
+    FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE,
+    FOREIGN KEY (writer_id) REFERENCES users (id)
 );
