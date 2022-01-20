a55840ca9ef21c894008be4fe26386343905d5f2
Hongjeongmin <wjdals0471@naver.com>
2022-01-20 14:51:38 +0900

    REFACTORING : QuestionController 함수 정리

--- src/main/java/com/kakao/cafe/controller/QuestionController.java
+++ src/main/java/com/kakao/cafe/controller/QuestionController.java
@@ -1,18 +1,5 @@
 package com.kakao.cafe.controller;
 
-import java.sql.SQLException;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.validation.Valid;
-
-import org.modelmapper.ModelMapper;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
 import com.kakao.cafe.common.exception.BaseException;
 import com.kakao.cafe.controller.common.SessionLoginUser;
 import com.kakao.cafe.question.Question;
@@ -22,16 +9,23 @@ import com.kakao.cafe.question.dto.QuestionDto;
 import com.kakao.cafe.question.dto.QuestionUpdateDto;
 import com.kakao.cafe.user.User;
 import com.kakao.cafe.user.dto.UserDto;
-
 import lombok.RequiredArgsConstructor;
-
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.sql.SQLException;
+import java.util.List;
 
 @Slf4j
 @Controller
 @RequestMapping("/questions")
 @RequiredArgsConstructor
 public class QuestionController {
+
     private static final Long NOT_FOUND_MEMBER_ID = -1L;
     private final QuestionService questionService;
     private final ModelMapper modelMapper;
@@ -39,6 +33,7 @@ public class QuestionController {
 
     @DeleteMapping("/{id}")
     public String deleteQuestion(@PathVariable("id") Long id, Model model) throws BaseException {
+
         Long memberId = getMemberId();
 
         questionService.deleteOne(id, memberId);
@@ -47,8 +42,8 @@ public class QuestionController {
     }
 
     @PostMapping("/create")
-    public String insertQuestion(@ModelAttribute("question")
-                                 @Valid QuestionCreateDto questionCreateDto, Model model) throws BaseException, SQLException {
+    public String insertQuestion(@ModelAttribute("question") @Valid QuestionCreateDto questionCreateDto, Model model) throws BaseException, SQLException {
+
         User user = modelMapper.map(sessionLoginUser.getLoginUser(), User.class);
         Question question = modelMapper.map(questionCreateDto, Question.class);
 
@@ -60,15 +55,13 @@ public class QuestionController {
     }
 
     @PutMapping("/update/{id}")
-    public String updateQuestion(@PathVariable("id") Long id, @Valid
-    @ModelAttribute("question") QuestionUpdateDto questionUpdateDto, Model model) throws BaseException {
+    public String updateQuestion(@PathVariable("id") Long id, @Valid @ModelAttribute("question") QuestionUpdateDto questionUpdateDto, Model model) throws BaseException {
+
         Long memberId = getMemberId();
-        Question question = new Question();
+        Question question = modelMapper.map(questionUpdateDto, Question.class);
 
         question.setId(id);
         question.setMemberId(memberId);
-        question.setTitle(questionUpdateDto.getTitle());
-        question.setContents(questionUpdateDto.getContents());
         questionService.updateOne(question);
 
         return String.format("redirect:/questions/%d", id);
@@ -76,10 +69,10 @@ public class QuestionController {
 
     @GetMapping("/{id}")
     public String viewQuestionDetail(@PathVariable("id") Long id, Model model) {
-        Long memberId = getMemberId();
-        QuestionDto question = modelMapper.map(questionService.findOne(id), QuestionDto.class);
 
-        question.setThisIsMine(memberId.equals(question.getMemberId()));
+        Long memberId = getMemberId();
+        QuestionDto question = QuestionDto.of(questionService.findOne(id), memberId);
+
         model.addAttribute("question", question);
 
         return "qna/detail";
@@ -104,6 +97,7 @@ public class QuestionController {
 
     @GetMapping("/update/{id}")
     public String viewUpdateQuestionForm(@PathVariable("id") Long id, Model model) throws BaseException {
+
         Question origin = questionService.findOne(id);
         QuestionUpdateDto question = modelMapper.map(origin, QuestionUpdateDto.class);
         UserDto loginUser = (UserDto) sessionLoginUser.getLoginUser();
@@ -118,27 +112,9 @@ public class QuestionController {
     }
 
     private Long getMemberId() {
+
         UserDto loginUser = (UserDto) sessionLoginUser.getLoginUser();
 
-        return (loginUser == null)
-                ? NOT_FOUND_MEMBER_ID
-                : loginUser.getId();
-    }
-
-    private List<QuestionDto> getQuestionDtos(Long memberId) {
-        return questionService.findAll()
-                .stream()
-                .map(
-                        q -> {
-                            QuestionDto question = modelMapper.map(q, QuestionDto.class);
-
-                            question.setThisIsMine(memberId.equals(q.getMemberId()));
-
-                            return question;
-                        })
-                .collect(Collectors.toList());
+        return (loginUser == null) ? NOT_FOUND_MEMBER_ID : loginUser.getId();
     }
 }
-
-
-//~ Formatted by Jindent --- http://www.jindent.com
