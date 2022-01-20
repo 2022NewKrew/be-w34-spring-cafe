f40844b1ae2bf2a9f2631ef31e4e756e992c6204
Kimgeunwook <wook961206@naver.com>
2022-01-20 16:33:41 +0900

    Feat: mysql krane에 배포

--- src/main/java/com/kakao/cafe/query/article/H2RepositoryQuery.java
+++ src/main/java/com/kakao/cafe/query/article/H2RepositoryQuery.java
@@ -1,10 +1,10 @@
 package com.kakao.cafe.query.article;
 
 public class H2RepositoryQuery {
-    public static final String insertQuery = "insert into article (writer, title, contents) values(?, ?, ?)";
-    public static final String selectAllQuery = "select * from article";
-    public static final String selectByIdQuery = "select * from article where id = ?";
-    public static final String deleteByIdQuery = "delete from article where id = ?";
-    public static final String deleteByWriterQuery = "delete from article where writer = ?";
+    public static final String insertQuery = "insert into ARTICLE (writer, title, contents) values(?, ?, ?)";
+    public static final String selectAllQuery = "select * from ARTICLE";
+    public static final String selectByIdQuery = "select * from ARTICLE where id = ?";
+    public static final String deleteByIdQuery = "delete from ARTICLE where id = ?";
+    public static final String deleteByWriterQuery = "delete from ARTICLE where writer = ?";
     public static final String updateQuery = "UPDATE ARTICLE SET title = ?, contents = ? WHERE ID = ?";
 }
--- src/main/java/com/kakao/cafe/query/reply/RepositoryQuery.java
+++ src/main/java/com/kakao/cafe/query/reply/RepositoryQuery.java
@@ -1,7 +1,7 @@
 package com.kakao.cafe.query.reply;
 
 public class RepositoryQuery {
-    public static final String selectByIdQuery = "select * from reply where FK_article_id = ?";
-    public static final String insertQuery = "insert into reply (writer, contents, FK_article_id) values (?,?,?)";
-    public static final String deleteByIdQuery = "delete from reply where id = ?";
+    public static final String selectByIdQuery = "select * from REPLY where FK_article_id = ?";
+    public static final String insertQuery = "insert into REPLY (writer, contents, FK_article_id) values (?,?,?)";
+    public static final String deleteByIdQuery = "delete from REPLY where id = ?";
 }
