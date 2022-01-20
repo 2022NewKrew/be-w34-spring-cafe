1000a9364a0149e6c0f888fb003442293b61bc38
punsoo <sonu.sohn@kakaocorp.com@gmail.com>
2022-01-21 04:36:56 +0900

    feat:MYSQL 연동

--- src/main/java/com/kakao/cafe/config/SpringJdbcConfig.java
+++ src/main/java/com/kakao/cafe/config/SpringJdbcConfig.java
@@ -1,21 +1,14 @@
 package com.kakao.cafe.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class SpringJdbcConfig {
-    @Bean
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2)
-                .setName("kakaodb")
-                .addScript("classpath:/jdbc/schema.sql")
-                .addScript("classpath:/jdbc/data.sql")
-                .build();
-    }
-}
+//@Configuration
+//public class SpringJdbcConfig {
+//    @Bean
+//    public DataSource dataSource() {
+//        return new EmbeddedDatabaseBuilder()
+//                .setType(EmbeddedDatabaseType.H2)
+//                .setName("kakaodb")
+//                .addScript("classpath:/jdbc/schema.sql")
+//                .addScript("classpath:/jdbc/data.sql")
+//                .build();
+//    }
+//}
