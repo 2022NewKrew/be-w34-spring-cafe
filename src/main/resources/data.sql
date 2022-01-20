7fc3704a85815a019eacb91a07e0ea615596d907
mylo.l <mylo.l@kakaocorp.com>
2022-01-21 00:24:33 +0900

    Step2-3 DB Foreign Key 추가, reply entity 생성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO users (userId, password, name, email) VALUES ('admin', '1234', 'admin', 'admin@kakaocorp.com');
-INSERT INTO articles (writer, title, contents, deleted) VALUES ('Admin', 'adminTest', 'We are testing Now', false);
+INSERT INTO articles (writer, title, contents, deleted) VALUES ('admin', 'adminTest', 'We are testing Now', false);
+INSERT INTO replies (articleid, writer, contents, deleted) VALUES (1, 'admin', 'reply testing Now', false);
