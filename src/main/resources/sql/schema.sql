8ba43a4b0b5f92ac14ef6c160dacb0d428ff6461
edward.baek <edward.baek@kakaocorp.com>
2022-01-20 14:25:45 +0900

    feat : commnet 스키마 추가

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -16,3 +16,12 @@ create table USER_ACCOUNT (
     created_at timestamp,
     primary key (user_account_id)
 );
+
+create table COMMENT (
+    comment_id bigint auto_increment,
+    content varchar(100),
+    created_at timestamp,
+    question_post_id bigint,
+    user_account_id bigint,
+    primary key (comment_id)
+);
