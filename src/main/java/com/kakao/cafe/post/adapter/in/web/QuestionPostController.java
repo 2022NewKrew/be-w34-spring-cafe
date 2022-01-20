12e0dffff8a003f3d0882f8b0726b47f0a520c12
edward.baek <edward.baek@kakaocorp.com>
2022-01-20 14:27:00 +0900

    feat : post에서 comment을 가져오도록 추가

--- src/main/java/com/kakao/cafe/post/adapter/in/web/QuestionPostController.java
+++ src/main/java/com/kakao/cafe/post/adapter/in/web/QuestionPostController.java
@@ -1,5 +1,8 @@
 package com.kakao.cafe.post.adapter.in.web;
 
+import com.kakao.cafe.comment.application.dto.command.GetRelatedPostCommentCommand;
+import com.kakao.cafe.comment.application.dto.result.GetRelatedPostCommentResult;
+import com.kakao.cafe.comment.application.port.in.GetCommentUseCase;
 import com.kakao.cafe.post.application.dto.command.QuestionPostClickCommand;
 import com.kakao.cafe.post.application.dto.command.QuestionPostDetailCommand;
 import com.kakao.cafe.post.application.dto.result.QuestionPostDetailResult;
@@ -21,13 +24,18 @@ import java.util.Objects;
 public class QuestionPostController {
 
     private final GetQuestionPostUseCase getQuestionPostUseCase;
-    private final UpdateQuestionPostUseCase updateQuestionPostUseCase;
+    private final GetCommentUseCase getCommentUseCase;
 
     @GetMapping("/{post-id}/detail")
     public String postDetail(@PathVariable(name = "post-id") Long postId, Model model, HttpSession httpSession) {
-        QuestionPostDetailResult postDetail = getQuestionPostUseCase.getPostDetail(new QuestionPostDetailCommand(postId));
-        updateQuestionPostUseCase.clickPost(new QuestionPostClickCommand(postId));
+        QuestionPostDetailResult postDetail = getQuestionPostUseCase.getPostDetail(
+                new QuestionPostDetailCommand(postId));
+
+        GetRelatedPostCommentResult relatedComment = getCommentUseCase.getRelatedPost(
+                new GetRelatedPostCommentCommand(postId));
+
         model.addAttribute("post", postDetail);
+        model.addAttribute("comment", relatedComment.getCommentResults());
 
         if(Objects.equals(httpSession.getAttribute("user-id"), postDetail.getUserAccountId())) {
             return "updateqnadetail";
