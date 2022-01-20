6e81ee2b26375d34e137bb9466f543b7a440f00d
flip1945 <coghdus1016@naver.com>
2022-01-20 21:24:24 +0900

    feat: Krane 서가에 도커로 배포하기 위한 설정 추가

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM amazoncorretto:11
+ARG JAR_FILE=./build/libs/cafe-0.0.1-SNAPSHOT.jar
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO MEMBER (email, password, nickname, create_date) VALUES ('javajigi@slipp.net', 'test', '자바지기', CURRENT_DATE());
-INSERT INTO MEMBER (email, password, nickname, create_date) VALUES ('sanjigi@slipp.net', 'test', '산지기', CURRENT_DATE());
