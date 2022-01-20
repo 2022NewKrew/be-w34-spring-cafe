cc64a637ea15a2af6d1654d2da10c2a8ff1275ef
Hongjeongmin <wjdals0471@naver.com>
2022-01-20 15:43:22 +0900

    REFACTORING : ReplyDto of 메서드 작성, 문구 변경 트랜잭셔날 추가

--- src/main/java/com/kakao/cafe/reply/ReplyServiceImpl.java
+++ src/main/java/com/kakao/cafe/reply/ReplyServiceImpl.java
@@ -3,12 +3,14 @@ package com.kakao.cafe.reply;
 import com.kakao.cafe.common.exception.BaseException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class ReplyServiceImpl implements ReplyService {
 
     private final ReplyRepository replyRepository;
