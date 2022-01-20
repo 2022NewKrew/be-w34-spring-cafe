cdae5f09750a810fa51456faec34efd0ae33b7ee
green.day <green.day@kakaocorp.com>
2022-01-20 17:57:59 +0900

    feat: 글 수정/삭제 시 본인확인을 인터셉터에서 할 수 있도록 변경

--- src/main/java/com/kakao/cafe/configuration/WebMvcConfig.java
+++ src/main/java/com/kakao/cafe/configuration/WebMvcConfig.java
@@ -1,16 +1,19 @@
 package com.kakao.cafe.configuration;
 
 import com.kakao.cafe.interceptor.SessionInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final SessionInterceptor sessionInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        SessionInterceptor sessionInterceptor = new SessionInterceptor();
         registry.addInterceptor(sessionInterceptor)
                 .addPathPatterns(sessionInterceptor.loginEssential)
                 .excludePathPatterns();
--- src/main/java/com/kakao/cafe/interceptor/SessionInterceptor.java
+++ src/main/java/com/kakao/cafe/interceptor/SessionInterceptor.java
@@ -10,29 +10,53 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.PatternMatchUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
+@RequiredArgsConstructor
 @Component
 public class SessionInterceptor implements HandlerInterceptor {
 
-    public List<String> loginEssential =
-            Arrays.asList("/user/profile/**", "/user/logout", "/article/**");
+    public final List<String> loginEssential =
+            Arrays.asList("/user/profile/**", "/user/logout",  "/article/*", "/article/**");
 
+    private final String[] sameUserEssential = {"/article/modify/*", "/article/delete/*"};
+
+    private final ArticleService articleService;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Object value = request.getSession().getAttribute("sessionedUser");
 
-        if(Objects.isNull(value)) response.sendRedirect("/user/login");
+        // 세션 있는지 검사
+        if (Objects.isNull(value)) {
+            response.sendRedirect("/user/login");
+            return false;
+        }
 
-        return true;
+        // 본인 인증이 필요한 uri인지 검사
+        if (!PatternMatchUtils.simpleMatch(sameUserEssential, request.getRequestURI())) {
+            return true;
+        }
+
+        // 본인 검사 - article에서만 현재 필요, reply 구현 예정
+        User user = (User) value;
+        Map<?, ?> pathVariables = (Map<?, ?>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        String articleId = (String) pathVariables.get("id");
+        if (articleId == null || articleId.isBlank()) return false;
+
+        Article article = articleService.findArticleById(articleId);
+
+        return user.isSameUser(article.getUserId());
     }
 
     @Override
