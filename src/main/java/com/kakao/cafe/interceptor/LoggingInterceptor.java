bc74a2e3764b9c2c55217e2a77b3ce3a9b4cd11a
김남현 <kssrcn@naver.com>
2022-01-21 18:07:38 +0900

    logging 인터셉터 추가

---
+++ src/main/java/com/kakao/cafe/interceptor/LoggingInterceptor.java
@@ -0,0 +1,17 @@
+package com.kakao.cafe.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("HTTP URI:{}, METHOD:{}", request.getRequestURI(), request.getMethod());
+        return true;
+    }
+}
