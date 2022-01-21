8d294e70a66f8f275fda18d4a6c5676d6d4ac243
Dongchan KIM <fromecha@gmail.com>
2022-01-21 09:19:48 +0900

    [feat] 로그아웃 시 세션 초기화

--- src/main/java/com/kakao/cafe/controller/AuthController.java
+++ src/main/java/com/kakao/cafe/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.kakao.cafe.dto.UserLoginDto;
 import com.kakao.cafe.entity.User;
 import com.kakao.cafe.service.AuthService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
@@ -21,6 +22,12 @@ public class AuthController {
     public String login(UserLoginDto userLoginDto, HttpSession httpSession) {
         User loginUser = authService.login(userLoginDto);
         httpSession.setAttribute("auth", loginUser);
+        return "redirect:/";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession httpSession) {
+        httpSession.invalidate();
         return "redirect:/";
     }
 }
