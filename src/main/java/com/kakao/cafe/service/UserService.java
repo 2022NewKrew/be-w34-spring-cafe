1e739e3e927adc7df2594df5bd357c54c910b415
keith.lee <keith.lee@kakaocorp.com>
2022-01-21 15:16:37 +0900

    UserListShow를 controller에서만 사용하도록 수정

--- src/main/java/com/kakao/cafe/service/UserService.java
+++ src/main/java/com/kakao/cafe/service/UserService.java
@@ -5,6 +5,7 @@ import com.kakao.cafe.dto.user.*;
 import com.kakao.cafe.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
@@ -27,12 +28,7 @@ public class UserService {
         return new UserProfileInfo(userRepository.search(userId));
     }
 
-    public List<UserListShow> getAllUsers() {
-        AtomicLong index = new AtomicLong(1);
-        return userRepository.getAllUser().stream()
-                .map(user -> new UserListShow(index.getAndIncrement(), user))
-                .collect(Collectors.toUnmodifiableList());
-    }
+    public List<User> getAllUsers() { return Collections.unmodifiableList(userRepository.getAllUser()); }
 
     public void createUser(UserCreateCommand ucc) {
         userRepository.store(ucc);
