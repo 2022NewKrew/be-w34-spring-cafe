02936250c8b94ab801221965ada258f874407fb6
JuyeoungJun <wjfhs01@gmail.com>
2022-01-23 23:15:21 +0900

    feat: Time Zone을 application 이 실행될때 설정하도록 변경 (step 2-5 리뷰 반영)

--- src/main/java/com/kakao/cafe/CafeApplication.java
+++ src/main/java/com/kakao/cafe/CafeApplication.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @PropertySource(value = {"classpath:mustache.properties"})
 public class CafeApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CafeApplication.class, args);
