f40b1fc3e3b61c0c97583a30c39aff46a4802f04
ato.mung <ato.mung@kakaocorp.com>
2022-01-24 18:09:40 +0900

    refactor: 에러핸들링을 서비스로 변경

--- src/main/java/com/kakao/cafe/user/repository/SessionMapRepository.java
+++ src/main/java/com/kakao/cafe/user/repository/SessionMapRepository.java
@@ -1,9 +1,9 @@
 package com.kakao.cafe.user.repository;
 
 import com.kakao.cafe.user.domain.User;
-import com.kakao.cafe.user.exception.CustomInvalidedSessionException;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Repository;
@@ -14,16 +14,8 @@ public class SessionMapRepository implements SessionRepository {
     private final Map<UUID, User> sessions = new ConcurrentHashMap<>();
 
     @Override
-    public User findBySessionId(UUID uuid) {
-        User user = sessions.get(uuid);
-        validateUser(user);
-        return user;
-    }
-
-    private void validateUser(User user) {
-        if (Objects.isNull(user)) {
-            throw new CustomInvalidedSessionException();
-        }
+    public Optional<User> findBySessionId(UUID uuid) {
+        return Optional.of(sessions.get(uuid));
     }
 
     @Override
--- src/main/java/com/kakao/cafe/user/repository/SessionRepository.java
+++ src/main/java/com/kakao/cafe/user/repository/SessionRepository.java
@@ -1,11 +1,12 @@
 package com.kakao.cafe.user.repository;
 
 import com.kakao.cafe.user.domain.User;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SessionRepository {
 
-    User findBySessionId(UUID uuid);
+    Optional<User> findBySessionId(UUID uuid);
 
     void save(UUID uuid, User user);
 
