fe007e655e429fcffe1c4c4af0ecf1843ba23282
MELT <melt.ep00@gmail.com>
2022-02-03 20:24:31 +0900

    스프링 카페 추가 구현 - `NoSuchUserIdException` 추가

--- src/main/java/com/kakao/cafe/system/LoginService.java
+++ src/main/java/com/kakao/cafe/system/LoginService.java
@@ -1,8 +1,12 @@
 package com.kakao.cafe.system;
 
+import com.kakao.cafe.exception.NoSuchUserIdException;
 import com.kakao.cafe.user.User;
 import com.kakao.cafe.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,16 +21,23 @@ public class LoginService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final Logger log = LoggerFactory.getLogger(LoginService.class);
 
     public User login(String userId, String password) {
-        User loginUser = userRepository.findUserByUserId(userId);
+        try {
+            User loginUser = userRepository.findUserByUserId(userId);
 
-        if (loginUser == null) {
-            return null;
-        }
+            if (loginUser == null) {
+                return null;
+            }
 
-        if (passwordEncoder.matches(password, loginUser.getPassword())) {
-            return loginUser;
+            if (passwordEncoder.matches(password, loginUser.getPassword())) {
+                return loginUser;
+            }
+
+        } catch (DataAccessException e) {
+            log.error("일치하는 userId가 존재하지 않습니다.");
+            throw new NoSuchUserIdException();
         }
         return null;
     }
