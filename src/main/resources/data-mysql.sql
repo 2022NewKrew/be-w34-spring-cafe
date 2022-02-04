197d8753566f8411325b24ed3170a011a38ca8d8
MELT <melt.ep00@gmail.com>
2022-02-04 17:59:54 +0900

    스프링 카페 추가 구현 - USERS 행 추가 삭제

--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -1,5 +1,2 @@
-INSERT INTO USERS (id, user_id, password, name, email) VALUES (1, 'javajigi', 'test', '자바지기', 'javajigi@slipp.net');
-INSERT INTO USERS (id, user_id, password, name, email) VALUES (2, 'sanjigi', 'test', '산지기', 'sanjigi@slipp.net');
-
 INSERT INTO ARTICLES (writer, title, contents, is_deleted, created_date, modified_date)
-VALUES ('javajigi', 'test', 'test', 'N', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+VALUES ('javajigi', 'test', 'test', 'N', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
