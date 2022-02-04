95d30cbfe4791c9a364944dfffe60fea7abb201e
MELT <melt.ep00@gmail.com>
2022-02-04 18:03:39 +0900

    스프링 카페 추가 구현 - 단위 테스트 추가

--- src/test/java/com/kakao/cafe/user/UserServiceTest.java
+++ src/test/java/com/kakao/cafe/user/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.kakao.cafe.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,28 +18,81 @@ class UserServiceTest {
     @Autowired
     private UserService userService;
 
-    @Test
-    public void addUserTest(){
-        // given
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private static final UserDto testUserDto;
+
+    static {
         String userId = "melodist";
         String password = "test";
         String name = "test";
         String email = "test@email.com";
 
+        testUserDto = new UserDto();
+
+        testUserDto.setUserId(userId);
+        testUserDto.setPassword(password);
+        testUserDto.setName(name);
+        testUserDto.setEmail(email);
+    }
+
+    @Test
+    public void addUserTest(){
+        // when
+        userService.addUser(testUserDto);
+        User user = userService.findAll().get(0);
+
+        // then
+        assertThat(user.getUserId()).isEqualTo(testUserDto.getUserId());
+        assertThat(passwordEncoder.matches(testUserDto.getPassword(), user.getPassword()));
+    }
+
+    @Test
+    public void updateUserTest(){
+        // given
+        String password = "test";
+        String name = "test1";
+        String email = "test1@email.com";
+
         UserDto userDto = new UserDto();
 
-        userDto.setUserId(userId);
         userDto.setPassword(password);
         userDto.setName(name);
         userDto.setEmail(email);
 
+        userService.addUser(testUserDto);
+        Integer id = userService.findAll().get(0).getId();
+
         // when
-        userService.addUser(userDto);
-        User user = userService.findAll().get(0);
+        User updatedUser = userService.updateUser(userDto, id);
 
         // then
-        assertThat(user.getUserId()).isEqualTo(userId);
-        assertThat(user.getPassword()).isNotEqualTo(password);
+        assertThat(updatedUser)
+                .usingRecursiveComparison().ignoringFields("id")
+                .isEqualTo(userDto.toEntity());
     }
 
+    @Test
+    public void updateUserFailTest(){
+        // given
+        String password = "test1";
+        String name = "test1";
+        String email = "test1@email.com";
+
+        UserDto userDto = new UserDto();
+
+        userDto.setPassword(password);
+        userDto.setName(name);
+        userDto.setEmail(email);
+
+        userService.addUser(testUserDto);
+        Integer id = userService.findAll().get(0).getId();
+
+        // when
+        User updatedUser = userService.updateUser(userDto, id);
+
+        // then
+        assertThat(updatedUser).isNull();
+    }
 }
